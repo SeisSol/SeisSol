f07d01d64ca33884961dad49f33bce92070a3582
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2015-09-24 11:42:16 +0200

    script for creating a coastline file in vtk format to be displayed with the simulation results

---
+++ postprocessing/visualization/tools/CreateVtkCoastLineFromGmt.py
@@ -0,0 +1,72 @@
+###
+#Author: Thomas Ulrich, LMU, 24.09.2015
+#Read a coastline file from GMT
+#Create a vtk file from that input
+#aim: diplaying the coastline with the simulation results for instance
+projectlatlon = True
+lonmin=90
+lonmax=100
+latmin=-5
+latmax=15
+###
+
+if projectlatlon:
+   import mpl_toolkits.basemap.pyproj as pyproj
+   UTM46N=pyproj.Proj("+init=EPSG:32646")
+   print("using pyproj to project the coordinates...Please check that the projection used corresponds with your lat/lon range") 
+
+#export cordinates from GMT
+import os
+command = "module load gmt;gmt pscoast -R%f/%f/%f/%f -Di -M -W > coastline.dat" %(lonmin, lonmax, latmin, latmax)
+os.system(command)
+
+#Read GMT file
+vertices=[]
+segments=[]
+nvert=0
+newPolyLine=True
+fid=open('coastline.dat')
+for line in fid:
+   if (line.startswith('#')):
+      continue
+   if (line.startswith('>')):
+      newPolyLine=True
+   else:
+      vertices.append(line.split())
+      nvert=nvert+1
+      if newPolyLine==False:
+         segments.append([nvert-1,nvert])
+      newPolyLine=False
+fid.close()
+
+
+#Now write vtk file
+fout=open('CoastLine.vtk','w')
+nlines=0
+
+fout.write('# vtk DataFile Version 2.0\n\
+parabola - polyline\n\
+ASCII\n\n\
+DATASET POLYDATA\n')
+
+
+fout.write('POINTS      %d float\n' %(len(vertices)+1))
+#dummy point for avoiding changing numbering scheme
+fout.write('0 0 0\n')
+for vert in vertices:
+   if projectlatlon:
+      latlon=[float(v) for v in vert[0:2]]
+      xy=UTM46N(latlon[0],latlon[1])
+      fout.write('%e %e 0.\n' %tuple(xy))
+   else:
+      fout.write('%s %s 0.\n' %tuple(vert[0:2]))
+
+
+fout.write('\nLINES %d %d\n' %(len(segments),3*len(segments)))
+for seg in segments:
+   fout.write('2 %s %s\n' %tuple(seg))
+
+fout.write('\n')
+fout.close()
+
+print("CoastLine.vtk successfully created")
