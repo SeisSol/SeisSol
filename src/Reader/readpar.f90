SeisSol/SeisSol

commit d785e92faef0b765bfd2fd641aac162eab71a065

Introduce MPI wrapper Move from fortran main to C main

src/SeisSol.cpp
@@ -44,8 +44,14 @@
 #include <omp.h>
 #endif // _OPENMP
 
-void seissol::SeisSol::init(int rank)
+#include "utils/timeutils.h"
+
+void seissol::SeisSol::init(int argc, char* argv[])
 {
+	m_mpi.init(argc, argv);
+
+	const int rank = m_mpi.rank();
+
   // Print welcome message
   logInfo(rank) << "Welcome to SeisSol";
   logInfo(rank) << "Copyright (c) 2012-2015, SeisSol Group";
@@ -70,15 +76,37 @@ void seissol::SeisSol::init(int rank)
 #endif // _OPENMP
 }
 
-seissol::SeisSol seissol::SeisSol::main;
-
-// Fortran interface
-extern "C"
+void seissol::SeisSol::finalize()
 {
+	const int rank = m_mpi.rank();
 
-void init(int rank)
-{
-  seissol::SeisSol::main.init(rank);
+	m_mpi.finalize();
+
+	logInfo(rank) << "SeisSol done. Goodbye.";
 }
 
+seissol::SeisSol seissol::SeisSol::main;
+
+// Fortran main definition
+extern "C" {
+
+void fortran_main();
+
+}
+
+// Start here
+
+int main(int argc, char* argv[])
+{
+	EPIK_TRACER("SeisSol");
+	SCOREP_USER_REGION("SeisSol", SCOREP_USER_REGION_TYPE_FUNCTION);
+
+	// Initialize SeisSol
+	seissol::SeisSol::main.init(argc, argv);
+
+	// Initialize Fortan Part and run SeisSol
+	fortran_main();
+
+	// Finalize SeisSol
+	seissol::SeisSol::main.finalize();
 }

src/SeisSol.h
@@ -38,9 +38,7 @@
  * Main C++ SeisSol file
  */
 
-#ifdef USE_MPI
-#include <mpi.h>
-#endif
+#include "Parallel/MPI.h"
 
 #ifdef GENERATEDKERNELS
 #include "Solver/time_stepping/TimeManager.h"
@@ -66,6 +64,8 @@ namespace seissol
 class SeisSol
 {
 private:
+	MPI m_mpi;
+
 	MeshReader* m_meshReader;
 
 #ifdef GENERATEDKERNELS
@@ -106,10 +106,18 @@ public:
 
 	/**
 	 * Initialize C++ part of the program
-	 *
-	 * @param rank
 	 */
-	void init(int rank);
+	void init(int argc, char* argv[]);
+
+	/**
+	 * Finalize SeisSol
+	 */
+	void finalize();
+
+	const MPI& mpi() const
+	{
+		return m_mpi;
+	}
 
 #ifdef GENERATEDKERNELS
 	initializers::time_stepping::LtsLayout& getLtsLayout(){ return m_ltsLayout; }

src/Parallel/MPI.cpp
@@ -1,0 +1,41 @@
+/**
+ * @file
+ * This file is part of SeisSol.
+ *
+ * @author Sebastian Rettenberger (sebastian.rettenberger AT tum.de, http://www5.in.tum.de/wiki/index.php/Sebastian_Rettenberger)
+ *
+ * @section LICENSE
+ * Copyright (c) 2015, SeisSol Group
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @section DESCRIPTION
+ */
+
+#include "MPI.h"
+

src/Parallel/MPI.h
@@ -1,0 +1,99 @@
+/**
+ * @file
+ * This file is part of SeisSol.
+ *
+ * @author Sebastian Rettenberger (sebastian.rettenberger AT tum.de, http://www5.in.tum.de/wiki/index.php/Sebastian_Rettenberger)
+ *
+ * @section LICENSE
+ * Copyright (c) 2015, SeisSol Group
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @section DESCRIPTION
+ * MPI Wrapper
+ */
+
+#ifndef MPI_H
+#define MPI_H
+
+#ifndef USE_MPI
+#include "Parallel/MPIDummy.h"
+#else // USE_MPI
+
+#include <mpi.h>
+
+#include "Parallel/MPIBasic.h"
+
+#endif // USE_MPI
+
+namespace seissol
+{
+
+#ifndef USE_MPI
+typedef MPIDummy MPI;
+#else // USE_MPI
+
+/**
+ * MPI handling.
+ *
+ * Make sure only one instance of this class exists!
+ */
+class MPI : public MPIBasic
+{
+public:
+	MPI()
+	{ }
+
+	~MPI()
+	{ }
+
+	/**
+	 * Initialize MPI
+	 */
+	void init(int &argc, char** &argv)
+	{
+		MPI_Init(&argc, &argv);
+
+		MPI_Comm_rank(MPI_COMM_WORLD, &m_rank);
+		MPI_Comm_size(MPI_COMM_WORLD, &m_size);
+	}
+
+	/**
+	 * Finalize MPI
+	 */
+	void finalize()
+	{
+		MPI_Finalize();
+	}
+};
+
+#endif // USE_MPI
+
+}
+
+#endif // MPI_H

src/Parallel/MPIBasic.h
@@ -1,0 +1,87 @@
+/**
+ * @file
+ * This file is part of SeisSol.
+ *
+ * @author Sebastian Rettenberger (sebastian.rettenberger AT tum.de, http://www5.in.tum.de/wiki/index.php/Sebastian_Rettenberger)
+ *
+ * @section LICENSE
+ * Copyright (c) 2015, SeisSol Group
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @section DESCRIPTION
+ */
+
+#ifndef MPI_BASIC_H
+#define MPI_BASIC_H
+
+namespace seissol
+{
+
+/**
+ * Basic MPI abstraction
+ */
+class MPIBasic
+{
+protected:
+	/** This rank */
+	int m_rank;
+
+	/** Number of processors */
+	int m_size;
+
+protected:
+	MPIBasic()
+		: m_rank(0), m_size(1)
+	{ }
+
+public:
+	~MPIBasic()
+	{ }
+
+	/**
+	 * @return The rank of this process
+	 */
+	int rank() const
+	{
+		return m_rank;
+	}
+
+	/**
+	 * @return The total number of processes
+	 */
+	int size() const
+	{
+		return m_size;
+	}
+
+};
+
+}
+
+#endif // MPI_BASIC_H

src/Parallel/MPIDummy.h
@@ -1,0 +1,79 @@
+/**
+ * @file
+ * This file is part of SeisSol.
+ *
+ * @author Sebastian Rettenberger (sebastian.rettenberger AT tum.de, http://www5.in.tum.de/wiki/index.php/Sebastian_Rettenberger)
+ *
+ * @section LICENSE
+ * Copyright (c) 2015, SeisSol Group
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @section DESCRIPTION
+ * Dummy MPI Wrapper
+ */
+
+#ifndef MPI_DUMMY_H
+#define MPI_DUMMY_H
+
+#include "Parallel/MPIBasic.h"
+
+namespace seissol
+{
+
+/**
+ * Dummy class when running without MPI
+ */
+class MPIDummy : public MPIBasic
+{
+public:
+	MPIDummy()
+	{ }
+
+	~MPIDummy()
+	{ }
+
+	/**
+	 * Does nothing
+	 */
+	void init(int &argc, char** &argv)
+	{
+	}
+
+	/**
+	 * Does nothing
+	 */
+	void finalize()
+	{
+	}
+
+};
+
+}
+
+#endif // MPI_DUMMY_H

src/Parallel/SConscript
@@ -1,12 +1,12 @@
-#! /usr/bin/python
+#!/usr/bin/env python
 ##
 # @file
 # This file is part of SeisSol.
 #
-# @author Alex Breuer (breuer AT mytum.de, http://www5.in.tum.de/wiki/index.php/Dipl.-Math._Alexander_Breuer)
+# @author Sebastian Rettenberger (sebastian.rettenberger AT tum.de, http://www5.in.tum.de/wiki/index.php/Sebastian_Rettenberger)
 #
 # @section LICENSE
-# Copyright (c) 2012, SeisSol Group
+# Copyright (c) 2015, SeisSol Group
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -34,33 +34,10 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-#
-# @section DESCRIPTION
-# Definition of the source files.
-#
 
-Import('env')
+Import('env','Glob')
 
-sourceFiles = ['seissolxx.f90', 'SeisSol.cpp']
+sources = Glob('*.f90') + Glob('*.f')
+objs = env.Object(['MPI.cpp'])
 
-for i in sourceFiles:
-    env.sourceFiles.append(env.Object(i))
-
-sourceDirectories = ['Monitoring', 'Geometry', 'Initializer', 'Equations', 'Model', 'Numerical_aux', 'Physics', 'Reader', 'ResultWriter', 'Solver', 'Parallel']
-
-if env['generatedKernels']:
-  sourceDirectories = sourceDirectories + ['Checkpoint']
-
-for sourceDir in sourceDirectories:
-  Export('env')
-  SConscript(sourceDir+'/SConscript_generatedKernels', variant_dir='#/'+env['buildDir']+'/'+sourceDir, duplicate=0)
-  Import('env')
-
-if env['parallelization'] in ['hybrid', 'mpi']:
-  Export('env')
-  SConscript('xdmfwriter/SConscript', variant_dir='#/'+env['buildDir']+'/xdmfwriter', duplicate=0)
-  Import('env')
-  env.sourceFiles.append(env.XdmfWriterSourceFiles)
-
-
-Export('env')
+Return('objs')

src/Parallel/SConscript_generatedKernels
@@ -3,10 +3,10 @@
 # @file
 # This file is part of SeisSol.
 #
-# @author Alex Breuer (breuer AT mytum.de, http://www5.in.tum.de/wiki/index.php/Dipl.-Math._Alexander_Breuer)
+# @author Sebastian Rettenberger (sebastian.rettenberger AT tum.de, http://www5.in.tum.de/wiki/index.php/Sebastian_Rettenberger)
 #
 # @section LICENSE
-# Copyright (c) 2012, SeisSol Group
+# Copyright (c) 2015, SeisSol Group
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -36,31 +36,14 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 # @section DESCRIPTION
-# Definition of the source files.
-#
+# Definition of the reader source files.
 
 Import('env')
 
-sourceFiles = ['seissolxx.f90', 'SeisSol.cpp']
+# parameter reader source file
+readerFiles = [ 'MPI.cpp' ]
 
-for i in sourceFiles:
-    env.sourceFiles.append(env.Object(i))
-
-sourceDirectories = ['Monitoring', 'Geometry', 'Initializer', 'Equations', 'Model', 'Numerical_aux', 'Physics', 'Reader', 'ResultWriter', 'Solver', 'Parallel']
-
-if env['generatedKernels']:
-  sourceDirectories = sourceDirectories + ['Checkpoint']
-
-for sourceDir in sourceDirectories:
-  Export('env')
-  SConscript(sourceDir+'/SConscript_generatedKernels', variant_dir='#/'+env['buildDir']+'/'+sourceDir, duplicate=0)
-  Import('env')
-
-if env['parallelization'] in ['hybrid', 'mpi']:
-  Export('env')
-  SConscript('xdmfwriter/SConscript', variant_dir='#/'+env['buildDir']+'/xdmfwriter', duplicate=0)
-  Import('env')
-  env.sourceFiles.append(env.XdmfWriterSourceFiles)
-
+for i in readerFiles:
+  env.sourceFiles.append(env.Object(i))
 
 Export('env')

commit 3207b501b985a995ffa6c0e822c03f6d86acdc9f

Allow different parameter file names (regression from d785e92faef0b765bfd2fd641aac162eab71a065)

src/Reader/readpar.f90
@@ -3,9 +3,10 @@
 !! This file is part of SeisSol.
 !!
 !! @author Martin Kaeser (martin.kaeser AT geophysik.uni-muenchen.de, http://www.geophysik.uni-muenchen.de/Members/kaeser)
+!! @author Sebastian Rettenberger (sebastian.rettenberger @ tum.de, http://www5.in.tum.de/wiki/index.php/Sebastian_Rettenberger)
 !!
 !! @section LICENSE
-!! Copyright (c) 2010, SeisSol Group
+!! Copyright (c) 2010-2015, SeisSol Group
 !! All rights reserved.
 !! 
 !! Redistribution and use in source and binary forms, with or without
@@ -50,10 +51,6 @@ MODULE COMMON_readpar_mod
   !----------------------------------------------------------------------------
   INTERFACE readpar
      MODULE PROCEDURE readpar
-  END INTERFACE
-
-  INTERFACE readpar_getArguments
-     MODULE PROCEDURE readpar_getArguments
   END INTERFACE
 
   INTERFACE readpar_header
@@ -118,6 +115,7 @@ CONTAINS
 
   SUBROUTINE readpar(EQN,IC,usMESH,DISC,SOURCE,BND,IO, &
                      ANALYSE,Debug,programTitle,MPI)
+    use f_ftoc_bind_interoperability
     !--------------------------------------------------------------------------
     IMPLICIT NONE 
     !--------------------------------------------------------------------------
@@ -136,6 +134,7 @@ CONTAINS
     INTEGER                         :: actual_version_of_readpar
     CHARACTER(LEN=600)              :: Name
     CHARACTER(LEN=801)              :: Name1
+    logical :: existence
     !--------------------------------------------------------------------------
     INTENT(IN)                      :: programTitle
     INTENT(OUT)                     :: IC, BND, DISC, SOURCE, ANALYSE,Debug
@@ -155,11 +154,18 @@ CONTAINS
        STOP                                                                  !
     END IF                                                                   !
     !                                                                        !
-    CALL readpar_getArguments(                                          &    ! 
-            IC           = IC                                            , & !
-            IO           = IO                                            , & !
-            programTitle = programTitle                                    ) !
-    !                                                                        ! 
+    call c_interoperability_getParameterFile(len(IO%ParameterFile), IO%ParameterFile)
+
+    ! Check if the parameter file exists
+    inquire(file=IO%ParameterFile,EXIST=existence)
+    if(existence)then
+    else
+       logError(*) 'You did not specify a valid parameter-file'
+       stop
+    endif
+    !
+    logInfo0(*) '<  Parameters read from file: ', TRIM(IO%ParameterFile) ,'              >'
+    logInfo0(*) '<                                                         >'
     Name1 = TRIM(IO%ParameterFile)                       !
     Name = Name1(1:600)
     !                                                                        ! 
@@ -199,88 +205,6 @@ CONTAINS
     RETURN                                                                   !
     !                                                                        !        
   END SUBROUTINE readpar                                                     !
-
-  !============================================================================
-  ! G E T   A R G U M E N T S
-  !============================================================================
-
-  SUBROUTINE readpar_getArguments(IC,IO,programTitle)
-    !------------------------------------------------------------------------
-    
-    !------------------------------------------------------------------------
-    IMPLICIT NONE 
-    !------------------------------------------------------------------------
-#ifdef PARALLEL
-    include 'mpif.h'
-#endif
-    TYPE (tInitialCondition)   :: IC
-    TYPE (tInputOutput)        :: IO
-    CHARACTER(LEN=100)         :: programTitle
-    ! local variables
-    character(len=500) :: arg,strdummy
-    character(len=4):: file_ext
-    logical :: existence
-    INTEGER                    :: NARG,iErr,i,len_str,myrank,mpisize    
-    !------------------------------------------------------------------------
-    INTENT(IN)                 :: programTitle
-    INTENT(INOUT)              :: IC, IO
-    !------------------------------------------------------------------------
-    ! Reading in command line arguments                                     !
-    !                                                                       !
-    NARG = COMMAND_ARGUMENT_COUNT() ! Number of command-line arguments    
-    IO%MetisWeights =.FALSE.                                                
-    IO%ParameterFile='PARAMETERS.par'
-
-#ifdef PARALLEL
-    !broadcast number of arguments in mpi-version
-    call mpi_bcast(%val(loc(narg)),1,MPI_INTEGER,0,MPI_COMM_WORLD,iErr);
-    !comment on bcast:
-    !%VAL and LOC are non-intrinsic functions necessary when using compaq visual fortran.
-    !For the case your environment doesn't provide these functions - try without, e.g.:
-    !call mpi_bcast(arg,len(arg),MPI_CHARACTER,0,MPI_COMM_WORLD,iErr);
-
-    ! get the rank of this process
-    call mpi_comm_rank(mpi_comm_world,myrank,iErr)
-#endif
-    do i = 1,narg ! loop over commandline arguments
-       CALL GET_COMMAND_ARGUMENT(i,arg) !get argument strings
-#ifdef PARALLEL
-       !broadcast arguments in mpi-version
-       call mpi_bcast(%val(loc(arg)),len(arg),MPI_CHARACTER,0,MPI_COMM_WORLD,iErr);
-       !comment on bcast:
-       !%VAL and LOC are non-intrinsic functions necessary when using compaq visual fortran.
-       !For the case your environment doesn't provide these functions - try without, e.g.:
-       !call mpi_bcast(arg,len(arg),MPI_CHARACTER,0,MPI_COMM_WORLD,iErr);
-#endif
-       inquire(file=trim(adjustl(arg)),EXIST=existence)!check if argument is existing file
-       len_str=len(trim(adjustl(arg)))
-       strdummy=trim(adjustl(arg))
-       file_ext=strdummy(len_str-3:len_str)
-       if((existence).and.(file_ext=='.par'))then !if argument is file with .par extension use parameterfile
-          IO%ParameterFile=trim(adjustl(strdummy))
-          logInfo0(*) 'Will read from parameter-file: ',trim(adjustl(IO%ParameterFile))
-       endif
-       if(trim(adjustl(arg))=='weights')then
-          IO%MetisWeights =.TRUE.
-          logInfo0(*) 'set Metis weights: ',IO%MetisWeights
-       endif
-    enddo
-    inquire(file=IO%ParameterFile,EXIST=existence)
-    if(existence)then
-    else
-#ifdef PARALLEL
-       call mpi_barrier(MPI_COMM_WORLD,iErr)
-       call mpi_finalize(iErr)
-#endif
-
-       logError(*) 'You did not specify any parameter-file for: ',TRIM(adjustl(programTitle))
-       logError(*) '... finalized program execution!'
-       stop
-    endif
-    logInfo0(*) '<  Parameters read from file: ', TRIM(IO%ParameterFile) ,'              >'
-    logInfo0(*) '<                                                         >'
-   !
-  END SUBROUTINE readpar_getArguments                                       !
 
   !============================================================================
   ! H E A D E R
@@ -3668,7 +3592,7 @@ ALLOCATE( SpacePositionx(nDirac), &
 
       select case (io%checkpoint%backend)
         case ("posix")
-            logInfo(*) 'Using POSIX checkpoint backend'
+            logInfo0(*) 'Using POSIX checkpoint backend'
         case ("hdf5")
 #ifndef USE_HDF
             logError(*) 'This version does not support HDF5 checkpoints'
@@ -3686,13 +3610,13 @@ ALLOCATE( SpacePositionx(nDirac), &
             logError(*) 'This version does not support MPI-IO checkpoints'
             stop
 #endif
-            logInfo(*) 'Using async MPI-IO checkpoint backend'
+            logInfo0(*) 'Using async MPI-IO checkpoint backend'
         case ("sionlib")
 #ifndef USE_SIONLIB
             logError(*) 'This version does not support SIONlib checkpoints'
             stop
 #endif
-            logInfo(*) 'Using SIONlib checkpoint backend'
+            logInfo0(*) 'Using SIONlib checkpoint backend'
         case ("none")
             io%checkpoint%interval = 0
         case default

src/SeisSol.cpp
@@ -44,7 +44,7 @@
 #include <omp.h>
 #endif // _OPENMP
 
-#include "utils/timeutils.h"
+#include "utils/args.h"
 
 void seissol::SeisSol::init(int argc, char* argv[])
 {
@@ -74,6 +74,19 @@ void seissol::SeisSol::init(int argc, char* argv[])
 #endif
 #endif
 #endif // _OPENMP
+
+  // Parse command line arguments
+  utils::Args args;
+  args.addAdditionalOption("file", "The parameter file", false);
+  switch (args.parse(argc, argv)) {
+  case utils::Args::Help:
+  case utils::Args::Error:
+	  m_mpi.finalize();
+	  exit(1);
+	  break;
+  }
+
+  m_parameterFile = args.getAdditionalArgument("file", "PARAMETER.par");
 }
 
 void seissol::SeisSol::finalize()

src/SeisSol.h
@@ -40,6 +40,8 @@
 
 #include "Parallel/MPI.h"
 
+#include <string>
+
 #ifdef GENERATEDKERNELS
 #include "Solver/time_stepping/TimeManager.h"
 #include "Solver/Simulator.h"
@@ -65,6 +67,9 @@ class SeisSol
 {
 private:
 	MPI m_mpi;
+
+	/** The name of the parameter file */
+	std::string m_parameterFile;
 
 	MeshReader* m_meshReader;
 
@@ -117,6 +122,11 @@ public:
 	const MPI& mpi() const
 	{
 		return m_mpi;
+	}
+
+	const char* parameterFile() const
+	{
+		return m_parameterFile.c_str();
 	}
 
 #ifdef GENERATEDKERNELS

src/Solver/Interoperability.cpp
@@ -68,6 +68,11 @@ public:
  */
 extern "C" {
   // fortran to c
+  void c_interoperability_getParameterFile(int i_maxlen, char* o_file)
+  {
+	  strncpy(o_file, seissol::SeisSol::main.parameterFile(), i_maxlen);
+  }
+
   void c_interoperability_setDomain( void *i_domain ) {
     e_interoperability.setDomain( i_domain );
   }

src/Solver/f_ftoc_bind_interoperability.f90
@@ -46,6 +46,15 @@ module f_ftoc_bind_interoperability
   ! Fortran to C bindings
   !
 
+  interface
+    subroutine c_interoperability_getParameterFile( i_maxlen, o_file ) bind( C, name='c_interoperability_getParameterFile' )
+        use iso_c_binding
+        implicit none
+        integer(kind=c_int), value                        :: i_maxlen
+        character(kind=c_char), dimension(*), intent(out) :: o_file
+    end subroutine
+  end interface
+
   interface c_interoperability_setDomain
     subroutine c_interoperability_setDomain( i_domain ) bind( C, name='c_interoperability_setDomain' )
       use iso_c_binding, only: c_ptr