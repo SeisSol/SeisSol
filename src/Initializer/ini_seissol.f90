SeisSol/SeisSol

commit d063bf78ba932a23f9334c26f1358147c19df1d3

Fixed temporary array runtime warnings.

src/Initializer/ini_seissol.f90
@@ -116,6 +116,8 @@ CONTAINS
     CHARACTER(LEN=5)               :: cmyrank
     CHARACTER(LEN=600)             :: Filename, InFileName, OutFileName
     LOGICAL                        :: exists
+    REAL                           :: MaterialVals(EQN%nBackgroundVar)
+    REAL                           :: WaveSpeeds(EQN%nNonZeroEV)
 #ifdef HDF
 ! HDF5 variables
      CHARACTER(LEN=100) :: file_name = "" ! File name
@@ -474,17 +476,19 @@ CONTAINS
             l_B = 0.0
             l_attenuation = 0.0
 
+            MaterialVals = optionalFields%backgroundValue(l_elementId, :)
+            WaveSpeeds = disc%galerkin%waveSpeed(l_elementId, l_localFaceId, :)
             ! compute face normal jacobians
             call JacobiNormal3D( A       = l_A,\
                                  absA    = l_B,\
-                                 w0      = optionalFields%backgroundValue(l_elementId, :),\
+                                 w0      = MaterialVals,\
                                  n0      = mesh%fault%geoNormals(  1:3, l_ruptureFace ),\
                                  s0      = mesh%fault%geoTangent1( 1:3, l_ruptureFace ),\
                                  t0      = mesh%fault%geoTangent2( 1:3, l_ruptureFace ),\
                                  eqn     = eqn,\
                                  io      = io, &
                                  ! @breuera: different wave speeds for the faces? that's strange..
-                                 w_speed = disc%galerkin%waveSpeed(l_elementId, l_localFaceId, :), &
+                                 w_speed = WaveSpeeds, &
                                  aniVec  = l_attenuation )
 
             ! rotate jacobian to match SeisSol's storage scheme

commit f3d5546a7d26af11042b4acc29d777dc4c68f927

Fixed error introduced in d063bf7

src/Initializer/ini_seissol.f90
@@ -476,8 +476,8 @@ CONTAINS
             l_B = 0.0
             l_attenuation = 0.0
 
-            MaterialVals = optionalFields%backgroundValue(l_elementId, :)
-            WaveSpeeds = disc%galerkin%waveSpeed(l_elementId, l_localFaceId, :)
+            MaterialVals(1:EQN%nBackgroundVar) = optionalFields%backgroundValue(l_elementId, :)
+            WaveSpeeds(1:EQN%nNonZeroEV) = disc%galerkin%waveSpeed(l_elementId, l_localFaceId, :)
             ! compute face normal jacobians
             call JacobiNormal3D( A       = l_A,\
                                  absA    = l_B,\