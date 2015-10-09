SeisSol/SeisSol

commit 8660b43f30563b41a560a5939fd07971df283b7a

strike and dip slip calculation: revert changes from r2385 and use a different method: The slip is already calculated in Evaluate_friction_law. The idea is to use directly this value instead of recomputing from the SR. Because we want the 2 components of slip, I had to change slip array instead slip1 and slip2 (it affect the checkpointing routines, hopefully I didnt break anything). advantage: much efficient and probably accurate (with previous method, I observed slow down of ~40% on a 5e6 mesh case). disadvantage: the slip at a given point in now approximated by the slip at the closest GP.

src/Checkpoint/Fault.cpp
@@ -40,4 +40,4 @@
 #include "Fault.h"
 
 const char* seissol::checkpoint::Fault::VAR_NAMES[NUM_VARIABLES] = {
-		"mu", "slip_rate1", "slip_rate2", "slip", "state", "strength" };
+		"mu", "slip_rate1", "slip_rate2", "slip1", "slip2", "state", "strength" };

src/Checkpoint/Fault.h
@@ -62,7 +62,7 @@ namespace checkpoint
 class Fault : virtual public CheckPoint
 {
 protected:
-	static const int NUM_VARIABLES = 6;
+	static const int NUM_VARIABLES = 7;
 
 private:
 	/** Pointers to fault data */
@@ -84,7 +84,8 @@ public:
 	/**
 	 * @return True of a valid checkpoint is available
 	 */
-	virtual bool init(double* mu, double* slipRate1, double* slipRate2, double* slip,
+	virtual bool init(double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
+
 			double* state, double* strength,
 			unsigned int numSides, unsigned int numBndGP)
 	{
@@ -112,9 +113,10 @@ public:
 		m_data[0] = mu;
 		m_data[1] = slipRate1;
 		m_data[2] = slipRate2;
-		m_data[3] = slip;
-		m_data[4] = state;
-		m_data[5] = strength;
+		m_data[3] = slip1;
+		m_data[4] = slip2;
+		m_data[5] = state;
+		m_data[6] = strength;
 		m_numSides = numSides;
 		m_numBndGP = numBndGP;
 

src/Checkpoint/Manager.h
@@ -138,7 +138,8 @@ public:
 	 * @return True is a checkpoint was loaded, false otherwise
 	 */
 	bool init(real* dofs, unsigned int numDofs,
-			double* mu, double* slipRate1, double* slipRate2, double* slip,
+			double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
+
 			double* state, double* strength, unsigned int numSides, unsigned int numBndGP,
 			double &time, int &waveFieldTimeStep, int &faultTimeStep)
 	{
@@ -146,7 +147,7 @@ public:
 			return false;
 
 		int exists = m_waveField->init(dofs, numDofs);
-		exists &= m_fault->init(mu, slipRate1, slipRate2, slip,
+		exists &= m_fault->init(mu, slipRate1, slipRate2, slip1, slip2,
 				state, strength, numSides, numBndGP);
 
 		// Make sure all rank think the same about the existing checkpoint

src/Checkpoint/h5/Fault.cpp
@@ -40,11 +40,11 @@
 #include "Fault.h"
 
 bool seissol::checkpoint::h5::Fault::init(
-		double* mu, double* slipRate1, double* slipRate2, double* slip,
+		double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
 		double* state, double* strength,
 		unsigned int numSides, unsigned int numBndGP)
 {
-	seissol::checkpoint::Fault::init(mu, slipRate1, slipRate2, slip, state, strength,
+	seissol::checkpoint::Fault::init(mu, slipRate1, slipRate2, slip1, slip2, state, strength,
 			numSides, numBndGP);
 
 	if (numSides == 0)

src/Checkpoint/h5/Fault.h
@@ -86,7 +86,7 @@ private:
 	 * Identifiers of the main data set in the files
 	 * @todo Make the size depend on the size of VAR_NAMES
 	 */
-	hid_t m_h5data[2][6];
+	hid_t m_h5data[2][7];
 
 	/** Identifiers for the file space of the data set */
 	hid_t m_h5fSpaceData;
@@ -96,7 +96,7 @@ public:
 		: m_h5fSpaceData(-1)
 	{}
 
-	bool init(double* mu, double* slipRate1, double* slipRate2, double* slip,
+	bool init(double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
 			double* state, double* strength,
 			unsigned int numSides, unsigned int numBndGP);
 

src/Checkpoint/mpio/Fault.cpp
@@ -40,11 +40,12 @@
 #include "Fault.h"
 
 bool seissol::checkpoint::mpio::Fault::init(
-		double* mu, double* slipRate1, double* slipRate2, double* slip,
+		double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
+
 		double* state, double* strength,
 		unsigned int numSides, unsigned int numBndGP)
 {
-	seissol::checkpoint::Fault::init(mu, slipRate1, slipRate2, slip, state, strength,
+	seissol::checkpoint::Fault::init(mu, slipRate1, slipRate2, slip1, slip2, state, strength,
 			numSides, numBndGP);
 
 	if (numSides == 0)

src/Checkpoint/mpio/Fault.h
@@ -78,7 +78,7 @@ public:
 		: CheckPoint(0x7A849, sizeof(double)*NUM_VARIABLES)
 	{}
 
-	bool init(double* mu, double* slipRate1, double* slipRate2, double* slip,
+	bool init(double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
 			double* state, double* strength,
 			unsigned int numSides, unsigned int numBndGP);
 

commit 0185cbfa627eb7478dc0c078fb3e7eb4f757f270

(hopefully) fix bug introduced in 8660b43f30563b41a560a5939fd07971df283b7a.  Patch not extensively tested :p

src/Checkpoint/Fault.cpp
@@ -40,4 +40,4 @@
 #include "Fault.h"
 
 const char* seissol::checkpoint::Fault::VAR_NAMES[NUM_VARIABLES] = {
-		"mu", "slip_rate1", "slip_rate2", "slip1", "slip2", "state", "strength" };
+		"mu", "slip_rate1", "slip_rate2", "slip", "slip1", "slip2", "state", "strength" };

src/Checkpoint/Fault.h
@@ -62,7 +62,7 @@ namespace checkpoint
 class Fault : virtual public CheckPoint
 {
 protected:
-	static const unsigned int NUM_VARIABLES = 7;
+	static const unsigned int NUM_VARIABLES = 8;
 
 private:
 	/** Pointers to fault data */
@@ -84,7 +84,7 @@ public:
 	/**
 	 * @return True of a valid checkpoint is available
 	 */
-	virtual bool init(double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
+	virtual bool init(double* mu, double* slipRate1, double* slipRate2, double* slip, double* slip1, double* slip2,
 
 			double* state, double* strength,
 			unsigned int numSides, unsigned int numBndGP)
@@ -113,10 +113,11 @@ public:
 		m_data[0] = mu;
 		m_data[1] = slipRate1;
 		m_data[2] = slipRate2;
-		m_data[3] = slip1;
-		m_data[4] = slip2;
-		m_data[5] = state;
-		m_data[6] = strength;
+		m_data[3] = slip;
+		m_data[4] = slip1;
+		m_data[5] = slip2;
+		m_data[6] = state;
+		m_data[7] = strength;
 		m_numSides = numSides;
 		m_numBndGP = numBndGP;
 

src/Checkpoint/Manager.h
@@ -153,7 +153,7 @@ public:
 	 * @return True is a checkpoint was loaded, false otherwise
 	 */
 	bool init(real* dofs, unsigned int numDofs,
-			double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
+			double* mu, double* slipRate1, double* slipRate2, double* slip, double* slip1, double* slip2,
 
 			double* state, double* strength, unsigned int numSides, unsigned int numBndGP,
 			double &time, int &waveFieldTimeStep, int &faultTimeStep)
@@ -162,7 +162,7 @@ public:
 			return false;
 
 		int exists = m_waveField->init(dofs, numDofs);
-		exists &= m_fault->init(mu, slipRate1, slipRate2, slip1, slip2,
+		exists &= m_fault->init(mu, slipRate1, slipRate2, slip, slip1, slip2,
 				state, strength, numSides, numBndGP);
 
 		// Make sure all rank think the same about the existing checkpoint

src/Checkpoint/h5/Fault.cpp
@@ -40,11 +40,11 @@
 #include "Fault.h"
 
 bool seissol::checkpoint::h5::Fault::init(
-		double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
+		double* mu, double* slipRate1, double* slipRate2, double* slip, double* slip1, double* slip2,
 		double* state, double* strength,
 		unsigned int numSides, unsigned int numBndGP)
 {
-	seissol::checkpoint::Fault::init(mu, slipRate1, slipRate2, slip1, slip2, state, strength,
+	seissol::checkpoint::Fault::init(mu, slipRate1, slipRate2, slip, slip1, slip2, state, strength,
 			numSides, numBndGP);
 
 	if (numSides == 0)

src/Checkpoint/h5/Fault.h
@@ -86,7 +86,7 @@ private:
 	 * Identifiers of the main data set in the files
 	 * @todo Make the size depend on the size of VAR_NAMES
 	 */
-	hid_t m_h5data[2][7];
+	hid_t m_h5data[2][8];
 
 	/** Identifiers for the file space of the data set */
 	hid_t m_h5fSpaceData;
@@ -96,7 +96,7 @@ public:
 		: m_h5fSpaceData(-1)
 	{}
 
-	bool init(double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
+	bool init(double* mu, double* slipRate1, double* slipRate2, double* slip, double* slip1, double* slip2,
 			double* state, double* strength,
 			unsigned int numSides, unsigned int numBndGP);
 

src/Checkpoint/mpio/Fault.cpp
@@ -42,12 +42,12 @@
 #include "Fault.h"
 
 bool seissol::checkpoint::mpio::Fault::init(
-		double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
+		double* mu, double* slipRate1, double* slipRate2, double* slip, double* slip1, double* slip2,
 
 		double* state, double* strength,
 		unsigned int numSides, unsigned int numBndGP)
 {
-	seissol::checkpoint::Fault::init(mu, slipRate1, slipRate2, slip1, slip2, state, strength,
+	seissol::checkpoint::Fault::init(mu, slipRate1, slipRate2, slip, slip1, slip2, state, strength,
 			numSides, numBndGP);
 
 	if (numSides == 0)

src/Checkpoint/mpio/Fault.h
@@ -78,7 +78,7 @@ public:
 		: CheckPoint(0x7A849, sizeof(double)*NUM_VARIABLES)
 	{}
 
-	bool init(double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
+	bool init(double* mu, double* slipRate1, double* slipRate2, double* slip, double* slip1, double* slip2,
 			double* state, double* strength,
 			unsigned int numSides, unsigned int numBndGP);
 

src/Checkpoint/mpio/FaultAsync.cpp
@@ -44,11 +44,11 @@
 #include "FaultAsync.h"
 
 bool seissol::checkpoint::mpio::FaultAsync::init(
-		double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
+		double* mu, double* slipRate1, double* slipRate2, double* slip, double* slip1, double* slip2,
 		double* state, double* strength,
 		unsigned int numSides, unsigned int numBndGP)
 {
-	bool exists = Fault::init(mu, slipRate1, slipRate2, slip1, slip2, state, strength,
+	bool exists = Fault::init(mu, slipRate1, slipRate2, slip, slip1, slip2, state, strength,
 			numSides, numBndGP);
 
 	if (numSides != 0)

src/Checkpoint/mpio/FaultAsync.h
@@ -76,7 +76,7 @@ public:
 	{
 	}
 
-	bool init(double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
+	bool init(double* mu, double* slipRate1, double* slipRate2, double* slip, double* slip1, double* slip2,
 			double* state, double* strength,
 			unsigned int numSides, unsigned int numBndGP);
 

src/Checkpoint/posix/Fault.cpp
@@ -42,12 +42,12 @@
 #include "Kernels/precision.hpp"
 
 bool seissol::checkpoint::posix::Fault::init(
-		double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
+		double* mu, double* slipRate1, double* slipRate2, double* slip, double* slip1, double* slip2,
 
 		double* state, double* strength,
 		unsigned int numSides, unsigned int numBndGP)
 {
-	seissol::checkpoint::Fault::init(mu, slipRate1, slipRate2, slip1, slip2, state, strength,
+	seissol::checkpoint::Fault::init(mu, slipRate1, slipRate2, slip, slip1, slip2, state, strength,
 			numSides, numBndGP);
 
 	if (numSides == 0)

src/Checkpoint/posix/Fault.h
@@ -66,7 +66,7 @@ public:
 		: CheckPoint(0x7A127)
 	{}
 
-	bool init(double* mu, double* slipRate1, double* slipRate2, double* slip1, double* slip2,
+	bool init(double* mu, double* slipRate1, double* slipRate2, double* slip, double* slip1, double* slip2,
 			double* state, double* strength,
 			unsigned int numSides, unsigned int numBndGP);
 

src/Initializer/dg_setup.f90
@@ -1078,6 +1078,7 @@ CONTAINS
 
       ALLOCATE(DISC%DynRup%SlipRate1(MESH%Fault%nSide,DISC%Galerkin%nBndGP))
       ALLOCATE(DISC%DynRup%SlipRate2(MESH%Fault%nSide,DISC%Galerkin%nBndGP))
+      ALLOCATE(DISC%DynRup%Slip(MESH%Fault%nSide,DISC%Galerkin%nBndGP))
       ALLOCATE(DISC%DynRup%Slip1(MESH%Fault%nSide,DISC%Galerkin%nBndGP))
       ALLOCATE(DISC%DynRup%Slip2(MESH%Fault%nSide,DISC%Galerkin%nBndGP))
       ALLOCATE(DISC%DynRup%Mu(MESH%Fault%nSide,DISC%Galerkin%nBndGP))
@@ -1085,6 +1086,7 @@ CONTAINS
       !
       DISC%DynRup%SlipRate1     = EQN%IniSlipRate1
       DISC%DynRup%SlipRate2     = EQN%IniSlipRate2
+      DISC%DynRup%Slip          = 0.0D0
       DISC%DynRup%Slip1          = 0.0D0
       DISC%DynRup%Slip2          = 0.0D0
       DISC%DynRup%Mu(:,:)       = EQN%IniMu(:,:)

src/Numerical_aux/typesdef.f90
@@ -884,8 +884,9 @@ MODULE TypesDef
   END TYPE tDynRup_output
 
   TYPE tDynRup
-     REAL, POINTER                          :: Slip1(:,:)                        !< Slip path at given fault node along loc dir 1
-     REAL, POINTER                          :: Slip2(:,:)                        !< Slip path at given fault node along loc dir 2
+     REAL, POINTER                          :: Slip(:,:)                        !< Slip path at given fault node
+     REAL, POINTER                          :: Slip1(:,:)                        !< Slip at given fault node along loc dir 1
+     REAL, POINTER                          :: Slip2(:,:)                        !< Slip at given fault node along loc dir 2
      REAL, POINTER                          :: SlipRate1(:,:)                   !< Slip Rate at given fault node
      REAL, POINTER                          :: SlipRate2(:,:)                   !< Slip Rate at given fault node
      REAL, POINTER                          :: Mu(:,:)                          !< Current friction coefficient at given fault node

src/Physics/Evaluate_friction_law.f90
@@ -250,7 +250,7 @@ MODULE Eval_friction_law_mod
     REAL        :: iT(:,:)                                      ! inverse Transformation matrix    !
     REAL        :: Stress(1:6,1:nBndGP)
     REAL        :: tmpSlip
-    REAL        :: LocMu, LocD_C, LocSlip1, LocSlip2, LocSlip, LocP, P, LocSR, ShTest
+    REAL        :: LocMu, LocD_C, LocSlip, LocSlip1, LocSlip2, LocP, P, LocSR, ShTest
     REAL        :: LocMu_S, LocMu_D
     REAL        :: LocSR1,LocSR2
     REAL        :: P_0,Strength,cohesion
@@ -291,6 +291,7 @@ MODULE Eval_friction_law_mod
      LocMu_S   = DISC%DynRup%Mu_S(iFace,iBndGP)
      LocMu_D   = DISC%DynRup%Mu_D(iFace,iBndGP)
      LocD_C    = DISC%DynRup%D_C(iFace,iBndGP)
+     LocSlip   = DISC%DynRup%Slip(iFace,iBndGP)
      LocSlip1   = DISC%DynRup%Slip1(iFace,iBndGP)
      LocSlip2   = DISC%DynRup%Slip2(iFace,iBndGP)
      LocSR1    = DISC%DynRup%SlipRate1(iFace,iBndGP)
@@ -337,7 +338,7 @@ MODULE Eval_friction_law_mod
        ! Update slip
        LocSlip1 = LocSlip1 + LocSR1*time_inc
        LocSlip2 = LocSlip2 + LocSR2*time_inc
-       LocSlip      = SQRT(LocSlip1**2 + LocSlip2**2)
+       LocSlip = LocSlip + LocSR*time_inc
        tmpSlip = tmpSlip + LocSR*time_inc
        !
        IF(ABS(LocSlip).LT.LocD_C) THEN
@@ -374,6 +375,7 @@ MODULE Eval_friction_law_mod
      DISC%DynRup%Mu(iFace,iBndGP)        = LocMu
      DISC%DynRup%SlipRate1(iFace,iBndGP) = LocSR1
      DISC%DynRup%SlipRate2(iFace,iBndGP) = LocSR2
+     DISC%DynRup%Slip(iFace,iBndGP)      = LocSlip
      DISC%DynRup%Slip1(iFace,iBndGP)      = LocSlip1
      DISC%DynRup%Slip2(iFace,iBndGP)      = LocSlip2
      !
@@ -423,7 +425,7 @@ MODULE Eval_friction_law_mod
     REAL        :: sigma
     REAL        :: iT(:,:)                                      ! inverse Transformation matrix    !
     REAL        :: Stress(6,1:nBndGP)
-    REAL        :: LocMu, LocD_C, LocSlip1, LocSlip2, LocSlip, LocP, P, LocSR, ShTest
+    REAL        :: LocMu, LocD_C, LocSlip, LocSlip1, LocSlip2, LocP, P, LocSR, ShTest
     REAL        :: LocMu_S, LocMu_D
     REAL        :: LocSR1,LocSR2
     REAL        :: P_0,cohesion, Strength
@@ -462,6 +464,7 @@ MODULE Eval_friction_law_mod
      LocMu_S   = DISC%DynRup%Mu_S(iFace,iBndGP)
      LocMu_D   = DISC%DynRup%Mu_D(iFace,iBndGP)
      LocD_C    = DISC%DynRup%D_C(iFace,iBndGP)
+     LocSlip   = DISC%DynRup%Slip(iFace,iBndGP)
      LocSlip1   = DISC%DynRup%Slip1(iFace,iBndGP)
      LocSlip2   = DISC%DynRup%Slip2(iFace,iBndGP)
      LocSR1    = DISC%DynRup%SlipRate1(iFace,iBndGP)
@@ -505,7 +508,7 @@ MODULE Eval_friction_law_mod
        ! Update slip
        LocSlip1 = LocSlip1 + LocSR1*time_inc
        LocSlip2 = LocSlip2 + LocSR2*time_inc
-       LocSlip      = SQRT(LocSlip1**2 + LocSlip2**2)
+       LocSlip = LocSlip + LocSR*time_inc
        !
        IF(ABS(LocSlip).LT.LocD_C) THEN
          LocMu = LocMu_S - (LocMu_S-LocMu_D)/LocD_C*ABS(LocSlip)
@@ -540,6 +543,7 @@ MODULE Eval_friction_law_mod
      DISC%DynRup%Mu(iFace,iBndGP)        = LocMu
      DISC%DynRup%SlipRate1(iFace,iBndGP) = LocSR1
      DISC%DynRup%SlipRate2(iFace,iBndGP) = LocSR2
+     DISC%DynRup%Slip(iFace,iBndGP)      = LocSlip
      DISC%DynRup%Slip1(iFace,iBndGP)      = LocSlip1
      DISC%DynRup%Slip2(iFace,iBndGP)      = LocSlip2
      DISC%DynRup%Strength(iFace,iBndGP)  = Strength_exp
@@ -581,7 +585,7 @@ MODULE Eval_friction_law_mod
     REAL        :: TractionGP_XZ(nBndGP,nTimeGP)
     REAL        :: iT(:,:)                                      ! inverse Transformation matrix    !
     REAL        :: Stress(1:6,1:nBndGP)
-    REAL        :: LocMu, LocD_C, LocSlip1, LocSlip2, LocSlip, LocP, P, LocSR, ShTest
+    REAL        :: LocMu, LocD_C, LocSlip, LocSlip1, LocSlip2, LocP, P, LocSR, ShTest
     REAL        :: LocMu_S, LocMu_D
     REAL        :: tmpSlip
     REAL        :: LocSR1,LocSR2
@@ -625,6 +629,7 @@ MODULE Eval_friction_law_mod
      LocMu_S   = DISC%DynRup%Mu_S(iFace,iBndGP)
      LocMu_D   = DISC%DynRup%Mu_D(iFace,iBndGP)
      LocD_C    = DISC%DynRup%D_C(iFace,iBndGP)
+     LocSlip   = DISC%DynRup%Slip(iFace,iBndGP)
      LocSlip1   = DISC%DynRup%Slip1(iFace,iBndGP)
      LocSlip2   = DISC%DynRup%Slip2(iFace,iBndGP)
      LocSR1    = DISC%DynRup%SlipRate1(iFace,iBndGP)
@@ -672,7 +677,7 @@ MODULE Eval_friction_law_mod
        ! Update slip
        LocSlip1 = LocSlip1 + LocSR1*time_inc
        LocSlip2 = LocSlip2 + LocSR2*time_inc
-       LocSlip      = SQRT(LocSlip1**2 + LocSlip2**2)
+       LocSlip = LocSlip + LocSR*time_inc
        
        tmpSlip = tmpSlip + LocSR*time_inc
        !
@@ -718,6 +723,7 @@ MODULE Eval_friction_law_mod
      DISC%DynRup%Mu(iFace,iBndGP)        = LocMu
      DISC%DynRup%SlipRate1(iFace,iBndGP) = LocSR1
      DISC%DynRup%SlipRate2(iFace,iBndGP) = LocSR2
+     DISC%DynRup%Slip(iFace,iBndGP)      = LocSlip
      DISC%DynRup%Slip1(iFace,iBndGP)      = LocSlip1
      DISC%DynRup%Slip2(iFace,iBndGP)      = LocSlip2
      !
@@ -767,7 +773,7 @@ MODULE Eval_friction_law_mod
     REAL        :: TractionGP_XZ(nBndGP,nTimeGP)
     REAL        :: iT(:,:)                                      ! inverse Transformation matrix    !
     REAL        :: Stress(6,1:nBndGP)
-    REAL        :: LocMu, LocD_C, LocSlip1, LocSlip2, LocP, P, LocSR, ShTest
+    REAL        :: LocMu, LocD_C, LocSlip, LocSlip1, LocSlip2, LocP, P, LocSR, ShTest
     REAL        :: LocMu_S, LocMu_D
     REAL        :: LocSR1,LocSR2
     REAL        :: P_0,Strength,cohesion
@@ -804,6 +810,7 @@ MODULE Eval_friction_law_mod
 
     DO iBndGP=1,nBndGP
      !
+     LocSlip   = DISC%DynRup%Slip(iFace,iBndGP)
      LocSlip1   = DISC%DynRup%Slip1(iFace,iBndGP)
      LocSlip2   = DISC%DynRup%Slip2(iFace,iBndGP)
      LocSR1    = DISC%DynRup%SlipRate1(iFace,iBndGP)
@@ -897,7 +904,7 @@ MODULE Eval_friction_law_mod
        LocTracXZ = LocTracXZ - Stress(6,iBndGP)
        !
        ! Compute slip
-       !LocSlip   = LocSlip  + (LocSR)*time_inc ! ABS of LocSR removed as it would be the accumulated slip that is usually not needed in the solver, see linear slip weakening
+       LocSlip   = LocSlip  + (LocSR)*time_inc ! ABS of LocSR removed as it would be the accumulated slip that is usually not needed in the solver, see linear slip weakening
        !
        !Update slip rate (notice that LocSR(T=0)=-2c_s/mu*s_xy^{Godunov} is the slip rate caused by a free surface!)
        LocSR1     = -(1.0D0/(w_speed(2)*rho)+1.0D0/(w_speed_neig(2)*rho_neig))*(LocTracXY-XYStressGP(iBndGP,iTimeGP))
@@ -928,6 +935,7 @@ MODULE Eval_friction_law_mod
      DISC%DynRup%Mu(iFace,iBndGP)        = LocMu
      DISC%DynRup%SlipRate1(iFace,iBndGP) = LocSR1
      DISC%DynRup%SlipRate2(iFace,iBndGP) = LocSR2
+     DISC%DynRup%Slip(iFace,iBndGP)      = LocSlip
      DISC%DynRup%Slip1(iFace,iBndGP)      = LocSlip1
      DISC%DynRup%Slip2(iFace,iBndGP)      = LocSlip2
      DISC%DynRup%StateVar(iFace,iBndGP)  = LocSV
@@ -968,7 +976,7 @@ MODULE Eval_friction_law_mod
     REAL        :: TractionGP_XZ(nBndGP,nTimeGP)
     REAL        :: iT(:,:)                                      ! inverse Transformation matrix    !
     REAL        :: Stress(6,1:nBndGP)
-    REAL        :: LocMu, LocD_C, LocSlip1, LocSlip2, LocP, P, LocSR, ShTest
+    REAL        :: LocMu, LocD_C, LocSlip, LocSlip1, LocSlip2, LocP, P, LocSR, ShTest
     REAL        :: LocMu_S, LocMu_D
     REAL        :: LocSR1,LocSR2
     REAL        :: P_0,Strength,cohesion
@@ -1015,6 +1023,7 @@ MODULE Eval_friction_law_mod
 
     DO iBndGP=1,nBndGP
      !
+     LocSlip   = DISC%DynRup%Slip(iFace,iBndGP)
      LocSlip1   = DISC%DynRup%Slip1(iFace,iBndGP)
      LocSlip2   = DISC%DynRup%Slip2(iFace,iBndGP)
      LocSR1    = DISC%DynRup%SlipRate1(iFace,iBndGP)
@@ -1091,7 +1100,7 @@ MODULE Eval_friction_law_mod
        LocTracXZ = LocTracXZ - Stress(6,iBndGP)
        !
        ! Compute slip
-       !LocSlip   = LocSlip  + (LocSR)*time_inc ! ABS of LocSR removed as it would be the accumulated slip that is usually not needed in the solver, see linear slip weakening
+       LocSlip   = LocSlip  + (LocSR)*time_inc ! ABS of LocSR removed as it would be the accumulated slip that is usually not needed in the solver, see linear slip weakening
        !
        !Update slip rate (notice that LocSR(T=0)=-2c_s/mu*s_xy^{Godunov} is the slip rate caused by a free surface!)
        LocSR1     = -(1.0D0/(w_speed(2)*rho)+1.0D0/(w_speed_neig(2)*rho_neig))*(LocTracXY-XYStressGP(iBndGP,iTimeGP))
@@ -1120,6 +1129,7 @@ MODULE Eval_friction_law_mod
      DISC%DynRup%Mu(iFace,iBndGP)        = LocMu
      DISC%DynRup%SlipRate1(iFace,iBndGP) = LocSR1
      DISC%DynRup%SlipRate2(iFace,iBndGP) = LocSR2
+     DISC%DynRup%Slip(iFace,iBndGP)      = LocSlip
      DISC%DynRup%Slip1(iFace,iBndGP)      = LocSlip1
      DISC%DynRup%Slip2(iFace,iBndGP)      = LocSlip2
      DISC%DynRup%StateVar(iFace,iBndGP)  = LocSV
@@ -1166,7 +1176,7 @@ MODULE Eval_friction_law_mod
     REAL        :: TractionGP_XZ(nBndGP,nTimeGP)
     REAL        :: iT(:,:)                                      ! inverse Transformation matrix    !
     REAL        :: Stress(6,1:nBndGP)
-    REAL        :: LocMu, LocD_C, LocSlip1, LocSlip2, LocP, P, LocSR, ShTest
+    REAL        :: LocMu, LocD_C, LocSlip, LocSlip1, LocSlip2, LocP, P, LocSR, ShTest
     REAL        :: LocMu_S, LocMu_D
     REAL        :: LocSR1,LocSR2
     REAL        :: P_0,Strength,cohesion
@@ -1296,6 +1306,7 @@ MODULE Eval_friction_law_mod
     !
     DO iBndGP=1,nBndGP
      !
+     LocSlip   = DISC%DynRup%Slip(iFace,iBndGP)
      LocSlip1   = DISC%DynRup%Slip1(iFace,iBndGP)
      LocSlip2   = DISC%DynRup%Slip2(iFace,iBndGP)
      LocSR1    = DISC%DynRup%SlipRate1(iFace,iBndGP)
@@ -1386,7 +1397,7 @@ MODULE Eval_friction_law_mod
        LocTracXZ = LocTracXZ - Stress(6,iBndGP)
        !
        ! Compute slip
-       !LocSlip   = LocSlip  + (LocSR)*time_inc ! ABS of LocSR removed as it would be the accumulated slip that is usually not needed in the solver, see linear slip weakening
+       LocSlip   = LocSlip  + (LocSR)*time_inc ! ABS of LocSR removed as it would be the accumulated slip that is usually not needed in the solver, see linear slip weakening
        !
        !Update slip rate (notice that LocSR(T=0)=-2c_s/mu*s_xy^{Godunov} is the slip rate caused by a free surface!)
        LocSR1     = -(1.0D0/(w_speed(2)*rho)+1.0D0/(w_speed_neig(2)*rho_neig))*(LocTracXY-XYStressGP(iBndGP,iTimeGP))
@@ -1415,6 +1426,7 @@ MODULE Eval_friction_law_mod
      DISC%DynRup%Mu(iFace,iBndGP)        = LocMu
      DISC%DynRup%SlipRate1(iFace,iBndGP) = LocSR1
      DISC%DynRup%SlipRate2(iFace,iBndGP) = LocSR2
+     DISC%DynRup%Slip(iFace,iBndGP)      = LocSlip
      DISC%DynRup%Slip1(iFace,iBndGP)      = LocSlip1
      DISC%DynRup%Slip2(iFace,iBndGP)      = LocSlip2
      DISC%DynRup%StateVar(iFace,iBndGP)  = LocSV
@@ -1460,7 +1472,7 @@ MODULE Eval_friction_law_mod
     REAL        :: TractionGP_XZ(nBndGP,nTimeGP)
     REAL        :: iT(:,:)                                      ! inverse Transformation matrix    !
     REAL        :: Stress(6,1:nBndGP)
-    REAL        :: LocMu, LocD_C, LocSlip1, LocSlip2, LocP, P, LocSR, ShTest
+    REAL        :: LocMu, LocD_C, LocSlip, LocSlip1, LocSlip2, LocP, P, LocSR, ShTest
     REAL        :: LocMu_S, LocMu_D
     REAL        :: LocSR1,LocSR2
     REAL        :: P_0,Strength,cohesion
@@ -1591,6 +1603,7 @@ MODULE Eval_friction_law_mod
     !
     DO iBndGP=1,nBndGP
      !
+     LocSlip   = DISC%DynRup%Slip(iFace,iBndGP)
      LocSlip1   = DISC%DynRup%Slip1(iFace,iBndGP)
      LocSlip2   = DISC%DynRup%Slip2(iFace,iBndGP)
      LocSR1    = DISC%DynRup%SlipRate1(iFace,iBndGP)
@@ -1688,7 +1701,7 @@ MODULE Eval_friction_law_mod
          LocTracXZ = LocTracXZ - Stress(6,iBndGP)
          !
          ! Compute slip
-         !LocSlip   = LocSlip  + (LocSR)*time_inc ! ABS of LocSR removed as it would be the accumulated slip that is usually not needed in the solver, see linear slip weakening
+         LocSlip   = LocSlip  + (LocSR)*time_inc ! ABS of LocSR removed as it would be the accumulated slip that is usually not needed in the solver, see linear slip weakening
          !
          !Update slip rate (notice that LocSR(T=0)=-2c_s/mu*s_xy^{Godunov} is the slip rate caused by a free surface!)
          LocSR1     = -(1.0D0/(w_speed(2)*rho)+1.0D0/(w_speed_neig(2)*rho_neig))*(LocTracXY-XYStressGP(iBndGP,iTimeGP))
@@ -1717,6 +1730,7 @@ MODULE Eval_friction_law_mod
      DISC%DynRup%Mu(iFace,iBndGP)        = LocMu
      DISC%DynRup%SlipRate1(iFace,iBndGP) = LocSR1
      DISC%DynRup%SlipRate2(iFace,iBndGP) = LocSR2
+     DISC%DynRup%Slip(iFace,iBndGP)      = LocSlip
      DISC%DynRup%Slip1(iFace,iBndGP)      = LocSlip1
      DISC%DynRup%Slip2(iFace,iBndGP)      = LocSlip2
      DISC%DynRup%StateVar(iFace,iBndGP)  = LocSV

src/ResultWriter/inioutput_seissol.f90
@@ -186,6 +186,7 @@ CONTAINS
         i_mu        = disc%DynRup%mu,        &
         i_slipRate1 = disc%DynRup%slipRate1, &
         i_slipRate2 = disc%DynRup%slipRate2, &
+        i_slip     = disc%DynRup%slip,      &
         i_slip1     = disc%DynRup%slip1,    &
         i_slip2     = disc%DynRup%slip2,    &
         i_state     = disc%DynRup%stateVar,  &

src/Solver/Interoperability.cpp
@@ -161,9 +161,9 @@ extern "C" {
   }
 
   void c_interoperability_initializeIO( double* mu, double* slipRate1, double* slipRate2,
-		  double* slip1, double* slip2, double* state, double* strength,
+		  double* slip, double* slip1, double* slip2, double* state, double* strength,
 		  int numSides, int numBndGP) {
-	  e_interoperability.initializeIO(mu, slipRate1, slipRate2, slip1, slip2, state, strength,
+	  e_interoperability.initializeIO(mu, slipRate1, slipRate2, slip, slip1, slip2, state, strength,
 			  numSides, numBndGP);
   }
 
@@ -617,7 +617,7 @@ void seissol::Interoperability::enableCheckPointing( double i_checkPointInterval
 
 void seissol::Interoperability::initializeIO(
 		  double* mu, double* slipRate1, double* slipRate2,
-		  double* slip1, double* slip2, double* state, double* strength,
+		  double* slip, double* slip1, double* slip2, double* state, double* strength,
 		  int numSides, int numBndGP)
 {
 	  // Initialize checkpointing
@@ -626,7 +626,7 @@ void seissol::Interoperability::initializeIO(
 	  int faultTimeStep;
 	  bool hasCheckpoint = seissol::SeisSol::main.checkPointManager().init(reinterpret_cast<real*>(m_dofs),
 				  m_numberOfCopyInteriorCells * NUMBER_OF_ALIGNED_DOFS,
-				  mu, slipRate1, slipRate2, slip1, slip2,
+				  mu, slipRate1, slipRate2, slip, slip1, slip2,
 				  state, strength, numSides, numBndGP,
 				  currentTime, waveFieldTimeStep, faultTimeStep);
 	  if (hasCheckpoint) {

src/Solver/Interoperability.h
@@ -260,7 +260,7 @@ class seissol::Interoperability {
 		   const char *i_checkPointFilename, const char* i_checkPointBackend );
 
    void initializeIO(double* mu, double* slipRate1, double* slipRate2,
-			  double* slip1, double* slip2, double* state, double* strength,
+			  double* slip, double* slip1, double* slip2, double* state, double* strength,
 			  int numSides, int numBndGP);
 
    /**

src/Solver/f_ftoc_bind_interoperability.f90
@@ -169,7 +169,7 @@ module f_ftoc_bind_interoperability
       character(kind=c_char), dimension(*), intent(in) :: i_checkPointBackend
     end subroutine
 
-    subroutine c_interoperability_initializeIO( i_mu, i_slipRate1, i_slipRate2, i_slip1, i_slip2, i_state, i_strength, i_numSides, i_numBndGP ) &
+    subroutine c_interoperability_initializeIO( i_mu, i_slipRate1, i_slipRate2, i_slip, i_slip1, i_slip2, i_state, i_strength, i_numSides, i_numBndGP ) &
         bind( C, name='c_interoperability_initializeIO' )
       use iso_c_binding
       implicit none
@@ -177,6 +177,7 @@ module f_ftoc_bind_interoperability
       real(kind=c_double), dimension(*), intent(in) :: i_mu
       real(kind=c_double), dimension(*), intent(in) :: i_slipRate1
       real(kind=c_double), dimension(*), intent(in) :: i_slipRate2
+      real(kind=c_double), dimension(*), intent(in) :: i_slip
       real(kind=c_double), dimension(*), intent(in) :: i_slip1
       real(kind=c_double), dimension(*), intent(in) :: i_slip2
       real(kind=c_double), dimension(*), intent(in) :: i_state