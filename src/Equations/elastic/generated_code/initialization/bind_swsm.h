7723d4676406620552faca4d7cc0e60977a5c7e3
Alexander Heinecke <alexander.heinecke@intel.com>
2015-11-21 13:49:52 -0800

    additional update of kernels, using the latest version of the generation scripts as well

--- src/Equations/elastic/generated_code/initialization/bind_swsm.h
+++ src/Equations/elastic/generated_code/initialization/bind_swsm.h
@@ -29,7 +29,7 @@
 // @author Alexander Breuer (breuer AT mytum.de, http://www5.in.tum.de/wiki/index.php/Dipl.-Math._Alexander_Breuer)
 // @author Alexander Heinecke (alexander.heinecke AT mytum.de, http://www5.in.tum.de/wiki/index.php/Alexander_Heinecke,_M.Sc.,_M.Sc._with_honors)
 // 
-// @date 2015-11-21 13:21:20.794939
+// @date 2015-11-21 13:47:09.084899
 // 
 // @section LICENSE
 // Copyright (c) 2012-2015, SeisSol Group
--- src/Equations/elastic/generated_code/matrix_kernels/sparse_snoarch.h
+++ src/Equations/elastic/generated_code/matrix_kernels/sparse_snoarch.h
@@ -29,7 +29,7 @@
 // @author Alexander Breuer (breuer AT mytum.de, http://www5.in.tum.de/wiki/index.php/Dipl.-Math._Alexander_Breuer)
 // @author Alexander Heinecke (alexander.heinecke AT mytum.de, http://www5.in.tum.de/wiki/index.php/Alexander_Heinecke,_M.Sc.,_M.Sc._with_honors)
 // 
-// @date 2015-11-21 13:20:18.630069
+// @date 2015-11-21 13:46:08.046934
 // 
 // @section LICENSE
 // Copyright (c) 2012-2015, SeisSol Group
