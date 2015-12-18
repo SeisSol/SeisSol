d56a80d0b9bd35fdcceb8fdff045e434359527a4
Carsten Uphoff <uphoff@in.tum.de>
2015-12-18 12:55:12 +0100

    Fixed build errors in elastic version.

--- src/Equations/elastic/generated_code/init.h
+++ src/Equations/elastic/generated_code/init.h
@@ -7,21 +7,10 @@
 namespace seissol {
   namespace model {
     namespace AminusT {
-      unsigned const reals = NUMBER_OF_QUANTITIES*NUMBER_OF_QUANTITIES;
       int (* const index)(unsigned, unsigned) = &colMjrIndex<NUMBER_OF_QUANTITIES>;
     }
     namespace AplusT {
-      unsigned const reals = NUMBER_OF_QUANTITIES*NUMBER_OF_QUANTITIES;
       int (* const index)(unsigned, unsigned) = &colMjrIndex<NUMBER_OF_QUANTITIES>;
-    }
-    namespace AstarT {
-      unsigned const reals = STAR_NNZ;
-    }
-    namespace BstarT {
-      unsigned const reals = STAR_NNZ;
-    }
-    namespace CstarT {
-      unsigned const reals = STAR_NNZ;
     }
   }
 }
---
+++ src/Equations/elastic/generated_code/sizes.h
@@ -0,0 +1,25 @@
+#ifndef GENERATED_SIZES_H_
+#define GENERATED_SIZES_H_
+
+#include <generated_code/initialization/bind.h>
+
+namespace seissol {
+  namespace model {
+    namespace AminusT {
+      unsigned const reals = NUMBER_OF_QUANTITIES*NUMBER_OF_QUANTITIES;
+    }
+    namespace AplusT {
+      unsigned const reals = NUMBER_OF_QUANTITIES*NUMBER_OF_QUANTITIES;
+    }
+    namespace AstarT {
+      unsigned const reals = STAR_NNZ;
+    }
+    namespace BstarT {
+      unsigned const reals = STAR_NNZ;
+    }
+    namespace CstarT {
+      unsigned const reals = STAR_NNZ;
+    }
+  }
+}
+#endif
