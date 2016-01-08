052797d64c89faaf03d864a8edb028a55f1cd058
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2016-01-08 12:18:17 +0100

    a few modification in dxf -> stl converter (use of argparse, solid name)

--- preprocessing/meshing/GocadRelatedScripts/convertDxf2Stl.py
+++ preprocessing/meshing/GocadRelatedScripts/convertDxf2Stl.py
@@ -2,25 +2,19 @@
 #Convert dxf file (example GOCAD) to stl file (for SimModeler)
 import numpy as np
 import sys
-
-UtmProjection = False
-
-if UtmProjection==True:
-   print "projecting the nodes coordinates"
-   import mpl_toolkits.basemap.pyproj as pyproj
-   UTM46N=pyproj.Proj("+init=EPSG:32646")
-else:
-   print "no projection carried out"
+import os
 
 ###########READ DXF FUNCTIONS
 def RW_SECTION():
 	for i in range(0,2):
 		fid.readline()
-	fout.write("solid AssimpScene\n")
+	basename = os.path.basename(args.dxf_filename)
+	fout.write("solid %s\n" %(basename))
 
 def RW_POINT():
 	for i in range(0,8):
 		fid.readline()
+
 def RW_3DFACE():
 	fid.readline()
 	fid.readline()
@@ -35,16 +29,17 @@ def RW_3DFACE():
 	fout.write('facet normal %e %e %e\n' %tuple(normal/norm))
 	fout.write('outer loop\n')
 	for i in range(0,3):
-		if UtmProjection==True:
-                   xy=UTM46N(x[i,0]/1e4,x[i,1]/1e4)
-                   fout.write('vertex %.10e %.10e %.10e\n' %(xy[0],xy[1],x[i,2]))
+		if args.proj!='':
+                   xy=myproj(x[i,0],x[i,1])
+                   fout.write('vertex %.10e %.10e %.10e\n' %(xy[0],xy[1],1e3*x[i,2]))
 		else:
 		   fout.write('vertex %.10e %.10e %.10e\n' %tuple(x[i,:]))
 	fout.write("endloop\n")
 	fout.write("endfacet\n")
 
 def RW_ENDSEC():
-	fout.write("endsolid AssimpScene\n")
+	basename = os.path.basename(args.dxf_filename)
+	fout.write("endsolid %s\n" %(basename))
 	fout.close()
 	fid.close()
 	exit()
@@ -52,21 +47,28 @@ def RW_ENDSEC():
 options = {"SECTION": RW_SECTION, "POINT" : RW_POINT, "3DFACE": RW_3DFACE, "ENDSEC": RW_ENDSEC}
 #########################
 
-narg = len(sys.argv)
-if (narg <2) |  (narg>4):
-	print("usage (1): python convertDxf2Stl.py input.dxf output.stl")
-	print("usage (2): python convertDxf2Stl.py input.dxf")
-	print("usage (2): then outputfn = input.stl")
-	print("UtmProjection (bool) : hardcoded")
-	exit()
-elif narg==2:
-   fid=open(sys.argv[1])
-   fnout = sys.argv[1][0:-4]+'.stl'
-   fout=open(fnout,"w")
-elif narg==3:
-   fid=open(sys.argv[1])
-   fout=open(sys.argv[2],"w")
+# parsing python arguments
+import argparse
+parser = argparse.ArgumentParser(description='Convert dxf file to stl')
+parser.add_argument('dxf_filename', help='dxf filename')
+parser.add_argument('stl_filename', nargs='?',help='stl filname (if not used, stl_filename = dfxbasename.stl)',default='')
+parser.add_argument('--proj', nargs=1, metavar=('projname'), default = (''), help='name of the projection (ex EPSG:32646 (UTM46N)) if a projection is considered')
 
+args = parser.parse_args()
+
+if args.stl_filename == '':
+   args.stl_filename = args.dxf_filename[0:-4]+'.stl'
+
+fid = open(args.dxf_filename)
+fout = open(args.stl_filename,'w')
+
+if args.proj!='':
+   print "projecting the nodes coordinates"
+   import mpl_toolkits.basemap.pyproj as pyproj
+   sProj = "+init=%s" %args.proj[0]
+   myproj=pyproj.Proj(sProj)
+else:
+   print "no projection carried out"
 
 while fid.readline():  #0
 	sId = fid.readline().strip() #eg 3DFACE
