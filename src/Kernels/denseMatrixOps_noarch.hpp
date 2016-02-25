4e35900a909074eb503d9a29da2ccd57331d3e3d
Carsten Uphoff <uphoff@in.tum.de>
2016-02-25 18:13:03 +0100

    Fixed compile error for dnoarch

--- src/Kernels/denseMatrixOps_noarch.hpp
+++ src/Kernels/denseMatrixOps_noarch.hpp
@@ -39,7 +39,7 @@
  **/
 
 #define DMO_INCREMENT 1
-#define DMO_BROADCAST(IN, OUT) real OUT = IN;
+#define DMO_BROADCAST(IN, OUT) real OUT = *IN;
 #define DMO_STREAM(IN, OUT) *OUT = *IN;
 #define DMO_SXTYP(S, X, Y) *Y += S * *X;
 #define DMO_XYMST(S, X, Y, Z) *Z = (*X-*Y) * S;
