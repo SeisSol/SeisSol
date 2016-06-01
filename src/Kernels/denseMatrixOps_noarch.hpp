f219869a45dcb5088955af49c285d41f67854290
Carsten Uphoff <uphoff@in.tum.de>
2016-06-01 17:55:37 +0200

    Never use macros.

--- src/Kernels/denseMatrixOps_noarch.hpp
+++ src/Kernels/denseMatrixOps_noarch.hpp
@@ -41,8 +41,8 @@
 #include <Kernels/precision.hpp>
 
 #define DMO_INCREMENT 1
-#define DMO_BROADCAST(IN, OUT) real OUT = *IN;
-#define DMO_STREAM(IN, OUT) *OUT = *IN;
-#define DMO_SXTYP(S, X, Y) *Y += S * *X;
-#define DMO_XYMST(S, X, Y, Z) *Z = (*X-*Y) * S;
-#define DMO_XYMSTZP(S, X, Y, Z) *Z += (*X-*Y) * S;
+#define DMO_BROADCAST(IN, OUT) real OUT = *(IN);
+#define DMO_STREAM(IN, OUT) *(OUT) = *(IN);
+#define DMO_SXTYP(S, X, Y) *(Y) += (S) * *(X);
+#define DMO_XYMST(S, X, Y, Z) *(Z) = (*(X)-*(Y)) * (S);
+#define DMO_XYMSTZP(S, X, Y, Z) *(Z) += (*(X)-*(Y)) * (S);
