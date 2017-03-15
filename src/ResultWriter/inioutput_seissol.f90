1a38c98602f37a12b239ba094368750f7ff4f8cf
Sebastian Rettenberger <rettenbs@in.tum.de>
2017-03-15 15:24:04 +0100

    Add null char to filename

--- src/ResultWriter/inioutput_seissol.f90
+++ src/ResultWriter/inioutput_seissol.f90
@@ -211,7 +211,7 @@ CONTAINS
         i_outputMask= outputMaskInt,         &
         i_outputRegionBounds = io%OutputRegionBounds, &
         freeSurfaceInterval = io%SurfaceOutputInterval, &
-        freeSurfaceFilename = trim(io%OutputFile) )
+        freeSurfaceFilename = trim(io%OutputFile) // c_null_char )
 
     ! Initialize the fault Xdmf Writer
     IF(DISC%DynRup%OutputPointType.EQ.4.OR.DISC%DynRup%OutputPointType.EQ.5) THEN
