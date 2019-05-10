d795865569997b22c222d7bada71359111a0c03f
Carsten Uphoff <uphoff@in.tum.de>
2019-05-10 20:20:04 +0200

    Rewrote memlayout guess and renamed files

--- site_scons/memlayout.py
+++ site_scons/memlayout.py
@@ -39,21 +39,69 @@
 #
 
 import os
+import arch
+import re
+
+class Candidate(object):
+  IMPORTANCE = {'precision': 1, 'equations': 2, 'order': 3, 'pe': 4, 'multipleSimulations': 5}
+
+  def __init__(self, atts):
+    self.atts = atts
+
+  def score(self, reqs):
+    sc = 0
+    for key,val in reqs.items():
+      if key in self.atts and val == self.atts[key]:
+        sc += 2**self.IMPORTANCE[key]
+    return sc
+
+  def __repr__(self):
+    return repr(self.atts)
+
+def findCandidates():
+  archs = arch.getArchitectures()
+  pes = [arch.getCpu(a) for a in archs]
+
+  path = os.path.join('auto_tuning', 'config')
+  candidates = dict()
+  for c in os.listdir(path):
+    name, ext = os.path.splitext(c)
+    atts = dict()
+    for att in name.split('_'):
+      ms = re.match('ms([0-9]+)', att)
+      O = re.match('O([0-9]+)', att)
+      if ms:
+        atts['multipleSimulations'] = int(ms.group(1))
+      elif O:
+        atts['order'] = int(O.group(1))
+      elif att.lower() in ['s', 'd']:
+        atts['precision'] = att.lower()
+      elif att.lower() in pes:
+        atts['pe'] = att.lower()
+      else:
+        atts['equations'] = att
+    candidates[c] = Candidate(atts)
+  return candidates
 
 def guessMemoryLayout(env):    
   path = os.path.join('auto_tuning', 'config')
-  
-  # TODO: enable tuned memory layouts after auto-tuning for flux decomposition
-  name = 'NOT-TUNED'
-  #if env['equations'] == 'viscoelastic':
-  #  name = '{0}_{1}_O{2}_M{3}.xml'.format(env['equations'], env['arch'], env['order'], env['numberOfMechanisms'])
-  if env['equations'] == 'elastic':
-    name = '{0}_{1}_O{2}.xml'.format(env['equations'], env['arch'], env['order'])
-  
-  candidate = os.path.join(path, name)
-  if os.path.exists(candidate):
-    return candidate
-  else:
+
+  # from least to most
+  importance = ['precision', 'equations', 'order', 'pe', 'multipleSimulations']
+  values = {
+    'precision': env['arch'][0].lower(),
+    'equations': env['equations'].lower(),
+    'order': int(env['order']),
+    'pe': arch.getCpu(env['arch']),
+    'multipleSimulations': int(env['multipleSimulations'])
+  }
+
+  candidates = findCandidates()
+  bestFit = max(candidates.keys(), key=lambda key: candidates[key].score(values))
+  bestScore = candidates[bestFit].score(values)
+
+  if bestScore == 0:
     print('WARNING: No suitable memory layout found. (Will fall back to all dense.)')
-    return os.path.join(path, 'dense.xml')
-  
+    bestFit = 'dense.xml'
+  print('Using memory layout {}'.format(bestFit))
+  return os.path.join(path, bestFit)
