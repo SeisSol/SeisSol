2cbe39373c187e4c2330e834bea6e5fc83ec1251
Sebastian Wolf <wolf.sebastian@in.tum.de>
2019-08-20 12:56:05 +0200

    Save the current state with including the neighboring coordinate system

--- src/Initializer/CellLocalMatrices.cpp
+++ src/Initializer/CellLocalMatrices.cpp
@@ -101,13 +101,16 @@ void seissol::initializers::initializeCellLocalMatrices( MeshReader const&      
     real ATData[tensor::star::size(0)];
     real BTData[tensor::star::size(1)];
     real CTData[tensor::star::size(2)];
+    real DTData[tensor::star::size(0)];
     auto AT = init::star::view<0>::create(ATData);
     auto BT = init::star::view<0>::create(BTData);
     auto CT = init::star::view<0>::create(CTData);
+    auto DT = init::star::view<0>::create(DTData);
 
     real TData[seissol::tensor::T::size()];
     real TinvData[seissol::tensor::Tinv::size()];
-    real NData[6*6];
+    real NLocalData[6*6];
+    real NNeighborData[6*6];
     auto T = init::T::view::create(TData);
     auto Tinv = init::Tinv::view::create(TinvData);
 
@@ -152,18 +155,64 @@ void seissol::initializers::initializeCellLocalMatrices( MeshReader const&      
         VrtxCoords normal;
         VrtxCoords tangent1;
         VrtxCoords tangent2;
+        VrtxCoords neighborNormal;
+        VrtxCoords neighborTangent1;
+        VrtxCoords neighborTangent2;
         MeshTools::normalAndTangents(elements[meshId], side, vertices, normal, tangent1, tangent2);
+        int neighborElementID = elements[meshId].neighbors[side];
+        int neighborSideID = elements[meshId].neighborSides[side];
+        MeshTools::normalAndTangents(elements[neighborElementID], neighborSideID, vertices, neighborNormal, neighborTangent1, neighborTangent2);
         double surface = MeshTools::surface(normal);
         MeshTools::normalize(normal, normal);
         MeshTools::normalize(tangent1, tangent1);
         MeshTools::normalize(tangent2, tangent2);
-       
-        seissol::model::getBondMatrix(normal, tangent1, tangent2, NData);
+        MeshTools::normalize(neighborNormal, neighborNormal);
+        MeshTools::normalize(neighborTangent1, neighborTangent1);
+        MeshTools::normalize(neighborTangent2, neighborTangent2);
+
+        seissol::model::getBondMatrix(normal, tangent1, tangent2, NLocalData);
+        seissol::model::getBondMatrix(neighborNormal, neighborTangent1, neighborTangent2, NNeighborData);
         seissol::model::Material rotatedLocalMaterial;
         seissol::model::Material rotatedNeighborMaterial;
-        material[cell].local.getRotatedMaterialCoefficients(NData, rotatedLocalMaterial);
-        material[cell].neighbor[side].getRotatedMaterialCoefficients(NData, rotatedNeighborMaterial);
-        
+#ifdef USE_ANISOTROPIC
+        material[cell].local.getRotatedMaterialCoefficients(NLocalData, rotatedLocalMaterial);
+        material[cell].neighbor[side].getRotatedMaterialCoefficients(NLocalData, rotatedNeighborMaterial);
+        if(cell == 0 && side == 0) {
+          printf(" local material = \n");
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", rotatedLocalMaterial.c11, rotatedLocalMaterial.c12, rotatedLocalMaterial.c13, rotatedLocalMaterial.c14, rotatedLocalMaterial.c15, rotatedLocalMaterial.c16); 
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", rotatedLocalMaterial.c12, rotatedLocalMaterial.c22, rotatedLocalMaterial.c23, rotatedLocalMaterial.c24, rotatedLocalMaterial.c25, rotatedLocalMaterial.c26); 
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", rotatedLocalMaterial.c13, rotatedLocalMaterial.c23, rotatedLocalMaterial.c33, rotatedLocalMaterial.c34, rotatedLocalMaterial.c35, rotatedLocalMaterial.c36); 
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", rotatedLocalMaterial.c14, rotatedLocalMaterial.c24, rotatedLocalMaterial.c34, rotatedLocalMaterial.c44, rotatedLocalMaterial.c45, rotatedLocalMaterial.c46); 
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", rotatedLocalMaterial.c15, rotatedLocalMaterial.c25, rotatedLocalMaterial.c35, rotatedLocalMaterial.c45, rotatedLocalMaterial.c55, rotatedLocalMaterial.c56); 
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", rotatedLocalMaterial.c16, rotatedLocalMaterial.c26, rotatedLocalMaterial.c36, rotatedLocalMaterial.c46, rotatedLocalMaterial.c56, rotatedLocalMaterial.c66); 
+          printf("\n");
+          printf(" neighbor material = \n");
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", rotatedNeighborMaterial.c11, rotatedNeighborMaterial.c12, rotatedNeighborMaterial.c13, rotatedNeighborMaterial.c14, rotatedNeighborMaterial.c15, rotatedNeighborMaterial.c16); 
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", rotatedNeighborMaterial.c12, rotatedNeighborMaterial.c22, rotatedNeighborMaterial.c23, rotatedNeighborMaterial.c24, rotatedNeighborMaterial.c25, rotatedNeighborMaterial.c26); 
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", rotatedNeighborMaterial.c13, rotatedNeighborMaterial.c23, rotatedNeighborMaterial.c33, rotatedNeighborMaterial.c34, rotatedNeighborMaterial.c35, rotatedNeighborMaterial.c36); 
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", rotatedNeighborMaterial.c14, rotatedNeighborMaterial.c24, rotatedNeighborMaterial.c34, rotatedNeighborMaterial.c44, rotatedNeighborMaterial.c45, rotatedNeighborMaterial.c46); 
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", rotatedNeighborMaterial.c15, rotatedNeighborMaterial.c25, rotatedNeighborMaterial.c35, rotatedNeighborMaterial.c45, rotatedNeighborMaterial.c55, rotatedNeighborMaterial.c56); 
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", rotatedNeighborMaterial.c16, rotatedNeighborMaterial.c26, rotatedNeighborMaterial.c36, rotatedNeighborMaterial.c46, rotatedNeighborMaterial.c56, rotatedNeighborMaterial.c66); 
+          printf("\n");
+        }
+#else
+        rotatedLocalMaterial = material[cell].local;
+        rotatedNeighborMaterial = material[cell].neighbor[side];
+        if(cell == 0 && side == 0) {
+          printf(" local material = \n");
+          auto l2m = rotatedLocalMaterial.lambda + 2*rotatedLocalMaterial.mu;
+          auto l = rotatedLocalMaterial.lambda;
+          auto m = rotatedLocalMaterial.mu;
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", l2m, l, l, 0.0, 0.0, 0.0); 
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", l, l2m, l, 0.0, 0.0, 0.0); 
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", l, l, l2m, 0.0, 0.0, 0.0); 
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", 0.0, 0.0, 0.0, m, 0.0, 0.0); 
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", 0.0, 0.0, 0.0, 0.0, m, 0.0); 
+          printf("% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e,% 2.2e\n", 0.0, 0.0, 0.0, 0.0, 0.0, m); 
+          printf("\n");
+        }
+
+#endif
         seissol::model::getTransposedGodunovState(  rotatedLocalMaterial,
                                                     rotatedNeighborMaterial, 
                                                     cellInformation[cell].faceTypes[side],
@@ -174,12 +223,13 @@ void seissol::initializers::initializeCellLocalMatrices( MeshReader const&      
           printf(" G = \n");
           for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
-              printf("%f ", QgodLocal(i,j));
+              printf("% 2.2e ", QgodLocal(i,j));
             }
             printf("\n");
           }
           printf("\n");
         }
+
         // Calculate transposed T instead
         seissol::model::getFaceRotationMatrix(normal, tangent1, tangent2, T, Tinv);
 
@@ -187,22 +237,25 @@ void seissol::initializers::initializeCellLocalMatrices( MeshReader const&      
         // must be subtracted.
         real fluxScale = -2.0 * surface / (6.0 * volume);
 
+        seissol::model::getTransposedCoefficientMatrix( rotatedLocalMaterial, 0, DT );
+
         kernel::computeFluxSolverLocal localKrnl;
         localKrnl.fluxScale = fluxScale;
         localKrnl.AplusT = localIntegration[cell].nApNm1[side];
         localKrnl.QgodLocal = QgodLocalData;
         localKrnl.T = TData;
         localKrnl.Tinv = TinvData;
-        localKrnl.star(0) = ATData;
+        localKrnl.star(0) = DTData;
         localKrnl.execute();
         
+
         kernel::computeFluxSolverNeighbor neighKrnl;
         neighKrnl.fluxScale = fluxScale;
         neighKrnl.AminusT = neighboringIntegration[cell].nAmNm1[side];
         neighKrnl.QgodNeighbor = QgodNeighborData;
         neighKrnl.T = TData;
         neighKrnl.Tinv = TinvData;
-        neighKrnl.star(0) = ATData;
+        neighKrnl.star(0) = DTData;
         neighKrnl.execute();
       }
 
