7ae5b39d4238abb8fa7a509bbf4669b075e598a7
Sebastian Wolf <wolf.sebastian@in.tum.de>
2019-10-16 10:22:39 +0200

    Forgot a file

--- src/Physics/InitialField.h
+++ src/Physics/InitialField.h
@@ -44,6 +44,29 @@ namespace seissol {
       std::array<std::complex<real>, NUMBER_OF_QUANTITIES>  m_lambdaA;
       real                                                  m_phase;
     };
+
+    class AnisotropicPlanarwave : public InitialField {
+    public:
+      //! Choose phase in [0, 2*pi]
+      AnisotropicPlanarwave(real phase = 0.0);
+
+      void evaluate(  double time,
+                      std::vector<std::array<double, 3>> const& points,
+                      yateto::DenseTensorView<2,real,unsigned>& dofsQP ) const;
+    private:
+      std::complex<real> m_eigenvectors1[NUMBER_OF_QUANTITIES*NUMBER_OF_QUANTITIES];
+      std::complex<real> m_eigenvectors2[NUMBER_OF_QUANTITIES*NUMBER_OF_QUANTITIES];
+      std::complex<real> m_eigenvectors3[NUMBER_OF_QUANTITIES*NUMBER_OF_QUANTITIES];
+      int const                                             m_setVar;
+      std::vector<std::complex<real>>                       m_ampField;
+      std::array<double, 3>                                 m_kVec1;
+      std::array<double, 3>                                 m_kVec2;
+      std::array<double, 3>                                 m_kVec3;
+      std::array<std::complex<real>, NUMBER_OF_QUANTITIES>  m_lambda1;
+      std::array<std::complex<real>, NUMBER_OF_QUANTITIES>  m_lambda2;
+      std::array<std::complex<real>, NUMBER_OF_QUANTITIES>  m_lambda3;
+      real                                                  m_phase;
+    };
   }
 }
 
