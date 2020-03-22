b7c5373dda18ca0ec5b53364a12d5e37c1ca458a
ravil <ravil.dorozhinskii@tum.de>
2020-03-22 20:18:36 +0100

    Added gmsh without gui support to seissol-utils

--- spack_support/packages/seissol-utils/package.py
+++ spack_support/packages/seissol-utils/package.py
@@ -7,6 +7,7 @@ from spack import *
 import sys
 import os
 import glob
+import shutil 
 
 class SeissolUtils(Package):
     """Seissol - A scientific software for the numerical simulation of seismic wave phenomena 
@@ -43,21 +44,24 @@ class SeissolUtils(Package):
     depends_on('pumgen')
     depends_on('glm@0.9.7.1')
     depends_on('proj@4.9.2')
+    depends_on("gmsh+hdf5+metis+openmp") 
 
-    depends_on("paraview", when="+paraview")    
+    depends_on("paraview+hdf5", when="+paraview") 
     depends_on('scons@3.0.1:3.1.2', when='+building_tools')
     
     def install(self, spec, prefix):
-        install_tree(self.stage.source_path.cookbook, prefix.cookbook)
+        install_tree("cookbook", prefix.cookbook)
+
+        if "+benchmarks" in spec:
+            install_tree("benchmarks", prefix.benchmarks)
 
         source_directory = self.stage.source_path
+
         # gmsh2gambit
         build_gmsh2gambit_dir = join_path(source_directory, 'preprocessing/meshing/gmsh2gambit')
         with working_dir(build_gmsh2gambit_dir, create=False):
             scons()
-            files = glob.glob(join_path(build_gmsh2gambit_dir, 'build/bin', '*'))
-            for file in files:
-                install(file, prefix.bin)
+            install_tree(os.path.join(build_gmsh2gambit_dir, "build", "bin"), prefix.gmsh2gambit)
 
         """
         #TODO: find a way how to install gambit2netcdf
@@ -73,15 +77,28 @@ class SeissolUtils(Package):
         build_cube_dir = join_path(source_directory, 'preprocessing/meshing/cube_c')
         with working_dir(build_cube_dir, create=False):
             scons()
-            files = glob.glob(join_path(build_cube_dir, 'build/bin', '*'))
-            for file in files:
-                install(file, prefix.bin)
-
+            install_tree(os.path.join(build_cube_dir, "build", "bin"), prefix.cube_c)
 
         # rconv for point sources
         build_rconv_dir = join_path(source_directory, 'preprocessing/science/rconv')
         with working_dir(build_rconv_dir, create=False):
             scons('compiler={}'.format(*self.compiler.cc_names))
-            files = glob.glob(join_path(build_rconv_dir, 'build/bin', '*'))
-            for file in files:
-                install(file, prefix.bin)
+            install_tree(os.path.join(build_rconv_dir, "build", "bin"), prefix.rconv)
+    
+    
+    def setup_run_environment(self, env):
+        dependencies = self.spec.dependencies_dict()
+        bins = [self.spec.prefix.gmsh2gambit, 
+                self.spec.prefix.cube_c, 
+                self.spec.prefix.rconv,
+                dependencies["pumgen"].spec.prefix.bin
+                dependencies["gmsh"].spec.prefix.bin]
+
+        if "+paraview" in self.spec:
+            bins.append(dependencies["paraview"].spec.previx.bin)
+
+        env.prepend_path('PATH', ":".join(bins))
+
+        env.set('COOKBOOK', self.spec.prefix.cookbook)
+        if "+benchmarks" in self.spec:
+            env.set('BENCHMARKS', self.spec.prefix.benchmarks)
