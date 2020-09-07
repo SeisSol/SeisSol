949dde328f4450e0ee6ba773a74c7d63d85d4160
ga96mog <ga96mog@mytum.de>
2020-09-07 17:03:30 +0200

    added Brent's Method (not done)

--- src/DynamicRupture/DR_solver_rate_and_state.h
+++ src/DynamicRupture/DR_solver_rate_and_state.h
@@ -390,6 +390,93 @@ protected:
     }
   }
 
+  /*
+ * If the function did not converge it returns false
+   * From Upoffc git Zero.ccp
+ */
+  bool IterativelyInvertSR_Brent(unsigned int face, int nSRupdates, std::array<real, numOfPointsPadded> &LocSR,
+                            std::array<real, numOfPointsPadded> &LocSV, std::array<real, numOfPointsPadded> &n_stress,
+                            std::array<real, numOfPointsPadded> &sh_stress, double invZ,  std::array<real, numOfPointsPadded> &SRtest ){
+
+    double a;
+    double b;
+    std::function<double(double)> F;
+    double tol;
+
+    double eps = std::numeric_limits<double>::epsilon();
+    double Fa = F(a);
+    double Fb = F(b);
+    assert(std::copysign(Fa, Fb) != Fa); // Fa and Fb have different signs
+    double c = a;
+    double Fc = Fa;
+    double d = b - a;
+    double e = d;
+    while (Fb != 0.0) {
+      if (std::copysign(Fb, Fc) == Fb) {
+        c = a;
+        Fc = Fa;
+        d = b - a;
+        e = d;
+      }
+      if (std::fabs(Fc) < std::fabs(Fb)) {
+        a = b;
+        b = c;
+        c = a;
+        Fa = Fb;
+        Fb = Fc;
+        Fc = Fa;
+      }
+      // Convergence test
+      double xm = 0.5 * (c - b);
+      double tol1 = 2.0 * eps * std::fabs(b) + 0.5 * tol;
+      if (std::fabs(xm) <= tol1 || Fb == 0.0) {
+        break;
+      }
+      if (std::fabs(e) < tol1 || std::fabs(Fa) <= std::fabs(Fb)) {
+        // bisection
+        d = xm;
+        e = d;
+      } else {
+        double s = Fb / Fa;
+        double p, q;
+        if (a != c) {
+          // linear interpolation
+          q = Fa / Fc;
+          double r = Fb / Fc;
+          p = s * (2.0 * xm * q * (q - r) - (b - a) * (r - 1.0));
+          q = (q - 1.0) * (r - 1.0) * (s - 1.0);
+        } else {
+          // inverse quadratic interpolation
+          p = 2.0 * xm * s;
+          q = 1.0 - s;
+        }
+        if (p > 0) {
+          q = -q;
+        } else {
+          p = -p;
+        }
+        if (2.0 * p < 3.0 * xm * q - std::fabs(tol1 * q) && p < std::fabs(0.5 * e * q)) {
+          e = d;
+          d = p / q;
+        } else {
+          // bisection
+          d = xm;
+          e = d;
+        }
+      }
+      a = b;
+      Fa = Fb;
+      if (std::fabs(d) > tol1) {
+        b += d;
+      } else {
+        b += std::copysign(tol1, xm);
+      }
+      Fb = F(b);
+    }
+    return b;
+
+  }
+
   //output time when shear stress is equal to the dynamic stress after rupture arrived
   //currently only for linear slip weakening
   void outputDynamicStress(
