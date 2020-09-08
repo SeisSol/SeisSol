2a25f5ba194f62237c8c78c41840f1be859b66f6
ga96mog <ga96mog@mytum.de>
2020-09-08 19:04:11 +0200

    theoretical implementation of Brent's method

--- src/DynamicRupture/DR_solver_rate_and_state.h
+++ src/DynamicRupture/DR_solver_rate_and_state.h
@@ -398,83 +398,100 @@ protected:
                             std::array<real, numOfPointsPadded> &LocSV, std::array<real, numOfPointsPadded> &n_stress,
                             std::array<real, numOfPointsPadded> &sh_stress, double invZ,  std::array<real, numOfPointsPadded> &SRtest ){
 
-    double a;
-    double b;
-    std::function<double(double)> F;
-    double tol;
+    double a = -impAndEta[face].eta_s;
+    double b  = impAndEta[face].eta_s;
+    std::function<double(double, int)> F;
+    double tol = 1e-8;
 
-    double eps = std::numeric_limits<double>::epsilon();
-    double Fa = F(a);
-    double Fb = F(b);
-    assert(std::copysign(Fa, Fb) != Fa); // Fa and Fb have different signs
-    double c = a;
-    double Fc = Fa;
-    double d = b - a;
-    double e = d;
-    while (Fb != 0.0) {
-      if (std::copysign(Fb, Fc) == Fb) {
-        c = a;
-        Fc = Fa;
-        d = b - a;
-        e = d;
-      }
-      if (std::fabs(Fc) < std::fabs(Fb)) {
-        a = b;
-        b = c;
-        c = a;
-        Fa = Fb;
-        Fb = Fc;
-        Fc = Fa;
-      }
-      // Convergence test
-      double xm = 0.5 * (c - b);
-      double tol1 = 2.0 * eps * std::fabs(b) + 0.5 * tol;
-      if (std::fabs(xm) <= tol1 || Fb == 0.0) {
-        break;
-      }
-      if (std::fabs(e) < tol1 || std::fabs(Fa) <= std::fabs(Fb)) {
-        // bisection
-        d = xm;
-        e = d;
-      } else {
-        double s = Fb / Fa;
-        double p, q;
-        if (a != c) {
-          // linear interpolation
-          q = Fa / Fc;
-          double r = Fb / Fc;
-          p = s * (2.0 * xm * q * (q - r) - (b - a) * (r - 1.0));
-          q = (q - 1.0) * (r - 1.0) * (s - 1.0);
-        } else {
-          // inverse quadratic interpolation
-          p = 2.0 * xm * s;
-          q = 1.0 - s;
-        }
-        if (p > 0) {
-          q = -q;
-        } else {
-          p = -p;
-        }
-        if (2.0 * p < 3.0 * xm * q - std::fabs(tol1 * q) && p < std::fabs(0.5 * e * q)) {
+    double RS_fw = Mu_w;
+    double *RS_srW = RS_srW_array[face];
+    double *RS_a = RS_a_array[face];
+    double *RS_sl0 = RS_sl0_array[face];
+    double RS_sr0_ = RS_sr0;
+
+    F = [invZ, &sh_stress, n_stress, SRtest, RS_a, LocSV, RS_sr0_](double SR, int iBndGP){
+      double tmp   =  0.5 / RS_sr0_ *exp(LocSV[iBndGP]/RS_a[iBndGP]);
+      double tmp2  = tmp*SRtest[iBndGP];
+      double mu_f  = RS_a[iBndGP] * log(tmp2+sqrt(seissol::dr::aux::power(tmp2,2)+1.0));
+      return -invZ * (fabs(n_stress[iBndGP])*mu_f-sh_stress[iBndGP])-SRtest[iBndGP];
+    };
+
+    //TODO: padded?
+    for(int iBndGP = 0; iBndGP < numberOfPoints; iBndGP++){
+
+      double eps = std::numeric_limits<double>::epsilon();
+      double Fa = F(a, iBndGP);
+      double Fb = F(b, iBndGP);
+      assert(std::copysign(Fa, Fb) != Fa); // Fa and Fb have different signs
+      double c = a;
+      double Fc = Fa;
+      double d = b - a;
+      double e = d;
+      while (Fb != 0.0) {
+        if (std::copysign(Fb, Fc) == Fb) {
+          c = a;
+          Fc = Fa;
+          d = b - a;
           e = d;
-          d = p / q;
-        } else {
+        }
+        if (std::fabs(Fc) < std::fabs(Fb)) {
+          a = b;
+          b = c;
+          c = a;
+          Fa = Fb;
+          Fb = Fc;
+          Fc = Fa;
+        }
+        // Convergence test
+        double xm = 0.5 * (c - b);
+        double tol1 = 2.0 * eps * std::fabs(b) + 0.5 * tol;
+        if (std::fabs(xm) <= tol1 || Fb == 0.0) {
+          break;
+        }
+        if (std::fabs(e) < tol1 || std::fabs(Fa) <= std::fabs(Fb)) {
           // bisection
           d = xm;
           e = d;
+        } else {
+          double s = Fb / Fa;
+          double p, q;
+          if (a != c) {
+            // linear interpolation
+            q = Fa / Fc;
+            double r = Fb / Fc;
+            p = s * (2.0 * xm * q * (q - r) - (b - a) * (r - 1.0));
+            q = (q - 1.0) * (r - 1.0) * (s - 1.0);
+          } else {
+            // inverse quadratic interpolation
+            p = 2.0 * xm * s;
+            q = 1.0 - s;
+          }
+          if (p > 0) {
+            q = -q;
+          } else {
+            p = -p;
+          }
+          if (2.0 * p < 3.0 * xm * q - std::fabs(tol1 * q) && p < std::fabs(0.5 * e * q)) {
+            e = d;
+            d = p / q;
+          } else {
+            // bisection
+            d = xm;
+            e = d;
+          }
         }
+        a = b;
+        Fa = Fb;
+        if (std::fabs(d) > tol1) {
+          b += d;
+        } else {
+          b += std::copysign(tol1, xm);
+        }
+        Fb = F(b, iBndGP);
       }
-      a = b;
-      Fa = Fb;
-      if (std::fabs(d) > tol1) {
-        b += d;
-      } else {
-        b += std::copysign(tol1, xm);
-      }
-      Fb = F(b);
     }
-    return b;
 
+    return b;
   }
 
   //output time when shear stress is equal to the dynamic stress after rupture arrived
