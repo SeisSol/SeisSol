efee09c47dca9f47006c2f93aa7876f6b7ab1370
ravil-mobile <ravil.mobile.com@gmail.com>
2020-10-17 23:31:59 +0200

    Updated seissol-env/package.py
    * PYTHONPATH and PATH to pspamm

--- spack_support/packages/seissol-env/package.py
+++ spack_support/packages/seissol-env/package.py
@@ -101,5 +101,5 @@ class SeissolEnv(BundlePackage):
         env.prepend_path('PYTHONPATH', ":".join(filter(None, pythonpath)))
         
         # add pspamm while loading seissol-env
-        env.prepend_path('PYTHONPATH', self.spec.dependencies_dict()['pspamm'].spec.prefix)
-        env.prepend_path('PATH', self.spec.dependencies_dict()['pspamm'].spec.prefix)
+        env.prepend_path('PYTHONPATH', self.spec.dependencies_dict()['pspamm'].spec.prefix.pspamm)
+        env.prepend_path('PATH', self.spec.dependencies_dict()['pspamm'].spec.prefix.pspamm)
