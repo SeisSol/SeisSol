4290e276326a10c9c69316cfe29e3b5402f5a623
Sebastian Wolf <wolf.sebastian@in.tum.de>
2020-11-10 11:59:32 +0100

    Only consider one mode for the convergence test

--- src/Physics/InitialField.cpp
+++ src/Physics/InitialField.cpp
@@ -15,8 +15,8 @@
 extern seissol::Interoperability e_interoperability;
 
 seissol::physics::Planarwave::Planarwave(const CellMaterialData& materialData, double phase, std::array<double, 3> kVec)
-  : m_varField{3, NUMBER_OF_QUANTITIES-2},
-    m_ampField{100.0, 100.0},
+  : m_varField{0, },
+    m_ampField{100.0},
     m_phase(phase),
     m_kVec(kVec)
 {
