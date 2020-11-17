863dc14df1b7756d4f794bc9b492138a590913dd
ravil <ravil.dorozhinskii@tum.de>
2020-11-17 21:49:56 +0100

    Added data structs for batch recording

---
+++ src/Initializer/BatchRecorders/BatchTable.hpp
@@ -0,0 +1,80 @@
+#ifndef SEISSOL_POINTERSTABLE_HPP
+#define SEISSOL_POINTERSTABLE_HPP
+
+#include <device.h>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+using namespace device;
+
+namespace seissol {
+namespace initializers {
+namespace recording {
+
+class BatchPointers {
+public:
+  BatchPointers(const std::vector<real *> &collectedPointers)
+      : pointers(collectedPointers), devicePtrs(nullptr) {
+    if (!pointers.empty()) {
+      devicePtrs = (real **)device.api->allocGlobMem(pointers.size() * sizeof(real *));
+      device.api->copyTo(devicePtrs, pointers.data(), pointers.size() * sizeof(real *));
+    }
+  }
+
+  explicit BatchPointers(const BatchPointers &other)
+      : pointers(other.pointers), devicePtrs(nullptr) {
+    if (!pointers.empty()) {
+      if (other.devicePtrs != nullptr) {
+        devicePtrs = (real**)device.api->allocGlobMem(other.pointers.size() * sizeof(real *));
+        device.api->copyBetween(devicePtrs, other.devicePtrs,
+                                other.pointers.size() * sizeof(real *));
+      }
+    }
+  }
+
+  BatchPointers &operator=(const BatchPointers &Other) = delete;
+
+  virtual ~BatchPointers() {
+    if (m_DevicePtrs != nullptr) {
+      device.api->freeMem(devicePtrs);
+      devicePtrs = nullptr;
+    }
+  }
+
+  real **getPointers() {
+    return mevicePtrs;
+  }
+  index_t getSize() {
+    return pointers.size();
+  }
+
+private:
+  std::vector<real *> pointers{};
+  real **devicePtrs{};
+  DeviceInstance &device = DeviceInstance::getInstance();
+};
+} // namespace recording
+} // namespace initializers
+} // namespace seissol
+
+
+namespace seissol {
+namespace initializers {
+namespace recording {
+struct BatchTable {
+
+public:
+  BatchTable() {
+    for (auto &item : content)
+      item = nullptr;
+  }
+
+  std::array<BatchPointers *, *VariableID::Count> content{};
+};
+} // namespace recording
+} // namespace initializers
+} // namespace seissol
+
+#endif // SEISSOL_POINTERSTABLE_HPP
---
+++ src/Initializer/BatchRecorders/ConditionalTable.hpp
@@ -0,0 +1,11 @@
+#ifndef SEISSOL_CONDITIONALTABLE_HPP
+#define SEISSOL_CONDITIONALTABLE_HPP
+
+#include "Initializer/recording/Condition.h"
+#include "Initializer/recording/ConditionalKey.h"
+#include "Initializer/recording/EncodedConstants.h"
+#include "Initializer/recording/BatchTable.h"
+
+using ConditionalTableT = std::unordered_map<ConditionalKey, BatchTable, ConditionalHash<ConditionalKey>>;
+
+#endif //SEISSOL_CONDITIONALTABLE_HPP
