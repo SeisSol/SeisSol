73226666f88ab0ecfafcda2bfec50e8d11c7c6f5
VMPW <viktoria.pauw@lrz.de>
2021-03-11 15:45:13 +0100

    Update test_singularity_cpu.sh

--- test_singularity_cpu.sh
+++ test_singularity_cpu.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 ​
 set -euo pipefail
-​
-export HOST_ARCH=hsw
-export NUM_PROC=3
-
 for build_type in Release Debug; do
     for equation in elastic viscoelastic2 anisotropic; do
         for precision in double single; do
             dirname="./SeisSol/build_${equation}_${precision}_${build_type}"
             mkdir -p $dirname && cd $dirname
-​
             if [ "$equation" = viscoelastic2 ]; then
                 mechanisms=3
             else
                 mechanisms=0
             fi
-​
             cmake .. -DNETCDF=OFF -DMETIS=ON -DCOMMTHREAD=ON -DASAGI=OFF -DHDF5=ON \
                 -DCMAKE_BUILD_TYPE=$build_type -DTESTING=ON -DLOG_LEVEL=warning \
                 -DLOG_LEVEL_MASTER=info -DHOST_ARCH=$HOST_ARCH -DPRECISION=$precision \
@@ -27,5 +21,4 @@ for build_type in Release Debug; do
         done
     done
 done
-​
 set +u
