fdc9a9e7593debb4bbd616635d79ef0d3ce75fd4
VMPW <viktoria.pauw@lrz.de>
2021-03-12 12:48:06 +0100

    Update test_singularity_cpu.sh

--- test_singularity_cpu.sh
+++ test_singularity_cpu.sh
@@ -5,20 +5,14 @@ export CTEST_OUTPUT_ON_FAILURE=1
 export OMPI_MCA_rmaps_base_oversubscribe=1
 export equation=elastic
 export build_type=Release
-for precision in double single; do
-            dirname="./SeisSol/build_${equation}_${precision}_${build_type}"
-            mkdir -p $dirname && cd $dirname
-            if [ "$equation" = viscoelastic2 ]; then
-                mechanisms=3
-            else
-                mechanisms=0
-            fi
-            cmake .. -DNETCDF=OFF -DMETIS=ON -DCOMMTHREAD=ON -DASAGI=OFF -DHDF5=ON \
+export precision=double
+dirname="./SeisSol/build_${equation}_${precision}_${build_type}"
+mkdir -p $dirname && cd $dirname
+cmake .. -DNETCDF=OFF -DMETIS=ON -DCOMMTHREAD=ON -DASAGI=OFF -DHDF5=ON \
                 -DCMAKE_BUILD_TYPE=$build_type -DTESTING=ON -DLOG_LEVEL=warning \
                 -DLOG_LEVEL_MASTER=info -DHOST_ARCH=$HOST_ARCH -DPRECISION=$precision \
                 -DEQUATIONS=$equation -DNUMBER_OF_MECHANISMS=$mechanisms
-            make -j $NUM_PROC
-            make test
-            cd ../..
-done
+make -j $NUM_PROC
+make test
+cd ../..
 set +u
