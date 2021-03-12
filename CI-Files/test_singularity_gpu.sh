6d21a928533e249fa3def6e4cf8980bdd396ffbc
VMPW <viktoria.pauw@lrz.de>
2021-03-12 11:58:38 +0100

    Create test_singularity_gpu.sh

---
+++ CI-Files/test_singularity_gpu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# install gemmforge
+pip3 install git+https://github.com/ravil-mobile/gemmforge.git
+
+# prepare
+cd ./tests/convergence_elastic && ./generateCubes.sh && cd ../..
+
+for precision in double single; do
+    mkdir -p ./SeisSol/_build_${precision} && cd ./SeisSol/_build_${precision}
+    cp ../../tests/convergence_elastic/* .
+
+    # compile
+    cmake .. -DCMAKE_BUILD_TYPE=Release -DDEVICE_ARCH=${GPU_VENDOR} -DDEVICE_SUB_ARCH=${GPU_MODEL}  -DHOST_ARCH=${HOST} -DPRECISION=${precision}
+    make -j
+
+    # run proxy
+    ./SeisSol_proxy_Release_?${GPU_VENDOR}_?_elastic 100000 50 all
+    
+    # run convergence test
+    mpirun -n 1 ./SeisSol_Release_?nvidia_?_elastic ./parameters.par
+    cd ../..
+done
+set +u
