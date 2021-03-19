f35a3b63a91e0164e7f28a1e58618cdc267e9037
Lukas Krenz <lukas@krenz.land>
2021-03-19 15:47:45 +0100

    Address review.

--- cmake/process_users_input.cmake
+++ cmake/process_users_input.cmake
@@ -67,7 +67,7 @@ set(MEMORY_LAYOUT "auto" CACHE FILEPATH "A file with a specific memory layout or
 
 option(COMMTHREAD "Use a communication thread for MPI+MP." OFF)
 
-option(NUMA_AWARE_PINNING "Use libnuma to pin threads to correct NUMA nodes" OFF)
+option(NUMA_AWARE_PINNING "Use libnuma to pin threads to correct NUMA nodes" ON)
 
 
 set(LOG_LEVEL "warning" CACHE STRING "Log level for the code")
