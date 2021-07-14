f56a0091e1b2a43865675305fb9cee4392ad8aca
VMPW <viktoria.pauw@lrz.de>
2021-07-14 17:34:42 +0200

    Update test_singularity_gpu.sh

--- CI-Files/test_singularity_gpu.sh
+++ CI-Files/test_singularity_gpu.sh
@@ -20,6 +20,7 @@ for precision in double single; do
     ./SeisSol_proxy_Release_?${GPU_VENDOR}_?_elastic 100000 50 all
     
     # run convergence test
+    mkdir -p output
     mpirun -n 1 ./SeisSol_Release_?nvidia_?_elastic ./parameters.par
     cd ../..
 done
