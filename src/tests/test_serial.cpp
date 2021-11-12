288e4eb84127fb526dc4846c7832b9368e50802e
Lukas Krenz <lukas@krenz.land>
2021-11-12 16:11:59 +0100

    Add initial doctest

---
+++ src/tests/test_serial.cpp
@@ -0,0 +1,14 @@
+#include "doctest.h"
+
+TEST_CASE("Testing works") {
+  const auto x = 10;
+  REQUIRE(x == 10);
+  REQUIRE(x == 9);
+}
+
+TEST_CASE("Testing works 2") {
+  const auto x = 10;
+  REQUIRE(x == 10);
+  REQUIRE(x == 9);
+}
+
