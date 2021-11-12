a92013d80f325ca36fcad7a5652a199f228c21cb
ravil-mobile <ravil.mobile.com@gmail.com>
2021-11-12 09:45:09 +0100

    Dr/tpv5 ci (#414)
    * Added TPV5 to CI
    
    * A fix in ctof_bind_interoperability
    
    -      !call c_f_pointer( i_averaged_Slip,   l_averaged_Slip)
    +      call c_f_pointer( i_averaged_Slip,   l_averaged_Slip)
    
    * Add python script to compute l2 errors of variables on the fault
    
    * Add the script to the gitlab pipeline
    
    Co-authored-by: ravil <ravil.dorozhinskii@tum.de>
    Co-authored-by: Sebastian Wolf <wolf.sebastian@in.tum.de>

--- src/DynamicRupture/Parameters.h
+++ src/DynamicRupture/Parameters.h
@@ -47,36 +47,38 @@ struct seissol::dr::DRParameters {
   void setAllInputParam(const YAML::Node& Params) {
     using namespace initializers;
 
-    const YAML::Node& DrParams = Params["dynamicrupture"];
-    OutputPointType = getParamIfExists(DrParams, "outputpointtype", 3);
-    SlipRateOutputType = getParamIfExists(DrParams, "sliprateoutputtype", 1);
-    FrictionLawType = getParamIfExists(DrParams, "fl", 0);
-    BackgroundType = getParamIfExists(DrParams, "backgroundtype", 0);
-    IsRfOutputOn = getParamIfExists(DrParams, "rf_output_on", false);
-    IsDsOutputOn = getParamIfExists(DrParams, "ds_output_on", false);
-    IsMagnitudeOutputOn = getParamIfExists(DrParams, "magnitude_output_on", false);
-    IsEnergyRateOutputOn = getParamIfExists(DrParams, "energy_rate_output_on", false);
-    IsGpWiseOutput = getParamIfExists(DrParams, "gpwise", false);
-    IsTermalPressureOn = getParamIfExists(DrParams, "thermalpress", false);
-    BackgroundType = getParamIfExists(DrParams, "energy_rate_printtimeinterval", 1);
-    IsInstaHealingOn = getParamIfExists(DrParams, "inst_healing", false);
-    t_0 = getParamIfExists(DrParams, "t_0", 0.0);
-    rs_f0 = getParamIfExists(DrParams, "rs_f0", 0.0);
-    rs_a = getParamIfExists(DrParams, "rs_a", 0.0);
-    rs_b = getParamIfExists(DrParams, "rs_b", 0.0);
-    rs_sr0 = getParamIfExists(DrParams, "rs_sr0", 0.0);
-    mu_w = getParamIfExists(DrParams, "mu_w", 0.0);
+    if (Params["dynamicrupture"]) {
+      const YAML::Node& DrParams = Params["dynamicrupture"];
+      OutputPointType = getParamIfExists(DrParams, "outputpointtype", 3);
+      SlipRateOutputType = getParamIfExists(DrParams, "sliprateoutputtype", 1);
+      FrictionLawType = getParamIfExists(DrParams, "fl", 0);
+      BackgroundType = getParamIfExists(DrParams, "backgroundtype", 0);
+      IsRfOutputOn = getParamIfExists(DrParams, "rf_output_on", false);
+      IsDsOutputOn = getParamIfExists(DrParams, "ds_output_on", false);
+      IsMagnitudeOutputOn = getParamIfExists(DrParams, "magnitude_output_on", false);
+      IsEnergyRateOutputOn = getParamIfExists(DrParams, "energy_rate_output_on", false);
+      IsGpWiseOutput = getParamIfExists(DrParams, "gpwise", false);
+      IsTermalPressureOn = getParamIfExists(DrParams, "thermalpress", false);
+      BackgroundType = getParamIfExists(DrParams, "energy_rate_printtimeinterval", 1);
+      IsInstaHealingOn = getParamIfExists(DrParams, "inst_healing", false);
+      t_0 = getParamIfExists(DrParams, "t_0", 0.0);
+      rs_f0 = getParamIfExists(DrParams, "rs_f0", 0.0);
+      rs_a = getParamIfExists(DrParams, "rs_a", 0.0);
+      rs_b = getParamIfExists(DrParams, "rs_b", 0.0);
+      rs_sr0 = getParamIfExists(DrParams, "rs_sr0", 0.0);
+      mu_w = getParamIfExists(DrParams, "mu_w", 0.0);
 
-    // if ThermalPress == true
-    alpha_th = getParamIfExists(DrParams, "alpha_th", 0.0);
-    rho_c = getParamIfExists(DrParams, "rho_c", 0.0);
-    TP_lambda = getParamIfExists(DrParams, "tp_lambda", 0.0);
-    IniTemp = getParamIfExists(DrParams, "initemp", 0.0);
-    IniPressure = getParamIfExists(DrParams, "inipressure", 0.0);
+      // if ThermalPress == true
+      alpha_th = getParamIfExists(DrParams, "alpha_th", 0.0);
+      rho_c = getParamIfExists(DrParams, "rho_c", 0.0);
+      TP_lambda = getParamIfExists(DrParams, "tp_lambda", 0.0);
+      IniTemp = getParamIfExists(DrParams, "initemp", 0.0);
+      IniPressure = getParamIfExists(DrParams, "inipressure", 0.0);
 
-    // Prakash-Clifton regularization parameter
-    v_star = getParamIfExists(DrParams, "v_star", 0.0);
-    prakash_length = getParamIfExists(DrParams, "L", 0.0);
+      // Prakash-Clifton regularization parameter
+      v_star = getParamIfExists(DrParams, "v_star", 0.0);
+      prakash_length = getParamIfExists(DrParams, "L", 0.0);
+    }
   }
 };
 
