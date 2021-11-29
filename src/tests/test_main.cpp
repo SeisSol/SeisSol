002ebda86fd03b2bf4c24c710564cb811855cf0b
Lukas Krenz <lukas@krenz.land>
2021-11-29 18:50:56 +0100

    Cleanup test directory
    Delete ancient unused tests

--- src/tests/test_main.cpp
+++ src/tests/test_main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv) {
 
   context.applyCommandLine(argc, argv);
 
-  int returnValue = context.run(); // run
+  int returnValue = context.run();
 
   seissol::MPI::mpi.finalize();
 
