a5718f0cfb89937074837bd0a3c88f2eea9b80e3
Sebastian Wolf <wolf.sebastian@in.tum.de>
2022-01-20 17:17:41 +0100

    Fix single precision TPV104

--- src/DynamicRupture/Misc.cpp
+++ src/DynamicRupture/Misc.cpp
@@ -4,12 +4,12 @@
 
 namespace seissol::dr::misc {
 template <>
-real power<8>(real base) {
-  real baseSquared = base * base;
-  real baseToTheFour = baseSquared * baseSquared;
-  real baseToTheEight = baseToTheFour * baseToTheFour;
+double power<8>(double base) {
+  double baseSquared = base * base;
+  double baseToTheFour = baseSquared * baseSquared;
+  double baseToTheEight = baseToTheFour * baseToTheFour;
   return baseToTheEight;
 }
 real magnitude(real x, real y) { return std::sqrt(x * x + y * y); }
-real asinh(real x) { return std::log(x + std::sqrt(misc::power<2>(x) + 1.0)); }
+double asinh(double x) { return std::log(x + std::sqrt(x * x + 1.0)); }
 } // namespace seissol::dr::misc
--- src/DynamicRupture/Misc.h
+++ src/DynamicRupture/Misc.h
@@ -28,19 +28,36 @@ constexpr F forEach(TupleT&& tuple, F&& functor) {
       std::forward<F>(functor),
       std::make_index_sequence<std::tuple_size<std::remove_reference_t<TupleT>>::value>{});
 }
-
+/**
+ * Compute base^exp
+ * Note: precision has to be double, otherwise we would loose too much precision.
+ * @param base
+ * @return
+ */
 template <size_t exp>
-real power(real base) {
-  real result = 1.0;
+double power(double base) {
+  double result = 1.0;
   for (size_t i = 0; i < exp; ++i) {
     result *= base;
   }
   return result;
 }
 
+/**
+ * Computes the magnitude of the vector (x, y)
+ * @param x First component of the vector
+ * @param y Second component of the vector
+ * @return magnitude of the vector
+ */
 real magnitude(real x, real y);
 
-real asinh(real x);
+/**
+ * Computes the arcus sinus hyperbolicus of x.
+ * Note: precision has to be double, otherwise we would loose too much precision.
+ * @param x
+ * @return asinh(x)
+ */
+double asinh(double x);
 } // namespace seissol::dr::misc
 
 #endif // SEISSOL_DR_MISC_H
