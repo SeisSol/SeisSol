94207d6cf5247b975469b5a65238c419d07815ba
Thomas-Ulrich <thomas.ulrich@geophysik.uni-muenchen.de>
2022-02-22 17:01:24 +0100

    remove loop over lts leaves

--- src/ResultWriter/EnergyOutput.cpp
+++ src/ResultWriter/EnergyOutput.cpp
@@ -9,23 +9,19 @@ void seissol::writer::printPlasticMoment(MeshReader const& i_meshReader,
   std::vector<Element> const& elements = i_meshReader.getElements();
   std::vector<Vertex> const& vertices = i_meshReader.getVertices();
 
-  seissol::initializers::LayerMask ghostMask(Ghost);
-  for (auto it = i_ltsTree->beginLeaf(ghostMask); it != i_ltsTree->endLeaf(); ++it) {
-    real(*pstrain)[7 * NUMBER_OF_ALIGNED_BASIS_FUNCTIONS] = it->var(i_lts->pstrain);
 #ifdef _OPENMP
 #pragma omp parallel for schedule(static) reduction(+ : plasticMomentLocal)
 #endif
-    for (std::size_t meshId = 0; meshId < elements.size(); ++meshId) {
-      real* pstrainCell = i_ltsLut->lookup(i_lts->pstrain, meshId);
-      double volume = MeshTools::volume(elements[meshId], vertices);
-      CellMaterialData& material = i_ltsLut->lookup(i_lts->material, meshId);
+  for (std::size_t meshId = 0; meshId < elements.size(); ++meshId) {
+    real* pstrainCell = i_ltsLut->lookup(i_lts->pstrain, meshId);
+    double volume = MeshTools::volume(elements[meshId], vertices);
+    CellMaterialData& material = i_ltsLut->lookup(i_lts->material, meshId);
 #ifdef USE_ANISOTROPIC
-      double mu = (material.local.c44 + material.local.c55 + material.local.c66) / 3.0;
+    double mu = (material.local.c44 + material.local.c55 + material.local.c66) / 3.0;
 #else
-      double mu = material.local.mu;
+    double mu = material.local.mu;
 #endif
-      plasticMomentLocal += mu * volume * pstrainCell[6 * NUMBER_OF_ALIGNED_BASIS_FUNCTIONS];
-    }
+    plasticMomentLocal += mu * volume * pstrainCell[6 * NUMBER_OF_ALIGNED_BASIS_FUNCTIONS];
   }
 
   int rank;
