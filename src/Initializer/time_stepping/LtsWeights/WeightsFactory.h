c9cb4779796f7566f8459577e4caac1a6316bb33
ThrudPrimrose <yakup.paradox@gmail.com>
2022-04-26 17:12:29 +0200

    fix

--- src/Initializer/time_stepping/LtsWeights/WeightsFactory.h
+++ src/Initializer/time_stepping/LtsWeights/WeightsFactory.h
@@ -27,7 +27,7 @@ enum class EdgeWeightModelTypes : int {
   ApproximateCommunicationWithMessageCount,
   ApproximateCommunicationWithPenalizeBetweenClusters,
   ReverseApproximateCommunication,
-  ReverseApproximateCommunicationWithPenalizeBetweenClusters
+  ReverseApproximateCommunicationWithPenalizeBetweenClusters,
   Count
 };
 
--- src/Initializer/time_stepping/LtsWeights/WeightsModels.cpp
+++ src/Initializer/time_stepping/LtsWeights/WeightsModels.cpp
@@ -352,7 +352,7 @@ void ReverseApproximateCommunication::setEdgeWeights(
 }
 
 
-void ApproximateCommunicationWithPenalizeBetweenClusters ::setEdgeWeights(
+void ApproximateCommunicationWithPenalizeBetweenClusters::setEdgeWeights(
     std::tuple<const std::vector<idx_t>&, const std::vector<idx_t>&, const std::vector<idx_t>&>&
         graph) {
   const std::vector<int>& clusterIds = ltsWeights.getClusterIds();
@@ -386,7 +386,7 @@ void ApproximateCommunicationWithPenalizeBetweenClusters ::setEdgeWeights(
   EdgeWeightModel::setEdgeWeights(graph, factor);
 }
 
-void ApproximateCommunicationWithPenalizeBetweenClusters ::setEdgeWeights(
+void ApproximateCommunicationWithPenalizeBetweenClusters::setEdgeWeights(
     std::tuple<const std::vector<idx_t>&, const std::vector<idx_t>&, const std::vector<idx_t>&>&
         graph) {
   const std::vector<int>& clusterIds = ltsWeights.getClusterIds();
