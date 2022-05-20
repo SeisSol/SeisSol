b7ca3bcb695f80e956259ea9c2d3ec0d7b4cd91f
thrud primrose <budanaz.yakup@gmail.com>
2022-05-20 20:08:54 +0200

    rm build folder from the repository...

--- build/CMakeFiles/FindOpenMP/OpenMPTryFlag.f90
+++
@@ -1,8 +0,0 @@
-
-      program test
-      implicit none
-      include 'omp_lib.h'
-  !$  integer :: n
-      n = omp_get_num_threads()
-      end program test
-  
--- build/CMakeFiles/SeisSol-proxy-core.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libSeisSol-proxy-core.a"
-)
