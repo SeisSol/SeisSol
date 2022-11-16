dd2a5089d40b52a7f030669ddaf29aa851ce0f11
Sebastian Wolf <wolf.sebastian@in.tum.de>
2022-11-16 15:22:48 +0100

    Fix faultreceiver comparison

--- postprocessing/validation/compare-receivers.py
+++ postprocessing/validation/compare-receivers.py
@@ -8,6 +8,221 @@ import os
 import re
 import glob
 
+
+def velocity_norm(receiver):
+    assert (
+        "v1" in receiver.columns
+        and "v2" in receiver.columns
+        and "v3" in receiver.columns
+    )
+    return np.sqrt(receiver["v1"] ** 2 + receiver["v2"] ** 2 + receiver["v3"] ** 2)
+
+
+def stress_norm(receiver):
+    return np.sqrt(
+        receiver["xx"] ** 2
+        + receiver["yy"] ** 2
+        + receiver["zz"] ** 2
+        + receiver["xy"] ** 2
+        + receiver["yz"] ** 2
+        + receiver["xz"] ** 2
+    )
+
+
+def absolute_slip_norm(receiver):
+    return np.sqrt(receiver["ASl"] ** 2)
+
+
+def dynstress_norm(receiver):
+    return np.sqrt(receiver["DS"] ** 2)
+
+
+def friction_coefficient_norm(receiver):
+    return np.sqrt(receiver["Mud"] ** 2)
+
+
+def peak_sliprate_norm(receiver):
+    return np.sqrt(receiver["PSR"] ** 2)
+
+
+def pressure_norm(receiver):
+    return np.sqrt(receiver["P_f"] ** 2)
+
+
+def traction_norm(receiver):
+    return np.sqrt(receiver["Td0"] ** 2 + receiver["Ts0"] ** 2 + receiver["Pn0"] ** 2)
+
+
+def rupture_time_norm(receiver):
+    return np.sqrt(receiver["RT"] ** 2)
+
+
+def sliprate_norm(receiver):
+    return np.sqrt(receiver["SRs"] ** 2 + receiver["SRd"] ** 2)
+
+
+def slip_norm(receiver):
+    return np.sqrt(receiver["Sls"] ** 2 + receiver["Sld"] ** 2)
+
+
+def statevariable_norm(receiver):
+    return np.sqrt(receiver["StV"] ** 2)
+
+
+def temperature_norm(receiver):
+    return np.sqrt(receiver["Tmp"] ** 2)
+
+
+def rupture_velocity_norm(receiver):
+    return np.sqrt(receiver["Vr"] ** 2)
+
+
+def normal_velocity_norm(receiver):
+    return np.sqrt(receiver["u_n"] ** 2)
+
+
+def integrate_in_time(time, samples):
+    return np.trapz(samples, x=time)
+
+
+def integrate_quantity_in_time(receiver, quantity):
+    quantity_to_norm = {
+        "absolute slip": absolute_slip_norm,
+        "friction coefficient": friction_coefficient_norm,
+        "peak sliprate": peak_sliprate_norm,
+        "traction": traction_norm,
+        "rupture time": rupture_time_norm,
+        "sliprate": sliprate_norm,
+        "slip": slip_norm,
+        "rupture velocity": rupture_velocity_norm,
+        "normal velocity": normal_velocity_norm,
+        "dynstress time": dynstress_norm,
+        "state variable": statevariable_norm,
+        "pressure": pressure_norm,
+        "temperature": temperature_norm,
+    }
+    return integrate_in_time(receiver["Time"], quantity_to_norm[quantity](receiver))
+
+
+def read_receiver(filename):
+    with open(filename) as receiver_file:
+        # find variable names
+        # We expect the header to look like the following
+        # TITLE = "title"
+        # VARIABLES = "variable 1", "variable 2", ..., "variable n"
+        # # x1	x coordinate of receiver
+        # # x2	y coordinate of receiver
+        # # x3	z coordinate of receiver
+        # # possibly more comments, starting with #
+        lines = receiver_file.readlines()
+        # remove the first 12 characters from the line ("VARIABLES = ")
+        variable_line = lines[1][12:].split(",")
+        variables = [s.strip().replace('"', "") for s in variable_line]
+        # find first row without comments
+        first_row = 2
+        while lines[first_row][0] == "#":
+            first_row += 1
+    receiver = pd.read_csv(filename, header=None, skiprows=first_row, sep="\s+")
+
+    def replace(x, y, l):
+        if x in l:
+            x_index = l.index(x)
+            l[x_index] = y
+        return l
+
+    # Recently, we changed the receiver variables from u,v,w to v1,v2,v3. If the receiver is stored in legacy format, we adapt it
+    variables = replace("u", "v1", variables)
+    variables = replace("v", "v2", variables)
+    variables = replace("w", "v3", variables)
+    receiver.columns = variables
+    return receiver
+
+
+def receiver_diff(args, i):
+    sim_files = glob.glob(f"{args.output}/{args.prefix}-receiver-{i:05d}-*.dat")
+    ref_files = glob.glob(f"{args.output_ref}/{args.prefix}-receiver-{i:05d}-*.dat")
+    assert len(sim_files) == 1
+    assert len(ref_files) == 1
+    sim_receiver = read_receiver(sim_files[0])
+    ref_receiver = read_receiver(ref_files[0])
+    # both receivers must have the same time axis
+    assert np.max(np.abs(sim_receiver["Time"] - ref_receiver["Time"])) < 1e-14
+    time = sim_receiver["Time"]
+    difference = sim_receiver - ref_receiver
+
+    ref_velocity_norm = integrate_in_time(time, velocity_norm(ref_receiver))
+    diff_velocity_norm = integrate_in_time(time, velocity_norm(difference))
+
+    ref_stress_norm = integrate_in_time(time, stress_norm(ref_receiver))
+    diff_stress_norm = integrate_in_time(time, stress_norm(difference))
+
+    return (
+        diff_velocity_norm / ref_velocity_norm,
+        diff_stress_norm / ref_stress_norm,
+    )
+
+
+def faultreceiver_diff(args, i, quantities):
+    sim_files = glob.glob(f"{args.output}/{args.prefix}-faultreceiver-{i:05d}-*.dat")
+    ref_files = glob.glob(
+        f"{args.output_ref}/{args.prefix}-faultreceiver-{i:05d}-*.dat"
+    )
+    assert len(sim_files) == 1
+    assert len(ref_files) == 1
+    sim_receiver = read_receiver(sim_files[0]).iloc[1:]
+    ref_receiver = read_receiver(ref_files[0])
+
+    sim_receiver.reset_index(drop=True, inplace=True)
+
+    # both receivers must have the same time axis
+    assert np.max(np.abs(sim_receiver["Time"] - ref_receiver["Time"])) < 1e-14
+    time = sim_receiver["Time"]
+    difference = sim_receiver - ref_receiver
+    # We still want to use the same time and not the difference in time steps.
+    difference["Time"] = ref_receiver["Time"]
+
+    errors = pd.DataFrame(index=[i], columns=quantities)
+
+    possible_quantity_names = [
+        "absolute slip",
+        "friction coefficient",
+        "peak sliprate",
+        "traction",
+        "rupture time",
+        "sliprate",
+        "slip",
+        "rupture velocity",
+        "normal velocity",
+        "dynstress time",
+        "state variable",
+        "pressure",
+        "temperature",
+    ]
+
+    for quantity_name in possible_quantity_names:
+        if quantity_name in quantities:
+            ref_norm = integrate_quantity_in_time(ref_receiver, quantity_name)
+            diff_norm = integrate_quantity_in_time(difference, quantity_name)
+            errors.loc[i, quantity_name] = diff_norm / ref_norm
+
+    return errors
+
+
+def find_all_receivers(directory, prefix, faultreceiver=False):
+    if faultreceiver:
+        file_candidates = glob.glob(f"{directory}/{prefix}-faultreceiver-*-*.dat")
+    else:
+        file_candidates = glob.glob(f"{directory}/{prefix}-receiver-*-*.dat")
+    extract_id = re.compile(".+/\w+-\w+-(\d+)-\d+.dat")
+
+    receiver_ids = []
+    for fn in file_candidates:
+        extract_id_result = extract_id.search(fn)
+        if extract_id_result:
+            receiver_ids.append(int(extract_id_result.group(1)))
+    return np.array(sorted(receiver_ids))
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Compare two sets of receivers.")
     parser.add_argument("output", type=str)
@@ -16,208 +231,15 @@ if __name__ == "__main__":
     parser.add_argument(
         "--mode", type=str, default="rs", required=False, choices=["rs", "lsw", "tp"]
     )
-
-    parser.add_argument(
-        "--prefix", type=str, default='tpv13', required=False
-        )
+    parser.add_argument("--prefix", type=str, default="tpv", required=False)
     parser.add_argument(
         "--compare_fault_receivers", type=bool, default=True, required=False
-        )
+    )
     args = parser.parse_args()
 
-    def velocity_norm(receiver):
-        if (
-            "u" in receiver.columns
-            and "v" in receiver.columns
-            and "w" in receiver.columns
-        ):
-            return np.sqrt(receiver["u"] ** 2 + receiver["v"] ** 2 + receiver["w"] ** 2)
-        elif (
-            "v1" in receiver.columns
-            and "v2" in receiver.columns
-            and "v3" in receiver.columns
-        ):
-            return np.sqrt(
-                receiver["v1"] ** 2 + receiver["v2"] ** 2 + receiver["v3"] ** 2
-            )
-        else:
-            raise ValueError("Could not find velocities in off-fault receiver.")
-
-    def stress_norm(receiver):
-        return np.sqrt(
-            receiver["xx"] ** 2
-            + receiver["yy"] ** 2
-            + receiver["zz"] ** 2
-            + receiver["xy"] ** 2
-            + receiver["yz"] ** 2
-            + receiver["xz"] ** 2
-        )
-
-    def absolute_slip_norm(receiver):
-        return np.sqrt(receiver["ASl"] ** 2)
-
-    def dynstress_norm(receiver):
-        return np.sqrt(receiver["DS"] ** 2)
-
-    def friction_coefficient_norm(receiver):
-        return np.sqrt(receiver["Mud"] ** 2)
-
-    def peak_sliprate_norm(receiver):
-        return np.sqrt(receiver["PSR"] ** 2)
-
-    def pressure_norm(receiver):
-        return np.sqrt(receiver["P_f"] ** 2)
-
-    def traction_norm(receiver):
-        return np.sqrt(
-            receiver["Td0"] ** 2 + receiver["Ts0"] ** 2 + receiver["Pn0"] ** 2
-        )
-
-    def rupture_time_norm(receiver):
-        return np.sqrt(receiver["RT"] ** 2)
-
-    def sliprate_norm(receiver):
-        return np.sqrt(receiver["SRs"] ** 2 + receiver["SRd"] ** 2)
-
-    def slip_norm(receiver):
-        return np.sqrt(receiver["Sls"] ** 2 + receiver["Sld"] ** 2)
-
-    def statevariable_norm(receiver):
-        return np.sqrt(receiver["StV"] ** 2)
-
-    def temperature_norm(receiver):
-        return np.sqrt(receiver["Tmp"] ** 2)
-
-    def fault_velocity_norm(receiver):
-        return np.sqrt(receiver["Vr"] ** 2)
-
-    def normal_velocity_norm(receiver):
-        return np.sqrt(receiver["u_n"] ** 2)
-
-    def integrate_in_time(time, samples):
-        return np.trapz(samples, x=time)
-
-    def integrate_quantity_in_time(receiver, quantity):
-        quantity_to_norm = {
-            "absolute slip": absolute_slip_norm,
-            "friction coefficient": friction_coefficient_norm,
-            "peak sliprate": peak_sliprate_norm,
-            "traction": traction_norm,
-            "rupture time": rupture_time_norm,
-            "sliprate": sliprate_norm,
-            "slip": slip_norm,
-            "rupture velocity": rupture_velocity_norm,
-            "normal velocity": normal_velocity_norm,
-            "dynstress time": dynstress_norm,
-            "state variable": statevariable_norm,
-            "pressure": pressure_norm,
-            "temperature": temperature_norm,
-        }
-        return integrate_in_time(receiver["Time"], quantity_to_norm[quantity](receiver))
-
-    def read_receiver(filename):
-        with open(filename) as receiver_file:
-            # find variable names
-            # We expect the header to look like the following
-            # TITLE = "title"
-            # VARIABLES = "variable 1", "variable 2", ..., "variable n"
-            # # x1	x coordinate of receiver
-            # # x2	y coordinate of receiver
-            # # x3	z coordinate of receiver
-            # # possibly more comments, starting with #
-            lines = receiver_file.readlines()
-            # remove the first 12 characters from the line ("VARIABLES = ")
-            variable_line = lines[1][12:].split(",")
-            variables = [s.strip().replace('"', "") for s in variable_line]
-            # find first row without comments
-            first_row = 2
-            while lines[first_row][0] == "#":
-                first_row += 1
-        receiver = pd.read_csv(filename, header=None, skiprows=first_row, sep="\s+")
-        receiver.columns = variables
-        return receiver
-
-    def receiver_diff(args, i):
-        sim_files = glob.glob(f"{args.output}/{args.prefix}-receiver-{i:05d}-*.dat")
-        ref_files = glob.glob(
-            f"{args.output_ref}/{args.prefix}-receiver-{i:05d}-*.dat"
-        )
-        assert(len(sim_files) == 1)
-        assert(len(ref_files) == 1)
-        sim_receiver = read_receiver(sim_files[0])
-        ref_receiver = read_receiver(ref_files[0])
-        # both receivers must have the same time axis
-        assert np.max(np.abs(sim_receiver["Time"] - ref_receiver["Time"])) < 1e-14
-        time = sim_receiver["Time"]
-        difference = sim_receiver - ref_receiver
-
-        ref_velocity_norm = integrate_in_time(time, velocity_norm(ref_receiver))
-        diff_velocity_norm = integrate_in_time(time, velocity_norm(difference))
-
-        ref_stress_norm = integrate_in_time(time, stress_norm(ref_receiver))
-        diff_stress_norm = integrate_in_time(time, stress_norm(difference))
-
-        return (
-            diff_velocity_norm / ref_velocity_norm,
-            diff_stress_norm / ref_stress_norm,
-        )
-
-    def faultreceiver_diff(args, i, quantities):
-        ref_filename = f"{args.output_ref}/{args.prefix}-faultreceiver-{i:05d}-00000.dat"
-        ref_receiver = read_receiver(ref_filename)
-
-        sim_filename = f"{args.output}/{args.prefix}-faultreceiver-{i:05d}-00000.dat"
-        sim_receiver = read_receiver(sim_filename).iloc[1:]
-        sim_receiver.reset_index(drop=True, inplace=True)
-
-        # both receivers must have the same time axis
-        assert np.max(np.abs(sim_receiver["Time"] - ref_receiver["Time"])) < 1e-14
-        time = sim_receiver["Time"]
-        difference = sim_receiver - ref_receiver
-        # We still want to use the same time and not the difference in time steps.
-        difference["Time"] = ref_receiver["Time"]
-
-        errors = pd.DataFrame(index=[i], columns=quantities)
-
-        possible_quantity_names = [
-            "absolute slip",
-            "friction coefficient",
-            "peak sliprate",
-            "traction",
-            "rupture time",
-            "sliprate",
-            "slip",
-            "rupture velocity",
-            "normal velocity",
-            "dynstress time",
-            "state variable",
-            "pressure",
-            "temperature",
-        ]
-
-        for quantity_name in possible_quantity_names:
-            if quantity_name in quantities:
-                ref_norm = integrate_quantity_in_time(ref_receiver, quantity_name)
-                diff_norm = integrate_quantity_in_time(difference, quantity_name)
-                errors.loc[i, quantity_name] = diff_norm / ref_norm
-
-        return errors
-
-    def find_all_receivers(directory, prefix, faultreceiver=False):
-        if faultreceiver:
-            receiver_re = re.compile(f"{prefix}-faultreceiver-(\d+)-\d+\.dat")
-        else:
-            receiver_re = re.compile(f"{prefix}-receiver-(\d+)-\d+\.dat")
-        receiver_ids = []
-        for fn in os.listdir(directory):
-            match = receiver_re.match(fn)
-            if match:
-                receiver_ids.append(int(match.groups()[0]))
-        return np.array(sorted(receiver_ids))
-
     if args.compare_fault_receivers:
-        sim_faultreceiver_ids = find_all_receivers(args.output, True)
-        ref_faultreceiver_ids = find_all_receivers(args.output_ref, True)
+        sim_faultreceiver_ids = find_all_receivers(args.output, args.prefix, True)
+        ref_faultreceiver_ids = find_all_receivers(args.output_ref, args.prefix, True)
         faultreceiver_ids = np.intersect1d(sim_faultreceiver_ids, ref_faultreceiver_ids)
         # Make sure, we actually compare some faultreceivers
         assert len(faultreceiver_ids) == len(ref_faultreceiver_ids)
@@ -231,7 +253,6 @@ if __name__ == "__main__":
     receiver_ids = np.intersect1d(sim_receiver_ids, ref_receiver_ids)
     # Make sure, we actually compare some receivers
     assert len(receiver_ids) == len(ref_receiver_ids)
-
 
     receiver_errors = pd.DataFrame(index=receiver_ids, columns=["velocity", "stress"])
     for i in receiver_ids:
@@ -273,15 +294,18 @@ if __name__ == "__main__":
         faultreceiver_errors.loc[i, :] = local_errors.loc[i, :]
 
     print("")
-    print("Relative L2 error of the different quantities at the different faultreceivers")
+    print(
+        "Relative L2 error of the different quantities at the different faultreceivers"
+    )
     print(faultreceiver_errors)
 
     for q in faultreceiver_errors.columns:
         broken_faultreceivers = faultreceiver_errors.index[
             faultreceiver_errors[q] > args.epsilon
         ].tolist()
-        print(f"{q} exceeds relative error of {args.epsilon} at faultreceveivers {broken_faultreceivers}")
- 
+        print(
+            f"{q} exceeds relative error of {args.epsilon} at faultreceveivers {broken_faultreceivers}"
+        )
 
     if (receiver_errors > args.epsilon).any().any() or (
         faultreceiver_errors > args.epsilon
