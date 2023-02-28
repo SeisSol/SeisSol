26c2c4a2b0c101bd5bb75b2894e082adc0798a4c
Lukas Krenz <lukas@krenz.land>
2023-02-28 13:46:53 +0100

    Add caching for clustering

--- src/Initializer/time_stepping/LtsWeights/LtsWeights.h
+++ src/Initializer/time_stepping/LtsWeights/LtsWeights.h
@@ -8,17 +8,17 @@
  * @section LICENSE
  * Copyright (c) 2017 - 2020, SeisSol Group
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. Neither the name of the copyright holder nor the names of its
  *    contributors may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
@@ -42,10 +42,11 @@
 #ifndef INITIALIZER_TIMESTEPPING_LTSWEIGHTS_H_
 #define INITIALIZER_TIMESTEPPING_LTSWEIGHTS_H_
 
+#include <limits>
+#include <map>
 #include <optional>
 #include <string>
 #include <vector>
-#include <limits>
 #include <Initializer/time_stepping/LtsParameters.h>
 
 #ifndef PUML_PUML_H
@@ -107,6 +108,7 @@ protected:
   int getCluster(double timestep, double globalMinTimestep, double wiggleFactor, unsigned rate);
   int getBoundaryCondition(int const *boundaryCond, unsigned cell, unsigned face);
   std::vector<int> computeClusterIds(double curWiggleFactor);
+  void computeClusterIdsAndEnforceMaximumDifferenceCached(double curWiggleFactor);
   int enforceMaximumDifference();
   int enforceMaximumDifferenceLocal(int maxDifference = 1);
   std::vector<int> computeCostsPerTimestep();
@@ -130,6 +132,7 @@ protected:
   std::vector<int> m_clusterIds{};
   const LtsParameters* ltsParameters;
   double wiggleFactor = 1.0;
+  std::map<double, decltype(m_clusterIds)> clusteringCache; // Maps wiggle factor to clustering
   struct ComputeWiggleFactorResult {
     int numberOfClusters;
     double wiggleFactor;
