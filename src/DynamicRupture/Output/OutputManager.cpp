791333cea99567a938b246496c00827364d19159
ravil <ravil.dorozhinskii@tum.de>
2023-04-20 16:46:03 +0200

    some compilers still have std::filesystem in the experimental sub-directory in `std`

--- src/DynamicRupture/Output/OutputManager.cpp
+++ src/DynamicRupture/Output/OutputManager.cpp
@@ -1,8 +1,8 @@
+#include "Common/filesystem.h"
 #include "DynamicRupture/Output/OutputManager.hpp"
 #include "DynamicRupture/Output/ReceiverBasedOutput.hpp"
 #include "ResultWriter/common.hpp"
 #include "SeisSol.h"
-#include <filesystem>
 #include <fstream>
 #include <type_traits>
 #include <unordered_map>
@@ -186,7 +186,7 @@ void OutputManager::initPickpointOutput() {
     filesystem_aux::generateBackupFileIfNecessary(fileName, "dat");
     fileName += ".dat";
 
-    if (!std::filesystem::exists(fileName)) {
+    if (!seissol::filesystem::exists(fileName)) {
       std::ofstream file(fileName, std::ios_base::out);
       if (file.is_open()) {
         std::stringstream title;
