042c5d197fbb5296e48d38c9458d35a4e5e55c8f
David Schneller <david.schneller@tum.de>
2023-05-04 02:09:44 +0200

    Make `target_sources` private

--- src/sources.cmake
+++ src/sources.cmake
@@ -143,7 +143,7 @@ target_compile_options(SeisSol-common-properties INTERFACE ${EXTRA_CXX_FLAGS})
 target_include_directories(SeisSol-common-properties INTERFACE ${CMAKE_CURRENT_BINARY_DIR}/src/generated_code)
 
 if (MPI)
-  target_sources(SeisSol-lib PUBLIC
+  target_sources(SeisSol-lib PRIVATE
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Checkpoint/mpio/Wavefield.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Checkpoint/mpio/FaultAsync.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Checkpoint/mpio/Fault.cpp
@@ -152,14 +152,14 @@ if (MPI)
 endif()
 
 if (HDF5)
-  target_sources(SeisSol-lib PUBLIC
+  target_sources(SeisSol-lib PRIVATE
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Checkpoint/h5/Wavefield.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Checkpoint/h5/Fault.cpp
     )
 endif()
 
 if (HDF5 AND METIS AND MPI)
-  target_sources(SeisSol-lib PUBLIC
+  target_sources(SeisSol-lib PRIVATE
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Geometry/PUMLReader.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Initializer/time_stepping/LtsWeights/LtsWeights.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Initializer/time_stepping/LtsWeights/WeightsModels.cpp
@@ -168,13 +168,13 @@ endif()
 
 
 if (NETCDF)
-  target_sources(SeisSol-lib PUBLIC
+  target_sources(SeisSol-lib PRIVATE
     ${CMAKE_CURRENT_SOURCE_DIR}/src/SourceTerm/NRFReader.cpp # if netCDF
     )
 endif()
 
 if (ASAGI)
-  target_sources(SeisSol-lib PUBLIC
+  target_sources(SeisSol-lib PRIVATE
     #todo:
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Reader/AsagiModule.cpp
     )
@@ -183,7 +183,7 @@ endif()
 
 # Eqations have to be set at compile time currently.
 if ("${EQUATIONS}" STREQUAL "elastic")
-  target_sources(SeisSol-lib PUBLIC
+  target_sources(SeisSol-lib PRIVATE
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Equations/elastic/Kernels/DirichletBoundary.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Equations/elastic/Kernels/Local.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Equations/elastic/Kernels/Neighbor.cpp
@@ -193,7 +193,7 @@ if ("${EQUATIONS}" STREQUAL "elastic")
   target_compile_definitions(SeisSol-common-properties INTERFACE USE_ELASTIC)
 
 elseif ("${EQUATIONS}" STREQUAL "viscoelastic")
-  target_sources(SeisSol-lib PUBLIC
+  target_sources(SeisSol-lib PRIVATE
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Equations/viscoelastic/Kernels/DirichletBoundary.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Equations/viscoelastic/Kernels/Local.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Equations/viscoelastic/Kernels/Neighbor.cpp
@@ -203,7 +203,7 @@ elseif ("${EQUATIONS}" STREQUAL "viscoelastic")
   target_compile_definitions(SeisSol-common-properties INTERFACE USE_VISCOELASTIC)
 
 elseif ("${EQUATIONS}" STREQUAL "viscoelastic2")
-  target_sources(SeisSol-lib PUBLIC
+  target_sources(SeisSol-lib PRIVATE
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Equations/viscoelastic2/Kernels/Neighbor.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Equations/viscoelastic2/Kernels/Local.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Equations/viscoelastic2/Kernels/Time.cpp
@@ -212,7 +212,7 @@ elseif ("${EQUATIONS}" STREQUAL "viscoelastic2")
   target_compile_definitions(SeisSol-common-properties INTERFACE USE_VISCOELASTIC2)
 
 elseif ("${EQUATIONS}" STREQUAL "anisotropic")
-  target_sources(SeisSol-lib PUBLIC
+  target_sources(SeisSol-lib PRIVATE
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Equations/anisotropic/Kernels/DirichletBoundary.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Equations/anisotropic/Kernels/Neighbor.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Equations/anisotropic/Kernels/Local.cpp
@@ -222,7 +222,7 @@ elseif ("${EQUATIONS}" STREQUAL "anisotropic")
   target_compile_definitions(SeisSol-common-properties INTERFACE USE_ANISOTROPIC)
 
 elseif ("${EQUATIONS}" STREQUAL "poroelastic")
-  target_sources(SeisSol-lib PUBLIC
+  target_sources(SeisSol-lib PRIVATE
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Equations/poroelastic/Kernels/Neighbor.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Equations/poroelastic/Kernels/Local.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/src/Equations/poroelastic/Kernels/Time.cpp
@@ -238,7 +238,7 @@ target_include_directories(SeisSol-common-properties INTERFACE
 
 # GPU code
 if (WITH_GPU)
-  target_sources(SeisSol-lib PUBLIC
+  target_sources(SeisSol-lib PRIVATE
           ${CMAKE_CURRENT_SOURCE_DIR}/src/Initializer/BatchRecorders/LocalIntegrationRecorder.cpp
           ${CMAKE_CURRENT_SOURCE_DIR}/src/Initializer/BatchRecorders/NeighIntegrationRecorder.cpp
           ${CMAKE_CURRENT_SOURCE_DIR}/src/Initializer/BatchRecorders/PlasticityRecorder.cpp
