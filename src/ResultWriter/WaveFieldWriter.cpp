76ba896107e3f02db9df02bf14b43f04e2912d10
aangniu <aangniu96@gmail.com>
2023-10-23 16:27:11 +0200

    output damage

--- src/ResultWriter/WaveFieldWriter.cpp
+++ src/ResultWriter/WaveFieldWriter.cpp
@@ -191,6 +191,7 @@ void seissol::writer::WaveFieldWriter::init(unsigned int numVars,
   m_outputFlags = new bool[m_numVariables];
   for (size_t i = 0; i < numVars; i++)
     m_outputFlags[i] = (outputMask[i] != 0);
+  m_outputFlags[9] = true;
   for (size_t i = 0; i < WaveFieldWriterExecutor::NUM_PLASTICITY_VARIABLES; i++)
     m_outputFlags[numVars + i] = (pstrain != 0L) && (plasticityMask[i] != 0L);
 
