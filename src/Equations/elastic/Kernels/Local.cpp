89a6457d0f5cfe949f16b6d5614fb1da9d69c132
aangniu <aangniu96@gmail.com>
2023-12-05 22:51:11 -0800

    initial strain set for bai setup 2

--- src/Equations/elastic/Kernels/Local.cpp
+++ src/Equations/elastic/Kernels/Local.cpp
@@ -189,10 +189,10 @@ void seissol::kernels::Local::computeIntegral(real i_timeIntegratedDegreesOfFree
           real* eyzNodal = (QInitialNodal + 4*NUMBER_OF_ALIGNED_BASIS_FUNCTIONS);
           real* ezxNodal = (QInitialNodal + 5*NUMBER_OF_ALIGNED_BASIS_FUNCTIONS);
           for (unsigned int q = 0; q<NUMBER_OF_ALIGNED_BASIS_FUNCTIONS; ++q){
-            exxNodal[q] = -0e-2;
-            eyyNodal[q] = -0e-1;
-            ezzNodal[q] = -0e-1;
-            exyNodal[q] = -0e-3;
+            exxNodal[q] = 4.63e-4;
+            eyyNodal[q] = -1.85e-3;
+            ezzNodal[q] = 4.63e-4;
+            exyNodal[q] = 1.11e-3;
             eyzNodal[q] = -0e-3;
             ezxNodal[q] = -0e-3;
           }
