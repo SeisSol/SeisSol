842c66587950109a288d857f513dde0a2cf21849
Vikas Kurapati <vikas.kurapati@tum.de>
2023-12-14 10:44:24 +0100

    format.sh correction

--- .ci/format.sh
+++ .ci/format.sh
@@ -16,6 +16,7 @@ format() {
         src/Initializer/BatchRecorders
         src/Initializer/InitProcedure
         src/SourceTerm
+        src/Physics
         "
     
     # NOTE: once the files of a directory are (almost) fully covered, consider moving it to allowlist_dir instead
@@ -49,14 +50,6 @@ format() {
         src/Geometry/CubeGenerator.h
         src/Geometry/CubeGenerator.cpp
         src/Parallel/Helper.hpp
-        src/Physics/Attenuation.hpp
-        src/Physics/Attenuation.cpp
-        src/Physics/InstantaneousTimeMirrorManager.h
-        src/Physics/InstantaneousTimeMirrorManager.cpp
-        src/Physics/InstantaneousTimeMirrorParameters.h
-        src/Physics/InstantaneousTimeMirrorParameters.cpp
-        src/Physics/InitialField.h
-        src/Physics/InitialField.cpp
         src/ResultWriter/WaveFieldWriter.h
         src/ResultWriter/EnergyOutput.h
         src/ResultWriter/EnergyOutput.cpp
