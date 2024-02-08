b3c38fd0eeb397144e9ca766a65a0320e11ea313
aangniu <aangniu96@gmail.com>
2024-02-08 16:47:21 +0100

    max str angle from 33 to 28

--- src/DynamicRupture/Output/ReceiverBasedOutput.cpp
+++ src/DynamicRupture/Output/ReceiverBasedOutput.cpp
@@ -120,13 +120,20 @@ void ReceiverOutput::calcFaultOutput(const OutputType type,
     // real epsInityz = -0e-1; // eps_yy0
     // real epsInitzx = -0e-1; // eps_zz0
 
-    real epsInitxx = -1.8738e-4; // eps_xx0
-    real epsInityy = -1.1225e-3; // eps_yy0
-    real epsInitzz = -1.8738e-4; // eps_zz0
+    // real epsInitxx = -1.8738e-4; // eps_xx0
+    // real epsInityy = -1.1225e-3; // eps_yy0
+    // real epsInitzz = -1.8738e-4; // eps_zz0
+    // real epsInitxy = 1.0909e-3; // eps_xy0
+    // real epsInityz = -0e-1; // eps_yz0
+    // real epsInitzx = -0e-1; // eps_zx0
+
+    real epsInitxx = 3.7986e-4; // eps_xx0
+    real epsInityy = -1.0383e-3; // eps_yy0
+    real epsInitzz = -1.0072e-3; // eps_zz0
     real epsInitxy = 1.0909e-3; // eps_xy0
     real epsInityz = -0e-1; // eps_yz0
     real epsInitzx = -0e-1; // eps_zx0
-
+    
     real lambda0P = impAndEtaGet->lambda0P;
     real mu0P = impAndEtaGet->mu0P;
     real lambda0M = impAndEtaGet->lambda0M;
