21632b11712f232b6e60f70d1223d81c803c4feb
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2024-03-12 18:08:42 +0100

    add boilerplate code

--- preprocessing/science/automated_workflow_dyn_from_kin/get_usgs_finite_fault_data.py
+++ preprocessing/science/automated_workflow_dyn_from_kin/get_usgs_finite_fault_data.py
@@ -52,52 +52,51 @@ def wget_overwrite(url, out_fname=None):
     filename = wget.download(url, out=out_fname, bar=None)
 
 
-parser = argparse.ArgumentParser(
-    description="download usgs finite model data for a specific earthquake"
-)
-parser.add_argument("eq_code", help="usgs earthquake code")
-parser.add_argument(
-    "--min_magnitude", nargs=1, help="min magnitude in eq query", default=[7.0]
-)
-args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="download usgs finite model data for a specific earthquake"
+    )
+    parser.add_argument("eq_code", help="usgs earthquake code")
+    parser.add_argument(
+        "--min_magnitude", nargs=1, help="min magnitude in eq query", default=[7.0]
+    )
+    args = parser.parse_args()
 
+    eq_code = args.eq_code
+    minM = args.min_magnitude[0]
+    url = f"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&&minmagnitude={minM}&eventid={eq_code}"
+    fn_json = f"{eq_code}.json"
+    wget_overwrite(url, fn_json)
 
-eq_code = args.eq_code
-minM = args.min_magnitude[0]
-url = f"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&&minmagnitude={minM}&eventid={eq_code}"
-fn_json = f"{eq_code}.json"
-wget_overwrite(url, fn_json)
+    with open(fn_json) as f:
+        jsondata = json.load(f)
 
-with open(fn_json) as f:
-    jsondata = json.load(f)
+    mag = get_value_from_usgs_data(jsondata, "mag")
+    place = get_value_from_usgs_data(jsondata, "place")
+    dyfi = get_value_from_usgs_data(jsondata, "dyfi")[0]
+    eventtime = dyfi["properties"]["eventtime"]
+    day = eventtime.split("T")[0]
+    descr = "_".join(place.split(",")[-1].split())
+    finite_fault = get_value_from_usgs_data(jsondata, "finite-fault")[0]
+    code_finite_fault = finite_fault["code"]
+    update_time = finite_fault["updateTime"]
+    hypocenter_x = finite_fault["properties"]["longitude"]
+    hypocenter_y = finite_fault["properties"]["latitude"]
+    hypocenter_z = finite_fault["properties"]["depth"]
 
-mag = get_value_from_usgs_data(jsondata, "mag")
-place = get_value_from_usgs_data(jsondata, "place")
-dyfi = get_value_from_usgs_data(jsondata, "dyfi")[0]
-eventtime = dyfi["properties"]["eventtime"]
-day = eventtime.split("T")[0]
-descr = "_".join(place.split(",")[-1].split())
-finite_fault = get_value_from_usgs_data(jsondata, "finite-fault")[0]
-code_finite_fault = finite_fault["code"]
-update_time = finite_fault["updateTime"]
-hypocenter_x = finite_fault["properties"]["longitude"]
-hypocenter_y = finite_fault["properties"]["latitude"]
-hypocenter_z = finite_fault["properties"]["depth"]
+    folder_name = f"{day}_Mw{mag}_{descr[:20]}_{code_finite_fault}"
 
-folder_name = f"{day}_Mw{mag}_{descr[:20]}_{code_finite_fault}"
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    if not os.path.exists(f"{folder_name}/tmp"):
+        os.makedirs(f"{folder_name}/tmp")
 
-if not os.path.exists(folder_name):
-    os.makedirs(folder_name)
-if not os.path.exists(f"{folder_name}/tmp"):
-    os.makedirs(f"{folder_name}/tmp")
+    with open(f"{folder_name}/tmp/hypocenter.txt", "w") as f:
+        jsondata = f.write(f"{hypocenter_x} {hypocenter_y} {hypocenter_z}\n")
 
+    for fn in ["moment_rate.mr", "basic_inversion.param"]:
+        url = f"https://earthquake.usgs.gov/product/finite-fault/{code_finite_fault}/us/{update_time}/{fn}"
+        wget_overwrite(url, f"{folder_name}/tmp/{fn}")
 
-with open(f"{folder_name}/tmp/hypocenter.txt", "w") as f:
-    jsondata = f.write(f"{hypocenter_x} {hypocenter_y} {hypocenter_z}\n")
-
-for fn in ["moment_rate.mr", "basic_inversion.param"]:
-    url = f"https://earthquake.usgs.gov/product/finite-fault/{code_finite_fault}/us/{update_time}/{fn}"
-    wget_overwrite(url, f"{folder_name}/tmp/{fn}")
-
-shutil.move(fn_json, f"{folder_name}/tmp/{fn_json}")
-print(folder_name)
+    shutil.move(fn_json, f"{folder_name}/tmp/{fn_json}")
+    print(folder_name)
