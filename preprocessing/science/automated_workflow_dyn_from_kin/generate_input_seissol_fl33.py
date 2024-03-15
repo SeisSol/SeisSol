dfdc54c1669df424c8dbe88615617e85e41017ef
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2024-03-15 17:53:01 +0100

    longer fl33 and remove usage check on gpus

--- preprocessing/science/automated_workflow_dyn_from_kin/generate_input_seissol_fl33.py
+++ preprocessing/science/automated_workflow_dyn_from_kin/generate_input_seissol_fl33.py
@@ -39,8 +39,7 @@ end_time = max_distance / 3200.0
 template_par = {}
 # well in theory we would need to run for end_time, but practically
 # a portion of it may be sufficient
-template_par["end_time"] = 0.3 * end_time
-
+template_par["end_time"] = max(15.0, 0.3 * end_time)
 
 template = templateEnv.get_template(f"parameters_fl34.tmpl.par")
 outputText = template.render(template_par)
--- preprocessing/science/automated_workflow_dyn_from_kin/run_two_GPU_heisenbug.sh
+++ preprocessing/science/automated_workflow_dyn_from_kin/run_two_GPU_heisenbug.sh
@@ -4,22 +4,17 @@ module unload seissol
 module load seissol/master-gcc-10.2.1-o4-elas-dunav-single-cuda-7x4s4mk
 mkdir -p logs
 nvidia-smi
-mem_usage=$(nvidia-smi --query-gpu=memory.used --format=csv | awk ' {s+=$1} END {printf "%.0f\n", s}')
 export SEISSOL_PREFERRED_MPI_DATA_TRANSFER_MODE=host
 export SEISSOL_MPI_PERSISTENT=1
 
-if [ $mem_usage == '0' ]; then
-   export OMP_NUM_THREADS=10
-   export OMP_PLACES="cores"
-   export OMP_PROC_BIND=spread
-   counter=0
-   total_params=$(ls parameters_dyn* | wc -l)
+export OMP_NUM_THREADS=10
+export OMP_PLACES="cores"
+export OMP_PROC_BIND=spread
+counter=0
+total_params=$(ls parameters_dyn* | wc -l)
 
-   for par_file in parameters_dyn*; do
-       counter=$((counter+1))
-       echo "Processing file $counter of $total_params: $par_file"
-       mpirun -n 2 --map-by ppr:2:numa:pe=$OMP_NUM_THREADS --report-bindings seissol-launch SeisSol_Release_ssm_86_cuda_4_elastic $par_file  2>&1 | tee logs/$par_file.out
-   done
-else
-    echo "GPU already in use!"
-fi
+for par_file in parameters_dyn*; do
+    counter=$((counter+1))
+    echo "Processing file $counter of $total_params: $par_file"
+    mpirun -n 2 --map-by ppr:2:numa:pe=$OMP_NUM_THREADS --report-bindings seissol-launch SeisSol_Release_ssm_86_cuda_4_elastic $par_file  2>&1 | tee logs/$par_file.out
+done
