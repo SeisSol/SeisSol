55b212399e789a51341aa4b250cc6ec7208c8ac0
Thomas-Ulrich <thomas.ulrich@geophysik.uni-muenchen.de>
2024-04-05 14:44:35 +0200

    fix rake low slip

--- preprocessing/science/automated_workflow_dyn_from_kin/workflow_usgs_finite_model_afterFL33.sh
+++ preprocessing/science/automated_workflow_dyn_from_kin/workflow_usgs_finite_model_afterFL33.sh
@@ -7,7 +7,9 @@ if [[ ! -v seissolpath ]]; then
 fi
 
 $seissolpath/preprocessing/science/kinematic_models/generate_fault_output_from_fl33_input_files.py output/fl33-fault.xdmf yaml_files/FL33_34_fault.yaml --stf Gaussian --output output/dyn-usgs-fault
-$seissolpath/preprocessing/science/automated_workflow_dyn_from_kin/project_fault_tractions_asagi_grid.py --dx 200 output/fl33-fault.xdmf
+read inferred_fault_mesh_size < tmp/inferred_fault_mesh_size.txt
+grid_size=$(echo "scale=2; $inferred_fault_mesh_size / 2" | bc)
+$seissolpath/preprocessing/science/automated_workflow_dyn_from_kin/project_fault_tractions_asagi_grid.py --dx $grid_size output/fl33-fault.xdmf --gaussian_kernel $inferred_fault_mesh_size
 $seissolpath/preprocessing/science/automated_workflow_dyn_from_kin/infer_segment_average_rake.py output/fl33-fault.xdmf
 module load seissol
 $seissolpath/preprocessing/science/automated_workflow_dyn_from_kin/generate_input_seissol_dr.py
