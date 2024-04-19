475ecc169a01cffa8dfd3982ffc657ee7a5de848
David Schneller <david.schneller@tum.de>
2024-04-19 11:22:30 +0200

    Second lambda fix

--- src/Parallel/Runtime/StreamSycl.cpp
+++ src/Parallel/Runtime/StreamSycl.cpp
@@ -7,17 +7,19 @@ namespace seissol::parallel::runtime {
 
 void StreamRuntime::syncToSycl(void* queuePtr) {
     sycl::queue* queue = static_cast<sycl::queue*>(queuePtr);
-    device().api->recordEventOnStream(forkEventSycl, streamPtr);
+    auto* localForkEventSycl{forkEventSycl};
+    device().api->recordEventOnStream(localForkEventSycl, streamPtr);
     syclNativeOperation(*queue, true, [=](void* stream) {
-        device().api->syncStreamWithEvent(stream, forkEventSycl);
+        device().api->syncStreamWithEvent(stream, localForkEventSycl);
     });
 }
 void StreamRuntime::syncFromSycl(void* queuePtr) {
     sycl::queue* queue = static_cast<sycl::queue*>(queuePtr);
+    auto* localJoinEventSycl{joinEventSycl};
     syclNativeOperation(*queue, true, [=](void* stream) {
-        device().api->recordEventOnStream(joinEventSycl, stream);
+        device().api->recordEventOnStream(localJoinEventSycl, stream);
     });
-    device().api->syncStreamWithEvent(streamPtr, joinEventSycl);
+    device().api->syncStreamWithEvent(streamPtr, localJoinEventSycl);
 }
 
 } // namespace seissol::parallel::runtime
