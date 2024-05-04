154b941031a4ff7342d9e39a7c8d2b08d23a3ec8
David Schneller <david.schneller@tum.de>
2024-05-04 23:54:29 +0200

    Disable synchronization in one direction (SYCL->CUDA/HIP)

--- src/Parallel/Runtime/StreamSycl.cpp
+++ src/Parallel/Runtime/StreamSycl.cpp
@@ -15,11 +15,16 @@ void StreamRuntime::syncToSycl(void* queuePtr) {
 }
 void StreamRuntime::syncFromSycl(void* queuePtr) {
   sycl::queue* queue = static_cast<sycl::queue*>(queuePtr);
+  queue->wait();
+
+  /*
+  // the following will not work, because SYCL may decide to postpone execution a bit
   auto* localJoinEventSycl{joinEventSycl};
   syclNativeOperation(*queue, true, [=](void* stream) {
     device().api->recordEventOnStream(localJoinEventSycl, stream);
   });
   device().api->syncStreamWithEvent(streamPtr, localJoinEventSycl);
+  */
 }
 
 } // namespace seissol::parallel::runtime
