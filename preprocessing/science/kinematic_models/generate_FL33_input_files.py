a66cd670106ff6f6b4cfa145dce8c4b7ce50ee91
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2024-05-17 15:15:58 +0200

    extend kinematic models

--- preprocessing/science/kinematic_models/generate_FL33_input_files.py
+++ preprocessing/science/kinematic_models/generate_FL33_input_files.py
@@ -1,25 +1,44 @@
+#!/usr/bin/env python3
 import os
 import argparse
-from FaultPlane import FaultPlane
+from FaultPlane import FaultPlane, MultiFaultPlane
 import os.path
 
 parser = argparse.ArgumentParser(
-    description="generate yaml and netcdf input to be used with friction law 33/34 based on a (here"
-    + "upsampled) kinematic model in the standard rupture format srf file."
+    description=(
+        "generate yaml and netcdf input to be used with friction law 33/34 based on a"
+        " (here upsampled) kinematic model in the standard rupture format srf file."
+    )
 )
 parser.add_argument("filename", help="filename of the srf file")
+
 parser.add_argument(
     "--interpolation_method",
     nargs=1,
-    metavar=("interpolation_method"),
+    metavar="interpolation_method",
     default=["linear"],
     help="interpolation method",
     choices=["linear", "nearest", "slinear", "cubic", "quintic"],
 )
 parser.add_argument(
+    "--proj",
+    metavar=("proj"),
+    nargs=1,
+    help=("proj4 string describing the projection"),
+    required=True,
+)
+parser.add_argument(
+    "--PSRthreshold",
+    help="peak slip rate threshold (0-1) to determine onset time and duration of STF",
+    nargs=1,
+    metavar="PSRthreshold",
+    type=float,
+    default=[0.0],
+)
+parser.add_argument(
     "--spatial_zoom",
     nargs=1,
-    metavar=("spatial_zoom"),
+    metavar="spatial_zoom",
     required=True,
     help="level of spatial upsampling",
     type=int,
@@ -31,42 +50,38 @@ parser.add_argument(
     help="write also netcdf readable by paraview",
     default=False,
 )
-parser.add_argument(
-    "--generate_ts_yaml",
-    metavar=("proj"),
-    nargs=1,
-    help="generate fault yaml file and write fault geometry (ts file). proj: proj4 string describing the projection.",
-)
-
-parser.add_argument(
-    "--PSRthreshold",
-    help="peak slip rate threshold (0-1) to determine onset time and duration of STF",
-    nargs=1,
-    metavar=("PSRthreshold"),
-    type=float,
-    default=[0.0],
-)
-
 args = parser.parse_args()
 
-p1 = FaultPlane()
-p1.init_from_srf(args.filename)
-p1.compute_time_array()
-p1.assess_STF_parameters(args.PSRthreshold[0])
 prefix, ext = os.path.splitext(args.filename)
 prefix = os.path.basename(prefix)
 
-p1.generate_netcdf_fl33(
+if ext == ".srf":
+    mfp = MultiFaultPlane.from_srf(args.filename)
+elif ext == ".param":
+    mfp = MultiFaultPlane.from_usgs_param_file(args.filename)
+elif ext == ".fsp":
+    mfp = MultiFaultPlane.from_usgs_fsp_file(args.filename)
+elif ext == ".txt":
+    mfp = MultiFaultPlane.from_slipnear_param_file(args.filename)
+else:
+    raise NotImplementedError(f" unknown extension: {ext}")
+
+for p, p1 in enumerate(mfp.fault_planes):
+    p1.compute_time_array()
+    if ext == ".srf":
+        p1.assess_STF_parameters(args.PSRthreshold[0])
+    p1.generate_netcdf_fl33(
+        f"{prefix}{p+1}",
+        method=args.interpolation_method[0],
+        spatial_zoom=args.spatial_zoom[0],
+        proj=args.proj[0],
+        write_paraview=args.write_paraview,
+        slip_cutoff=0.0,
+    )
+
+mfp.generate_fault_ts_yaml_fl33(
     prefix,
     method=args.interpolation_method[0],
     spatial_zoom=args.spatial_zoom[0],
-    proj=args.generate_ts_yaml,
-    write_paraview=args.write_paraview,
+    proj=args.proj[0],
 )
-if args.generate_ts_yaml:
-    p1.generate_fault_ts_yaml_fl33(
-        prefix,
-        method=args.interpolation_method[0],
-        spatial_zoom=args.spatial_zoom[0],
-        proj=args.generate_ts_yaml,
-    )
