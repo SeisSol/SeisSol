593c66c625f5907bab8ce2ac44ea5c118e9db225
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2024-05-27 13:32:35 +0200

    change back structure of kinematic model

--- preprocessing/science/kinematic_models/generate_FL33_input_files.py
+++ preprocessing/science/kinematic_models/generate_FL33_input_files.py
@@ -1,9 +1,50 @@
 #!/usr/bin/env python3
 import os
 import argparse
-from kinmodmodules.generate_FL33_input_files_routines import main
+from FaultPlane import FaultPlane, MultiFaultPlane
 import os.path
 
+def main(
+    filename,
+    interpolation_method,
+    spatial_zoom,
+    projection,
+    write_paraview,
+    PSRthreshold,
+):
+    prefix, ext = os.path.splitext(filename)
+    prefix = os.path.basename(prefix)
+
+    if ext == ".srf":
+        mfp = MultiFaultPlane.from_srf(filename)
+    elif ext == ".param":
+        mfp = MultiFaultPlane.from_usgs_param_file(filename)
+    elif ext == ".fsp":
+        mfp = MultiFaultPlane.from_usgs_fsp_file(filename)
+    elif ext == ".txt":
+        mfp = MultiFaultPlane.from_slipnear_param_file(filename)
+    else:
+        raise NotImplementedError(f" unknown extension: {ext}")
+
+    for p, p1 in enumerate(mfp.fault_planes):
+        p1.compute_time_array()
+        if ext == ".srf":
+            p1.assess_STF_parameters(PSRthreshold)
+        p1.generate_netcdf_fl33(
+            f"{prefix}{p+1}",
+            method=interpolation_method,
+            spatial_zoom=spatial_zoom,
+            proj=projection,
+            write_paraview=write_paraview,
+            slip_cutoff=0.0,
+        )
+
+    mfp.generate_fault_ts_yaml_fl33(
+        prefix,
+        method=interpolation_method,
+        spatial_zoom=spatial_zoom,
+        proj=projection,
+    )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
--- preprocessing/science/kinematic_models/refine_srf.py
+++ preprocessing/science/kinematic_models/refine_srf.py
@@ -1,6 +1,37 @@
 import os
 import argparse
-from kinmodmodules.refine_srf_routines import refine
+from FaultPlane import FaultPlane
+
+def refine(
+    filename,
+    projection,
+    yoffePSRthreshold,
+    spatial_order,
+    spatial_zoom,
+    temporal_zoom,
+    time_smoothing_kernel_as_dt_fraction,
+):
+
+    p1 = FaultPlane()
+    p1.init_from_srf(filename)
+    p1.compute_xy_from_latlon(projection)
+    p1.compute_time_array()
+
+    use_Yoffe = True if yoffePSRthreshold else False
+    if use_Yoffe:
+        p1.assess_STF_parameters(yoffePSRthreshold)
+
+    p2 = p1.upsample_fault(
+        spatial_order=spatial_order,
+        spatial_zoom=spatial_zoom,
+        temporal_zoom=temporal_zoom,
+        proj=projection,
+        use_Yoffe=use_Yoffe,
+        time_smoothing_kernel_as_dt_fraction=time_smoothing_kernel_as_dt_fraction,
+    )
+    prefix, ext = os.path.splitext(args.filename)
+    fnout = prefix + "_resampled" + ".srf"
+    p2.write_srf(fnout)
 
 
 if __name__ == "__main__":
