1a772526f5b634edbd1ad8cbb78d3d835189bf6e
David Schneller <david.schneller@tum.de>
2024-06-21 13:33:55 +0200

    Add AdaptiveCpp coarse-grained event support

--- src/Parallel/AcceleratorDevice.cpp
+++ src/Parallel/AcceleratorDevice.cpp
@@ -34,7 +34,16 @@ void AcceleratorDevice::bindSyclDevice(int deviceId) {
        << syclDevice.is_gpu() << "): " << syclDevice.get_info<sycl::info::device::name>();
   infoMessages.push_back(info.str());
 
+#if defined(ACPP_EXT_COARSE_GRAINED_EVENTS) && !defined(SEISSOL_KERNELS_SYCL)
+  sycl::property_list property{sycl::property::queue::in_order(),
+                               sycl::property::queue::AdaptiveCpp_coarse_grained_events()};
+#elif defined(HIPSYCL_EXT_COARSE_GRAINED_EVENTS) && !defined(SEISSOL_KERNELS_SYCL)
+  sycl::property_list property{sycl::property::queue::in_order(),
+                               sycl::property::queue::hipSYCL_coarse_grained_events()};
+#else
   sycl::property_list property{sycl::property::queue::in_order()};
+#endif
+
   syclDefaultQueue = sycl::queue(syclDevice, property);
 }
 
