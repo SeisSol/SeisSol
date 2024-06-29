473918f295f1a1bec1abeb5771513501bbc75502
David Schneller <david.schneller@tum.de>
2024-06-29 17:32:53 +0200

    Update permanent threading environment

--- src/Parallel/Runtime/Threading.cpp
+++ src/Parallel/Runtime/Threading.cpp
@@ -1,20 +1,46 @@
 #include "Threading.hpp"
+#include <atomic>
+#include <omp.h>
 
 namespace seissol::parallel::runtime {
 
+void ThreadingRuntime::add(SimpleTask&& task) {
+  std::lock_guard tasksLock(tasksMutex);
+  tasks.emplace_back(std::move(task));
+}
+
 // emulate a de-facto pthread-like runtime; until all OMP features are implemented properly
 void ThreadingRuntime::run() {
+  std::vector<SimpleTask> nextTasks;
+  std::vector<std::atomic<int>> done;
 #pragma omp parallel
   {
     while (true) {
 #pragma omp barrier
-      std::vector<std::function<void(std::size_t)>> nextTasks;
-      for (const auto& task : nextTasks) {
-#pragma omp for nowait
-        for (std::size_t i = 0; i < TODO; ++i) {
-          task(i);
+#pragma omp single
+      {
+        std::lock_guard tasksLock(tasksMutex);
+        nextTasks.resize(0);
+        done.resize(0);
+
+        for (auto it = tasks.begin(); it != tasks.end(); ++it) {
+          if (std::invoke(it->ready)) {
+            nextTasks.push_back(*it);
+            done.push_back(omp_get_num_threads());
+            it = tasks.erase(it);
+          }
         }
-        // TODO: notify task done
+      }
+      for (std::size_t j = 0; j < nextTasks.size(); ++j) {
+        auto& task = nextTasks[j];
+#pragma omp for nowait
+        for (std::size_t i = 0; i < task.size; ++i) {
+          std::invoke(task.function, i);
+        }
+        auto prev = done[j].fetch_sub(1);
+        if (prev == 1) {
+          std::invoke(task.completion);
+        }
       }
     }
   }
--- src/Parallel/Runtime/Threading.hpp
+++ src/Parallel/Runtime/Threading.hpp
@@ -2,16 +2,25 @@
 #define SEISSOL_PARALLEL_RUNTIME_THREADING_HPP
 
 #include <functional>
+#include <list>
+#include <mutex>
 namespace seissol::parallel::runtime {
+
+struct SimpleTask {
+  std::function<bool()> ready;
+  std::function<void(std::size_t)> function;
+  std::function<void()> completion;
+  std::size_t size;
+};
 
 class ThreadingRuntime {
   public:
   void run();
-  void add();
-  void addFor();
+  void add(SimpleTask&& task);
 
   private:
-  std::vector<std::function<void(std::size_t)>> functions;
+  std::mutex tasksMutex;
+  std::list<SimpleTask> tasks;
 };
 
 } // namespace seissol::parallel::runtime
