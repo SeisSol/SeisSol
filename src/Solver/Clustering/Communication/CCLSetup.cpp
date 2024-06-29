ebc38d7c366c1a1bdd96e4dfc8b38fc1892fe09c
David Schneller <david.schneller@tum.de>
2024-06-29 16:55:55 +0200

    Add more ifdef guards

--- src/Solver/Clustering/Communication/CCLSetup.cpp
+++ src/Solver/Clustering/Communication/CCLSetup.cpp
@@ -13,6 +13,7 @@
 namespace seissol::solver::clustering::communication {
 
 void* createComm() {
+#ifdef USE_CCL
   // cf. partially https://docs.nvidia.com/deeplearning/nccl/user-guide/docs/examples.html
   ncclComm_t preComm;
   ncclUniqueId cclId;
@@ -22,6 +23,9 @@ void* createComm() {
   MPI_Bcast(&cclId, sizeof(ncclUniqueId), MPI_BYTE, 0, seissol::MPI::mpi.comm());
   ncclCommInitRank(&preComm, seissol::MPI::mpi.size(), cclId, seissol::MPI::mpi.rank());
   return static_cast<void*>(preComm);
+#else
+  return nullptr;
+#endif
 }
 
 } // namespace seissol::solver::clustering::communication
