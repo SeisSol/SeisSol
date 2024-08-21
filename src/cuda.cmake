153543cfb5c4d36f020b1f4bc4288184d351a421
David Schneller <david.schneller@tum.de>
2024-08-21 22:14:30 +0200

    Switch CUDA and HIP to C++17

--- src/cuda.cmake
+++ src/cuda.cmake
@@ -1,5 +1,5 @@
 enable_language(CUDA)
-set(CMAKE_CUDA_STANDARD 14)
+set(CMAKE_CUDA_STANDARD 17)
 
 set(DEVICE_SRC ${DEVICE_SRC}
         ${CMAKE_BINARY_DIR}/src/generated_code/gpulike_subroutine.cpp
@@ -12,7 +12,7 @@ set_target_properties(SeisSol-device-lib PROPERTIES POSITION_INDEPENDENT_CODE ON
 set_source_files_properties(${DEVICE_SRC} PROPERTIES LANGUAGE CUDA)
 
 target_include_directories(SeisSol-device-lib PUBLIC ${SEISSOL_DEVICE_INCLUDE})
-target_compile_features(SeisSol-device-lib PRIVATE cxx_std_14)
+target_compile_features(SeisSol-device-lib PRIVATE cxx_std_17)
 target_compile_options(SeisSol-device-lib PRIVATE ${EXTRA_CXX_FLAGS})
 
 string(REPLACE "sm_" "" CUDA_DEVICE_ARCH "${DEVICE_ARCH}")
