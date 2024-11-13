99270267abd7e5f96ff404eebee87ae40d33375a
David Schneller <david.schneller@tum.de>
2024-11-13 12:26:13 +0100

    Make cluster array contiguous

--- src/Initializer/TimeStepping/LtsLayout.cpp
+++ src/Initializer/TimeStepping/LtsLayout.cpp
@@ -845,9 +845,18 @@ void seissol::initializer::time_stepping::LtsLayout::deriveClusteredCopyInterior
   }
 
   // HACK: add ghost cell clusters as well
-  for( int i = 0; i < m_plainNeighboringRanks.size(); ++i ) {
-    for (int j = 0; j < m_numberOfPlainGhostCells[i]; ++j) {
+  for( std::size_t i = 0; i < m_plainNeighboringRanks.size(); ++i ) {
+    for (unsigned int j = 0; j < m_numberOfPlainGhostCells[i]; ++j) {
       l_localClusters.insert( m_plainGhostCellClusterIds[i][j] );
+    }
+  }
+
+  // HACK 2: make contiguous
+  if (!l_localClusters.empty()) {
+    const auto minLocal = *l_localClusters.begin();
+    const auto maxLocal = *l_localClusters.rbegin();
+    for (auto i = minLocal; i <= maxLocal; ++i) {
+      l_localClusters.insert(i);
     }
   }
 
