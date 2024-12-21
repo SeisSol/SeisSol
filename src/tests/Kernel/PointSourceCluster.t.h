021b1156e6eedf54696d2cd4ac5794e8b0656df2
David Schneller <david.schneller@tum.de>
2024-12-21 19:31:30 +0100

    Shorten third-party licenses

--- src/tests/Kernel/PointSourceCluster.t.h
+++ src/tests/Kernel/PointSourceCluster.t.h
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2024 SeisSol Group
-// Copyright (C) 2023 Intel Corporation
+// SPDX-FileCopyrightText: 2023 Intel Corporation
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
