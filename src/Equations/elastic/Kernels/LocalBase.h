078471b40fdbdec2cd491c451606dd2a0a1a644c
David Schneller <david.schneller@tum.de>
2025-01-16 15:20:46 +0100

    Remove double comment lines

--- src/Equations/elastic/Kernels/LocalBase.h
+++ src/Equations/elastic/Kernels/LocalBase.h
@@ -3,7 +3,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // SPDX-LicenseComments: Full text under /LICENSE and /LICENSES/
 //
-//
 // SPDX-FileContributor: Author lists in /AUTHORS and /CITATION.cff
 // SPDX-FileContributor: Alexander Breuer
 
