ac8fd2d28f461ad772100be1f81e204f9461fa3a
John Rekoske <john.rekoske@gmail.com>
2025-02-04 22:05:44 +0100

    HDF5 updates.

--- src/ResultWriter/ParallelHdf5ReceiverWriter.h
+++ src/ResultWriter/ParallelHdf5ReceiverWriter.h
@@ -5,10 +5,10 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <Eigen/Dense>
 
 class ParallelHdf5ReceiverWriter {
   public:
-  // Same constructor, but now we also keep a copy of comm
   ParallelHdf5ReceiverWriter(MPI_Comm comm,
                              const std::string& filename,
                              hsize_t totalReceivers,
@@ -20,21 +20,28 @@ class ParallelHdf5ReceiverWriter {
                   hsize_t localReceiverCount,
                   const std::vector<double>& data);
 
+  void writePointIds(hsize_t receiverOffset,
+                     hsize_t localReceiverCount,
+                     const std::vector<unsigned long long>& pointIds);
+
+  void writeCoordinates(std::vector<Eigen::Vector3d> points);
+
   ~ParallelHdf5ReceiverWriter();
 
   private:
   static constexpr int RANK = 3;
 
-  // We store the communicator to print ranks
   MPI_Comm comm_;
   hid_t fileId_;
   hid_t dsetId_;
   hid_t filespaceId_;
+  hid_t pointIdSpaceId_;
+  hid_t pointIdDsetId_;
 
-  // dataset dimensions
   hsize_t dims_[RANK];
 
-  void checkStatus(herr_t status, const std::string& msg) const {
+      void
+      checkStatus(herr_t status, const std::string& msg) const {
     if (status < 0) {
       throw std::runtime_error("HDF5 error in: " + msg);
     }
