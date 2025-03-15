08572d75f8b29e2c81bfe61f1193162eae1c192e
David Schneller <david.schneller@tum.de>
2025-03-15 17:24:27 +0100

    Fix the debug builds

--- src/ResultWriter/WaveFieldWriterExecutor.h
+++ src/ResultWriter/WaveFieldWriterExecutor.h
@@ -124,8 +124,8 @@ class WaveFieldWriterExecutor {
     std::vector<const char*> variables;
     for (unsigned int i = 0; i < m_numVariables; i++) {
       if (m_outputFlags[i]) {
-        assert(i <
-               seissol::model::MaterialT::Quantities + seissol::model::PlasticityData::Quantities);
+        assert(i < seissol::model::MaterialT::Quantities.size() +
+                       seissol::model::PlasticityData::Quantities.size());
         variables.push_back(varNames->at(i).c_str());
       }
     }
