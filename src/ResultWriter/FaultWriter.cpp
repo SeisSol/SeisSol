85f2fd85bd3934db2ced7362d9e4d86f8ec48507
David Schneller <david.schneller@tum.de>
2025-05-13 16:50:36 +0200

    Add a floating communication thread

--- src/ResultWriter/FaultWriter.cpp
+++ src/ResultWriter/FaultWriter.cpp
@@ -30,7 +30,7 @@ void seissol::writer::FaultWriter::setUp() {
   setExecutor(m_executor);
 
   utils::Env env("SEISSOL_");
-  if (isAffinityNecessary() && useCommThread(seissol::MPI::mpi, env)) {
+  if (isAffinityNecessary() && useCommThread(seissol::MPI::mpi, env) == CommThreadType::Pinned) {
     const auto freeCpus = seissolInstance.getPinning().getFreeCPUsMask();
     logInfo() << "Fault writer thread affinity:" << parallel::Pinning::maskToString(freeCpus);
     if (parallel::Pinning::freeCPUsMaskEmpty(freeCpus)) {
