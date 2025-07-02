9ca241189c647d1ae2c91dfb6253a024674541a3
aangniu <aangniu96@gmail.com>
2025-07-02 17:23:34 +0200

    filled the buffers with integrated Fx, Fy and Fz as well

--- src/Kernels/NonLinearCK/Time.cpp
+++ src/Kernels/NonLinearCK/Time.cpp
@@ -404,7 +404,6 @@ void Spacetime::computeNonlAder(double timeStepWidth,
   real epsInityz = data.material().local.epsInit_yz;
   real epsInitzx = data.material().local.epsInit_xz;
 
-
   for (unsigned timeInterval = 0; timeInterval < ConvergenceOrder; ++timeInterval) {
     // Compute rusanovFluxMinusNodal at each time step and integrate in time
     // with temporal quadrature
@@ -421,12 +420,76 @@ void Spacetime::computeNonlAder(double timeStepWidth,
     real* vzNodal = (QInterpolatedBodyNodal[timeInterval] + 8*tensor::Q::Shape[0]);
     // time quadrature for each nodal values
     for (unsigned q = 0; q < tensor::Q::Shape[0]; ++q){
-      integratedFxNodal[0*tensor::Q::Shape[0] + q] += -vxNodal[q] * weight;
-    }
-  }
+      real EspI = (exxNodal[q]+epsInitxx) + (eyyNodal[q]+epsInityy) + (ezzNodal[q]+epsInitzz);
+      real EspII = (exxNodal[q]+epsInitxx)*(exxNodal[q]+epsInitxx)
+        + (eyyNodal[q]+epsInityy)*(eyyNodal[q]+epsInityy)
+        + (ezzNodal[q]+epsInitzz)*(ezzNodal[q]+epsInitzz)
+        + 2*(exyNodal[q]+epsInitxy)*(exyNodal[q]+epsInitxy)
+        + 2*(eyzNodal[q]+epsInityz)*(eyzNodal[q]+epsInityz)
+        + 2*(ezxNodal[q]+epsInitzx)*(ezxNodal[q]+epsInitzx);
+      
+      // Compute nonlinear flux term
+      real mu_eff = mat_mu0 - alphaNodal[q]*mat_mu0;
+      sxxNodal[q] = (1-alphaNodal[q])*mat_lambda0*EspI
+                    + 2*mu_eff*(exxNodal[q]+epsInitxx);
+      syyNodal[q] = (1-alphaNodal[q])*mat_lambda0*EspI
+                    + 2*mu_eff*(eyyNodal[q]+epsInityy);
+      szzNodal[q] = (1-alphaNodal[q])*mat_lambda0*EspI
+                    + 2*mu_eff*(ezzNodal[q]+epsInitzz);
+      sxyNodal[q] = 2*mu_eff*(exyNodal[q]+epsInitxy);
+      syzNodal[q] = 2*mu_eff*(eyzNodal[q]+epsInityz);
+      szxNodal[q] = 2*mu_eff*(ezxNodal[q]+epsInitzx);
 
-  // // Step 4: Convert the integrated Fx, Fy and Fz flux from Nodal to Modal space
+      integratedFxNodal[0*tensor::Q::Shape[0] + q] += -vxNodal[q]*weight;
+      integratedFxNodal[1*tensor::Q::Shape[0] + q] += 0;
+      integratedFxNodal[2*tensor::Q::Shape[0] + q] += 0;
+      integratedFxNodal[3*tensor::Q::Shape[0] + q] += -0.5*vyNodal[q]*weight;
+      integratedFxNodal[4*tensor::Q::Shape[0] + q] += 0;
+      integratedFxNodal[5*tensor::Q::Shape[0] + q] += -0.5*vzNodal[q]*weight;
+      integratedFxNodal[6*tensor::Q::Shape[0] + q] += -sxxNodal[q]/data.material().local.rho*weight;
+      integratedFxNodal[7*tensor::Q::Shape[0] + q] += -sxyNodal[q]/data.material().local.rho*weight;
+      integratedFxNodal[8*tensor::Q::Shape[0] + q] += -szxNodal[q]/data.material().local.rho*weight;
+      integratedFxNodal[9*tensor::Q::Shape[0] + q] += 0;
+
+      integratedFyNodal[0*tensor::Q::Shape[0] + q] += 0;
+      integratedFyNodal[1*tensor::Q::Shape[0] + q] += -vyNodal[q]*weight;
+      integratedFyNodal[2*tensor::Q::Shape[0] + q] += 0;
+      integratedFyNodal[3*tensor::Q::Shape[0] + q] += -0.5*vxNodal[q]*weight;
+      integratedFyNodal[4*tensor::Q::Shape[0] + q] += -0.5*vzNodal[q]*weight;
+      integratedFyNodal[5*tensor::Q::Shape[0] + q] += 0;
+      integratedFyNodal[6*tensor::Q::Shape[0] + q] += -sxyNodal[q]/data.material().local.rho*weight;
+      integratedFyNodal[7*tensor::Q::Shape[0] + q] += -syyNodal[q]/data.material().local.rho*weight;
+      integratedFyNodal[8*tensor::Q::Shape[0] + q] += -syzNodal[q]/data.material().local.rho*weight;
+      integratedFyNodal[9*tensor::Q::Shape[0] + q] += 0;
+
+      integratedFzNodal[0*tensor::Q::Shape[0] + q] += 0;
+      integratedFzNodal[1*tensor::Q::Shape[0] + q] += 0;
+      integratedFzNodal[2*tensor::Q::Shape[0] + q] += -vzNodal[q]*weight;
+      integratedFzNodal[3*tensor::Q::Shape[0] + q] += 0;
+      integratedFzNodal[4*tensor::Q::Shape[0] + q] += -0.5*vyNodal[q]*weight;
+      integratedFzNodal[5*tensor::Q::Shape[0] + q] += -0.5*vxNodal[q]*weight;
+      integratedFzNodal[6*tensor::Q::Shape[0] + q] += -szxNodal[q]/data.material().local.rho*weight;
+      integratedFzNodal[7*tensor::Q::Shape[0] + q] += -syzNodal[q]/data.material().local.rho*weight;
+      integratedFzNodal[8*tensor::Q::Shape[0] + q] += -szzNodal[q]/data.material().local.rho*weight;
+      integratedFzNodal[9*tensor::Q::Shape[0] + q] += 0;
+    }
+  } // end of time integration
+
+  // // Step 4: Convert the INTEGRATED Fx, Fy and Fz flux from Nodal to Modal space
   // Store them in integratedFx, integratedFy, and integratedFz
+  kernel::damageAssignFToDQ d_convertBackKrnl;
+  d_convertBackKrnl.vInv = init::vInv::Values;
+  d_convertBackKrnl.FNodal = integratedFxNodal;
+  d_convertBackKrnl.dQModal = integratedFx;
+  d_convertBackKrnl.execute();
+
+  d_convertBackKrnl.FNodal = integratedFyNodal;
+  d_convertBackKrnl.dQModal = integratedFy;
+  d_convertBackKrnl.execute();
+
+  d_convertBackKrnl.FNodal = integratedFzNodal;
+  d_convertBackKrnl.dQModal = integratedFz;
+  d_convertBackKrnl.execute();
 
   // Do not compute it like this if at interface
   // Compute integrated displacement over time step if needed.
