91980c4406703a4685cbef52765e309a69f97184
David Schneller <david.schneller@tum.de>
2025-07-31 04:55:06 +0200

    Add a layout code draft

---
+++ src/Initializer/InitProcedure/InitLayout.cpp
@@ -0,0 +1,252 @@
+// SPDX-FileCopyrightText: 2025 SeisSol Group
+//
+// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-LicenseComments: Full text under /LICENSE and /LICENSES/
+//
+// SPDX-FileContributor: Author lists in /AUTHORS and /CITATION.cff
+#include "InitLayout.h"
+#include "Internal/MeshLayout.h"
+#include "SeisSol.h"
+#include <Common/Iterator.h>
+#include <Equations/Datastructures.h>
+#include <Geometry/MeshReader.h>
+#include <Initializer/BasicTypedefs.h>
+#include <Initializer/InitProcedure/Internal/Buckets.h>
+#include <Initializer/InitProcedure/Internal/LtsSetup.h>
+#include <Initializer/ParameterDB.h>
+#include <Initializer/TimeStepping/ClusterLayout.h>
+#include <Initializer/TimeStepping/Halo.h>
+#include <Memory/Tree/Backmap.h>
+#include <Memory/Tree/Colormap.h>
+#include <Memory/Tree/LTSTree.h>
+#include <Model/CommonDatastructures.h>
+#include <Model/Plasticity.h>
+#include <Parallel/MPI.h>
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <limits>
+#include <mpi.h>
+#include <unordered_map>
+#include <utility>
+#include <utils/logger.h>
+#include <vector>
+
+namespace {
+using namespace seissol::initializer;
+
+void setupMemory(seissol::SeisSol& seissolInstance) {
+  const auto& seissolParams = seissolInstance.getSeisSolParameters();
+  const auto& meshReader = seissolInstance.meshReader();
+
+  seissolInstance.getLtsLayout().deriveLayout(TimeClustering::MultiRate,
+                                              seissolParams.timeStepping.lts.getRate());
+  const auto clusterLayout = seissolInstance.getLtsLayout().clusterLayout();
+
+  auto& ltsStorage = seissolInstance.getMemoryManager().getLtsStorage();
+  auto& backmap = seissolInstance.getMemoryManager().getBackmap();
+
+  std::vector<std::size_t> clusterMap(clusterLayout.globalClusterCount);
+  std::iota(clusterMap.begin(), clusterMap.end(), 0);
+
+  LTSColorMap colorMap(
+      initializer::EnumLayer<HaloType>({HaloType::Ghost, HaloType::Copy, HaloType::Interior}),
+      initializer::EnumLayer<std::size_t>(clusterMap),
+      initializer::TraitLayer<initializer::ConfigVariant>({Config()}));
+
+  std::vector<std::size_t> colors(meshReader.getElements().size());
+  for (std::size_t i = 0; i < colors.size(); ++i) {
+    const auto& element = meshReader.getElements()[i];
+    const auto halo =
+        geometry::isCopy(element, MPI::mpi.rank()) ? HaloType::Copy : HaloType::Interior;
+    colors[i] = colorMap.color(halo, element.clusterId, Config());
+  }
+
+  std::size_t ghostSize = 0;
+  for (const auto& [_, neighbor] : meshReader.getGhostlayerMetadata()) {
+    ghostSize += neighbor.size();
+  }
+
+  std::vector<std::size_t> colorsGhost(ghostSize);
+  std::size_t linearId = 0;
+  std::map<std::pair<int, std::size_t>, std::size_t> toLinear;
+  std::vector<std::pair<int, std::size_t>> fromLinear(ghostSize);
+  for (const auto& [rank, neighbor] : meshReader.getGhostlayerMetadata()) {
+    for (const auto [i, element] : common::enumerate(neighbor)) {
+      const auto halo = HaloType::Ghost;
+      colorsGhost[linearId] = colorMap.color(halo, element.clusterId, Config());
+      toLinear[std::pair<int, std::size_t>(rank, i)] = linearId;
+      fromLinear[linearId] = std::pair<int, std::size_t>(rank, i);
+      ++linearId;
+    }
+  }
+
+  const auto meshLayout = internal::layoutCells(colors, colorsGhost, colorMap, meshReader);
+
+  std::vector<std::size_t> wpStructure(colorMap.size());
+  for (std::size_t i = 0; i < wpStructure.size(); ++i) {
+    wpStructure[i] = meshLayout[i].cellMap.size();
+  }
+
+  LTS::addTo(ltsStorage, seissolInstance.getSeisSolParameters().model.plasticity);
+  ltsStorage.setLayerCount(colorMap);
+  ltsStorage.fixate();
+  for (auto [i, layer] : common::enumerate(ltsStorage.leaves())) {
+    layer.setNumberOfCells(meshLayout[i].cellMap.size());
+  }
+  ltsStorage.allocateVariables();
+  ltsStorage.touchVariables();
+  backmap.setSize(meshReader.getElements().size());
+
+  StorageBackmap<1> backmapGhost;
+  backmapGhost.setSize(ghostSize);
+
+  logInfo() << "Setting up cell storage...";
+  // just need pick a test variable that is available everywhere
+
+  backmap.setSize(seissolInstance.meshReader().getElements().size());
+  const auto* zero = ltsStorage.var<LTS::SecondaryInformation>();
+  for (auto& layer : ltsStorage.leaves(Ghost)) {
+    auto* zeroLayer = layer.var<LTS::SecondaryInformation>();
+
+    const auto addToBackmapCopyInterior = [&](auto cell, auto index) {
+      return backmap.addElement(layer.id(), zero, zeroLayer, cell, index);
+    };
+    const auto addToBackmapGhost = [&](auto cell, auto index) {
+      return backmapGhost.addElement(layer.id(), zero, zeroLayer, cell, index);
+    };
+    const auto& addToBackmap = [&](auto cell, auto index) {
+      if (layer.getIdentifier().halo == HaloType::Interior ||
+          layer.getIdentifier().halo == HaloType::Copy) {
+        return addToBackmapCopyInterior(cell, index);
+      } else {
+        return addToBackmapGhost(cell, index);
+      }
+    };
+
+    const auto& layout = meshLayout[layer.id()];
+    for (const auto [i, cell] : common::enumerate(layout.cellMap)) {
+      // TODO: two boundary elements with the same source (relevant for Ghost)
+      auto dup = addToBackmap(cell, i);
+
+      zeroLayer[i].meshId = cell;
+      zeroLayer[i].configId = layer.getIdentifier().config.index();
+      zeroLayer[i].clusterId = layer.getIdentifier().lts;
+      zeroLayer[i].duplicate = dup;
+      zeroLayer[i].rank = -1;
+      for (std::size_t face = 0; face < Cell::NumFaces; ++face) {
+        zeroLayer[i].neighborRanks[face] = -1;
+      }
+    }
+  }
+
+  logInfo() << "Setting up cell information data...";
+  for (auto& layer : ltsStorage.leaves()) {
+    const auto& layout = meshLayout[colorMap.colorId(layer.getIdentifier())];
+    auto* secondaryCellInformation = layer.var<LTS::SecondaryInformation>();
+    auto* cellInformation = layer.var<LTS::CellInformation>();
+    const auto& cells = layout.cellMap;
+    if (layer.getIdentifier().halo == HaloType::Interior ||
+        layer.getIdentifier().halo == HaloType::Copy) {
+#ifdef _OPENMP
+#pragma omp parallel for schedule(static)
+#endif
+      for (std::size_t i = 0; i < cells.size(); ++i) {
+        const auto cell = cells[i];
+        const auto index = i;
+
+        secondaryCellInformation[index].rank = MPI::mpi.rank();
+        for (int face = 0; face < 4; ++face) {
+          const auto& element = meshReader.getElements()[cell];
+          secondaryCellInformation[index].neighborRanks[face] = element.neighborRanks[face];
+          cellInformation[index].faceTypes[face] = static_cast<FaceType>(element.boundaries[face]);
+          cellInformation[index].faceRelations[face][0] = element.neighborSides[face];
+          cellInformation[index].faceRelations[face][1] = element.sideOrientations[face];
+
+          if (element.neighbors[face] != meshReader.getElements().size() ||
+              element.neighborRanks[face] != MPI::mpi.rank()) {
+            const auto& neighbor = [&]() {
+              const bool ghostNeighbor = element.neighborRanks[face] != MPI::mpi.rank();
+              if (ghostNeighbor) {
+                const auto rank = element.neighborRanks[face];
+                const auto mpiIndex = element.mpiIndices[face];
+                // ghost layer
+                return backmapGhost.get(toLinear.at({rank, mpiIndex}));
+              } else {
+                // copy/interior layer
+                return backmap.get(element.neighbors[face]);
+              }
+            }();
+
+            secondaryCellInformation[index].faceNeighborIds[face] = neighbor.global;
+            cellInformation[index].neighborConfigIds[face] = 0;
+          } else {
+            secondaryCellInformation[index].faceNeighborIds[face] =
+                std::numeric_limits<unsigned>::max();
+            cellInformation[index].neighborConfigIds[face] = -1;
+          }
+        }
+      }
+    } else if (layer.getIdentifier().halo == HaloType::Ghost) {
+#ifdef _OPENMP
+#pragma omp parallel for schedule(static)
+#endif
+      for (std::size_t i = 0; i < cells.size(); ++i) {
+        const auto cell = cells[i];
+        const auto index = i;
+
+        const auto delinear = fromLinear[cell];
+        const auto& boundaryElement =
+            meshReader.getMPINeighbors().at(delinear.first).elements[delinear.second];
+        secondaryCellInformation[index].rank = delinear.first;
+        const auto neighbor = backmap.get(boundaryElement.localElement);
+        secondaryCellInformation[index].faceNeighborIds[boundaryElement.localSide] =
+            neighbor.global;
+
+        const int face = boundaryElement.neighborSide;
+
+        secondaryCellInformation[index].neighborRanks[face] = MPI::mpi.rank();
+        cellInformation[index].neighborConfigIds[face] = neighbor.color;
+        cellInformation[index].faceTypes[face] =
+            static_cast<FaceType>(meshReader.getElements()[boundaryElement.localElement]
+                                      .boundaries[boundaryElement.localSide]);
+        cellInformation[index].faceRelations[face][0] = boundaryElement.localSide;
+        cellInformation[index].faceRelations[face][1] =
+            meshReader.getElements()[boundaryElement.localElement]
+                .sideOrientations[boundaryElement.localSide];
+      }
+    }
+  }
+
+  // pass 3: LTS setup
+  logInfo() << "Setting up LTS configuration...";
+  auto halo = HaloStructure();
+  halo.copy.resize(clusterLayout.globalClusterCount);
+  halo.ghost.resize(clusterLayout.globalClusterCount);
+  for (const auto [i, layout] : common::enumerate(meshLayout)) {
+    const auto desc = colorMap.argument(i);
+    if (desc.halo == HaloType::Copy) {
+      halo.copy[desc.lts] = layout.regions;
+    }
+    if (desc.halo == HaloType::Ghost) {
+      halo.ghost[desc.lts] = layout.regions;
+    }
+  }
+  internal::deriveLtsSetups(halo, ltsStorage);
+
+  // pass 4: correct LTS setup, again. Do bucket setup, determine communication datastructures
+  // logInfo() << "Setting up data exchange and face displacements (buckets)...";
+  // internal::bucketsAndCommunication(ltsStorage, halo);
+}
+
+} // namespace
+
+namespace seissol::initializer::initprocedure {
+void initLayout(seissol::SeisSol& seissolInstance) {
+  logInfo() << "Begin init layout.";
+
+  setupMemory(seissolInstance);
+
+  logInfo() << "End init layout.";
+}
+} // namespace seissol::initializer::initprocedure
---
+++ src/Initializer/InitProcedure/Internal/MeshLayout.cpp
@@ -0,0 +1,121 @@
+// SPDX-FileCopyrightText: 2025 SeisSol Group
+//
+// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-LicenseComments: Full text under /LICENSE and /LICENSES/
+//
+// SPDX-FileContributor: Author lists in /AUTHORS and /CITATION.cff
+#include "MeshLayout.h"
+
+#include <Geometry/MeshReader.h>
+#include <Initializer/BasicTypedefs.h>
+#include <Memory/Tree/Colormap.h>
+#include <cstddef>
+#include <unordered_map>
+#include <vector>
+
+namespace seissol::initializer::internal {
+
+std::vector<ClusterLayout> layoutCells(const std::vector<std::size_t>& color,
+                                       const std::vector<std::size_t>& ghostColor,
+                                       const LTSColorMap& colormap,
+                                       const geometry::MeshReader& meshReader) {
+  // add cells to color clusters
+  std::vector<ClusterLayout> clusters(colormap.size());
+
+  // layout interior/copy w/o halo
+  for (std::size_t i = 0; i < meshReader.getElements().size(); ++i) {
+    clusters[color[i]].cellMap.push_back(i);
+  }
+
+  // prepare halos
+  std::map<std::pair<int, std::size_t>, std::size_t> toLinear;
+  std::unordered_map<std::size_t, std::pair<int, std::size_t>> fromLinear;
+  std::size_t linearId = 0;
+  for (const auto& [rank, cells] : meshReader.getMPINeighbors()) {
+    for (const auto [i, cell] : common::enumerate(cells.elements)) {
+      clusters[ghostColor[linearId]].cellMap.push_back(linearId);
+
+      toLinear[std::pair<int, std::size_t>{rank, i}] = linearId;
+      fromLinear[linearId] = std::pair<int, std::size_t>{rank, i};
+      ++linearId;
+    }
+  }
+
+  // we keep the "old" layout:
+  // * ghost and copy colors are matched to each other
+  // * copy cells are duplicated, once per ghost color
+
+  // clustered halo
+  for (std::size_t i = 0; i < clusters.size(); ++i) {
+    if (colormap.argument(i).halo == HaloType::Copy) {
+      std::map<std::pair<int, std::size_t>, std::vector<std::size_t>> byColor;
+      std::size_t newCount = 0;
+      for (std::size_t j = 0; j < clusters[i].cellMap.size(); ++j) {
+        const auto cell = clusters[i].cellMap[j];
+        std::set<std::pair<int, std::size_t>> neighborColors;
+        for (std::size_t f = 0; f < Cell::NumFaces; ++f) {
+          // TODO: access linear ghost ID here
+          const auto& element = meshReader.getElements()[cell];
+
+          const auto ghostLinear = toLinear.at({element.neighborRanks[f], element.mpiIndices[f]});
+          const auto colorGhost = ghostColor[ghostLinear];
+          neighborColors.emplace(element.neighborRanks[f], colorGhost);
+        }
+        for (const auto& neighborColor : neighborColors) {
+          byColor[neighborColor].push_back(cell);
+        }
+        newCount += neighborColors.size();
+      }
+
+      clusters[i].cellMap.clear();
+      clusters[i].cellMap.reserve(newCount);
+      for (const auto& [id, cells] : byColor) {
+        const auto [rank, color] = id;
+
+        std::vector cellsCopy(cells.begin(), cells.end());
+        std::sort(cellsCopy.begin(), cellsCopy.end(), [&](const auto& a, const auto& b) {
+          return meshReader.getElements()[a].globalId < meshReader.getElements()[b].globalId;
+        });
+        clusters[i].cellMap.insert(clusters[i].cellMap.end(), cellsCopy.begin(), cellsCopy.end());
+        const auto tag = i + color * colormap.size();
+        clusters[i].regions.emplace_back(tag, i, color, cells.size(), rank);
+      }
+    } else {
+      const auto& neighbor = meshReader.getMPINeighbors();
+      const auto& ghostlayer = meshReader.getGhostlayerMetadata();
+      std::map<std::pair<int, std::size_t>, std::vector<std::size_t>> byColor;
+      std::size_t newCount = 0;
+      for (std::size_t j = 0; j < clusters[i].cellMap.size(); ++j) {
+        const auto cell = clusters[i].cellMap[j];
+        const auto dCell = fromLinear[cell];
+        const auto& metadata = ghostlayer.at(dCell.first)[dCell.second];
+        const auto& localInfo = neighbor.at(dCell.first).elements[dCell.second];
+        const auto copyColor = color[localInfo.localElement];
+        byColor[{dCell.first, copyColor}].emplace_back(cell);
+        newCount += 1;
+      }
+
+      clusters[i].cellMap.clear();
+      clusters[i].cellMap.reserve(newCount);
+      for (const auto& [id, cells] : byColor) {
+        const auto [rank, color] = id;
+
+        std::vector cellsCopy(cells.begin(), cells.end());
+        std::sort(cellsCopy.begin(), cellsCopy.end(), [&](const auto& a, const auto& b) {
+          const auto& dA = fromLinear[a];
+          const auto& dB = fromLinear[b];
+          const auto aId = ghostlayer.at(dA.first)[dA.second].globalId;
+          const auto bId = ghostlayer.at(dB.first)[dB.second].globalId;
+          return aId < bId;
+        });
+        clusters[i].cellMap.insert(clusters[i].cellMap.end(), cellsCopy.begin(), cellsCopy.end());
+        const auto tag = i * colormap.size() + color;
+        clusters[i].regions.emplace_back(tag, i, color, cells.size(), rank);
+      }
+    }
+  }
+
+  return clusters;
+}
+
+} // namespace seissol::initializer::internal
