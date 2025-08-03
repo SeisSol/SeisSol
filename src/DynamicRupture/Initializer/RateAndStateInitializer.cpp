39a2a24a2292b249999b1d7f8e1bd54f4df719c5
David Schneller <david.schneller@tum.de>
2025-08-04 01:15:28 +0200

    More double precision in the R+S init

--- src/DynamicRupture/Initializer/RateAndStateInitializer.cpp
+++ src/DynamicRupture/Initializer/RateAndStateInitializer.cpp
@@ -37,7 +37,7 @@ void RateAndStateInitializer::initializeFault(DynamicRupture::Storage& drStorage
     real(*rsA)[misc::NumPaddedPoints<Cfg>] = layer.var<LTSRateAndState::RsA>();
     auto* initialStressInFaultCS = layer.var<LTSRateAndState::InitialStressInFaultCS>();
 
-    const real initialSlipRate =
+    const double initialSlipRate =
         misc::magnitude(drParameters->rsInitialSlipRate1, drParameters->rsInitialSlipRate2);
 
     using namespace dr::misc::quantity_indices;
@@ -65,15 +65,15 @@ void RateAndStateInitializer::initializeFault(DynamicRupture::Storage& drStorage
 }
 
 RateAndStateInitializer::StateAndFriction
-    RateAndStateInitializer::computeInitialStateAndFriction(real traction1,
-                                                            real traction2,
-                                                            real pressure,
-                                                            real rsA,
-                                                            real rsB,
-                                                            real rsSl0,
-                                                            real rsSr0,
-                                                            real rsF0,
-                                                            real initialSlipRate) {
+    RateAndStateInitializer::computeInitialStateAndFriction(double traction1,
+                                                            double traction2,
+                                                            double pressure,
+                                                            double rsA,
+                                                            double rsB,
+                                                            double rsSl0,
+                                                            double rsSr0,
+                                                            double rsF0,
+                                                            double initialSlipRate) {
   StateAndFriction result{};
   const double absoluteTraction = misc::magnitude(traction1, traction2);
   const double tmp = std::abs(absoluteTraction / (rsA * pressure));
@@ -101,18 +101,18 @@ void RateAndStateInitializer::addAdditionalParameters(
 }
 
 RateAndStateInitializer::StateAndFriction
-    RateAndStateFastVelocityInitializer::computeInitialStateAndFriction(real traction1,
-                                                                        real traction2,
-                                                                        real pressure,
-                                                                        real rsA,
-                                                                        real rsB,
-                                                                        real rsSl0,
-                                                                        real rsSr0,
-                                                                        real rsF0,
-                                                                        real initialSlipRate) {
+    RateAndStateFastVelocityInitializer::computeInitialStateAndFriction(double traction1,
+                                                                        double traction2,
+                                                                        double pressure,
+                                                                        double rsA,
+                                                                        double rsB,
+                                                                        double rsSl0,
+                                                                        double rsSr0,
+                                                                        double rsF0,
+                                                                        double initialSlipRate) {
   StateAndFriction result{};
-  const real absoluteTraction = misc::magnitude(traction1, traction2);
-  const real tmp = std::abs(absoluteTraction / (rsA * pressure));
+  const double absoluteTraction = misc::magnitude(traction1, traction2);
+  const double tmp = std::abs(absoluteTraction / (rsA * pressure));
   result.stateVariable =
       rsA * std::log(2.0 * rsSr0 / initialSlipRate * (std::exp(tmp) - std::exp(-tmp)) / 2.0);
   if (result.stateVariable < 0) {
@@ -120,7 +120,7 @@ RateAndStateInitializer::StateAndFriction
         << "Found a negative state variable while initializing the fault. Are you sure your "
            "setup is correct?";
   }
-  const real tmp2 = initialSlipRate * 0.5 / rsSr0 * std::exp(result.stateVariable / rsA);
+  const double tmp2 = initialSlipRate * 0.5 / rsSr0 * std::exp(result.stateVariable / rsA);
   result.frictionCoefficient = rsA * std::asinh(tmp2);
   return result;
 }
