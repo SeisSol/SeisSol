35f317636d3b01d55e1f62296a40d26820c70ef3
David Schneller <david.schneller@tum.de>
2025-09-24 15:00:33 +0200

    Fix SVWL F0 usage

--- src/DynamicRupture/FrictionLaws/CpuImpl/SevereVelocityWeakeningLaw.h
+++ src/DynamicRupture/FrictionLaws/CpuImpl/SevereVelocityWeakeningLaw.h
@@ -45,6 +45,7 @@ class SevereVelocityWeakeningLaw
   struct MuDetails {
     std::array<double, misc::NumPaddedPoints> a{};
     std::array<double, misc::NumPaddedPoints> c{};
+    std::array<double, misc::NumPaddedPoints> f0{};
   };
 
   MuDetails getMuDetails(std::size_t ltsFace,
@@ -59,6 +60,7 @@ class SevereVelocityWeakeningLaw
                        (static_cast<double>(localStateVariable[pointIndex]) + localSl0);
       details.a[pointIndex] = localA;
       details.c[pointIndex] = c;
+      details.f0[pointIndex] = this->f0[ltsFace][pointIndex];
     }
     return details;
   }
@@ -66,7 +68,7 @@ class SevereVelocityWeakeningLaw
 #pragma omp declare simd
   double
       updateMu(std::uint32_t pointIndex, double localSlipRateMagnitude, const MuDetails& details) {
-    return this->drParameters->rsF0 +
+    return details.f0[pointIndex] +
            details.a[pointIndex] * localSlipRateMagnitude /
                (localSlipRateMagnitude + this->drParameters->rsSr0) -
            details.c[pointIndex];
