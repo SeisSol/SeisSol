d351349fc2940265f7241dc879bf2acee3b6dbd2
David Schneller <david.schneller@tum.de>
2025-10-06 05:33:22 +0200

    Add custom IO comms

--- src/IO/Writer/Writer.cpp
+++ src/IO/Writer/Writer.cpp
@@ -68,8 +68,8 @@ std::string Writer::serialize() {
   return sstr.str();
 }
 
-WriteInstance Writer::beginWrite(const async::ExecInfo& info) {
-  WriteInstance instance(MPI_COMM_WORLD);
+WriteInstance Writer::beginWrite(const async::ExecInfo& info, MPI_Comm comm) {
+  WriteInstance instance(comm);
   for (const auto& instruction : instructions) {
     instance.write(info, instruction);
   }
