ba55993dd66d8b96216907451426726fdb1b35a0
David Schneller <david.schneller@tum.de>
2025-10-28 19:02:34 +0100

    Barrier test

--- src/Kernels/LinearCK/DeviceAux/cudahip/KernelsAux.cpp
+++ src/Kernels/LinearCK/DeviceAux/cudahip/KernelsAux.cpp
@@ -184,6 +184,158 @@ void taylorSum(
 } // namespace seissol::kernels::time::aux
 
 namespace {
+__launch_bounds__(512) __global__ void kernel_local3(const float** A,
+                                                     const float** B,
+                                                     unsigned Boffset,
+                                                     const float* C1,
+                                                     const float* C2,
+                                                     const float* C3,
+                                                     const float* C4,
+                                                     float** D,
+                                                     size_t numElements,
+                                                     const unsigned* flags) {
+  // meta data:
+  // A = {rows: 64, cols: 9, addr: pointer_based, bbox: [np.int64(0), np.int64(0), np.int64(64),
+  // np.int64(9)]}; B = {rows: 9, cols: 9, addr: pointer_based, bbox: [np.int64(0), np.int64(0),
+  // np.int64(9), np.int64(9)]}; C = {rows: 56, cols: 56, addr: none, bbox: [np.int64(0),
+  // np.int64(0), np.int64(56), np.int64(56)]}; D = {rows: 64, cols: 9, addr: pointer_based, bbox:
+  // [np.int64(0), np.int64(0), np.int64(56), np.int64(9)]};
+
+  // tmp0 = 1.0 * A x B
+  // D = 1.0 * C x tmp0 + 1.0 * D
+
+  constexpr int Quantities = 9;
+  constexpr int Faces = 4;
+
+  constexpr int Count = Faces * Quantities * Quantities;
+  constexpr int CountH = Count / 64;
+  constexpr int CountR = Count % 64;
+
+  __shared__ __align__(8) float total_shrmem0[(576 + Count) * 8];
+
+  const auto tid_x = threadIdx.x;
+  unsigned batchId = threadIdx.y + blockDim.y * blockIdx.x;
+  if (batchId < numElements) {
+    const float* const __restrict__ glbA = A[batchId];
+    const float* const __restrict__ glbB = B[batchId] + Boffset;
+    const float* const __restrict__ glbC1 = C1;
+    const float* const __restrict__ glbC2 = C2;
+    const float* const __restrict__ glbC3 = C3;
+    const float* const __restrict__ glbC4 = C4;
+    float* const __restrict__ glbD = D[batchId];
+
+    const bool has1 = (flags[batchId] & 1) != 0;
+    const bool has2 = (flags[batchId] & 2) != 0;
+    const bool has3 = (flags[batchId] & 4) != 0;
+    const bool has4 = (flags[batchId] & 8) != 0;
+
+    float reg0[Faces][Quantities]{};
+    float reg1[Quantities]{};
+
+    float* shrmem0 = &total_shrmem0[(576 + Count) * threadIdx.y];
+
+    // writing to shr mem: from reg0 to _1
+    float* __restrict__ _1 = &shrmem0[0];
+#pragma unroll
+    for (int i = 0; i < Quantities; ++i) {
+      _1[tid_x + i * 64] = __builtin_nontemporal_load(&glbA[tid_x + i * 64]);
+    }
+
+    float* __restrict__ _0 = &shrmem0[576];
+
+    // load all 4 9×9 matrices
+    // loading glbB to _0: # no trans, extended
+#pragma unroll
+    for (int i = 0; i < CountH; ++i) {
+      _0[tid_x + i * 64] = glbB[tid_x + i * 64];
+    }
+    if (tid_x < CountR) {
+      _0[tid_x + CountH * 64] = glbB[tid_x + CountH * 64];
+    }
+
+#pragma unroll
+    for (int n = 0; n < Quantities; ++n) {
+      reg1[n] = __builtin_nontemporal_load(&glbD[tid_x + n * 64]);
+    }
+
+    __builtin_amdgcn_sched_group_barrier(0x220, Quantities + CountH + (CountR > 0), 0);
+
+    constexpr int Cache = 8;
+
+    // gemm: glbC x _1
+    if (tid_x < 56) {
+
+// #pragma unroll
+      for (int k = 0; k < 56; k += Cache) {
+        float values[Faces][Cache]{};
+        if (has1) {
+#pragma unroll
+          for (int kk = 0; kk < Cache; ++kk) {
+            values[0][kk] = glbC1[tid_x + (k + kk) * 56];
+          }
+        }
+        if (has2) {
+#pragma unroll
+          for (int kk = 0; kk < Cache; ++kk) {
+            values[1][kk] = glbC2[tid_x + (k + kk) * 56];
+          }
+        }
+        if (has3) {
+#pragma unroll
+          for (int kk = 0; kk < Cache; ++kk) {
+            values[2][kk] = glbC3[tid_x + (k + kk) * 56];
+          }
+        }
+        if (has4) {
+#pragma unroll
+          for (int kk = 0; kk < Cache; ++kk) {
+            values[3][kk] = glbC4[tid_x + (k + kk) * 56];
+          }
+        }
+
+        #pragma unroll
+        for (int n = 0; n < Quantities; ++n) {
+          float local[Cache]{};
+#pragma unroll
+          for (int kk = 0; kk < Cache; ++kk) {
+            local[kk] = _1[(k + kk) + n * 64];
+          }
+#pragma unroll
+          for (int kk = 0; kk < Cache; ++kk) {
+#pragma unroll
+            for (int d = 0; d < Faces; ++d) {
+              reg0[d][n] += values[d][kk] * local[kk];
+            }
+          }
+
+          __builtin_amdgcn_sched_group_barrier(0x0100, Cache, 0);
+        }
+
+        __builtin_amdgcn_sched_group_barrier(0x0020, 8 * Faces, 0);
+      }
+    }
+
+    // gemm: glbA x _0
+#pragma unroll
+    for (int d = 0; d < Faces; ++d) {
+#pragma unroll
+      for (int n = 0; n < Quantities; ++n) {
+#pragma unroll
+        for (int k = 0; k < Quantities; ++k) {
+          reg1[n] += reg0[d][k] * _0[k + n * Quantities + Quantities * Quantities * d];
+        }
+        __builtin_amdgcn_sched_group_barrier(0x0100, 8, 0);
+      }
+      // __builtin_amdgcn_sched_group_barrier(0x0100, 8, 0);
+    }
+
+    // write results back to glb. memory
+#pragma unroll
+    for (int n = 0; n < Quantities; ++n) {
+      __builtin_nontemporal_store(reg1[n], &glbD[tid_x + n * 64]);
+    }
+  }
+}
 
 __launch_bounds__(512) __global__ void kernel_local2(const float** A,
                                                      const float** B,
@@ -547,7 +699,7 @@ void launch_local(const float** A,
   dim3 block(64, 8, 1);
   dim3 grid((numElements + 8 - 1) / 8, 1, 1);
   hipStream_t stream = (streamPtr != nullptr) ? static_cast<hipStream_t>(streamPtr) : 0;
-  kernel_local<<<grid, block, 0, stream>>>(A, B, Boffset, C1, C2, C3, C4, D, numElements, flags);
+  kernel_local3<<<grid, block, 0, stream>>>(A, B, Boffset, C1, C2, C3, C4, D, numElements, flags);
 }
 } // namespace seissol::kernels::local::aux
 
