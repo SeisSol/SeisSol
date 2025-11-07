44642bf3d6cb410908e6237f13569d5d7d96a11d
David Schneller <david.schneller@tum.de>
2025-11-07 20:58:54 +0100

    Move tests directory to toplevel

---
+++ tests/Model/GodunovState.t.h
@@ -0,0 +1,74 @@
+// SPDX-FileCopyrightText: 2021 SeisSol Group
+//
+// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-LicenseComments: Full text under /LICENSE and /LICENSES/
+//
+// SPDX-FileContributor: Author lists in /AUTHORS and /CITATION.cff
+
+#include "Equations/Datastructures.h"
+#include "Equations/Setup.h"
+#include "Equations/acoustic/Model/Datastructures.h"
+#include "Model/Common.h"
+#include "Values.h"
+
+#include <cmath>
+#include "TestHelper.h"
+
+namespace seissol::unit_test {
+template <typename T>
+void test_matrix(init::QgodLocal::view::type& qgodLocal, const T& solution, double epsilon) {
+  // compute the Frobenius norms squared: ||QgodLocal - solution||^2 and ||solution||^2
+  double frobDiffSquared = 0.0;
+  double frobASquared = 0.0;
+  for (unsigned int i = 0; i < solution[0].size(); i++) {
+    for (unsigned int j = 0; j < solution.size(); j++) {
+      const auto diff = (qgodLocal(i, j) - solution[j][i]);
+      frobDiffSquared += diff * diff;
+      frobASquared += solution[j][i] * solution[j][i];
+    }
+  }
+  // Check whether ||QgodLocal - solution||^2 < epsilon^2 * ||solution||^2
+  REQUIRE(std::abs(frobDiffSquared) < epsilon * frobASquared * epsilon * frobASquared);
+}
+
+inline void test_nan(init::QgodLocal::view::type& qgodNeighbor) {
+  for (unsigned int i = 0; i < qgodNeighbor.shape(0); i++) {
+    for (unsigned int j = 0; j < qgodNeighbor.shape(1); j++) {
+      REQUIRE(std::isnan(qgodNeighbor(i, j)));
+    }
+  }
+}
+
+// TODO: add acoustic Godunov state
+TEST_CASE("Godunov state is correct" *
+          doctest::skip(std::is_same_v<model::MaterialT, model::AcousticMaterial>)) {
+  constexpr real Epsilon = 1e2 * std::numeric_limits<real>::epsilon();
+
+  real localData[tensor::QgodLocal::size()];
+  real neighborData[tensor::QgodLocal::size()];
+  init::QgodLocal::view::type qgodLocal = init::QgodLocal::view::create(localData);
+  init::QgodNeighbor::view::type qgodNeighbor = init::QgodNeighbor::view::create(neighborData);
+  qgodLocal.setZero();
+  qgodNeighbor.setZero();
+
+  // test homogeneous material
+  const model::MaterialT local(SolutionData<model::MaterialT>::MaterialVal1);
+  model::MaterialT neighbor(SolutionData<model::MaterialT>::MaterialVal1);
+
+  model::getTransposedGodunovState(local, neighbor, FaceType::Regular, qgodLocal, qgodNeighbor);
+  test_matrix(qgodLocal, SolutionData<model::MaterialT>::SolutionHomogeneousLocal, Epsilon);
+  test_matrix(qgodNeighbor, SolutionData<model::MaterialT>::SolutionHomogeneousNeighbor, Epsilon);
+
+  // test free surface
+  model::getTransposedGodunovState(local, neighbor, FaceType::FreeSurface, qgodLocal, qgodNeighbor);
+  test_matrix(qgodLocal, SolutionData<model::MaterialT>::SolutionBoundary, Epsilon);
+  test_nan(qgodNeighbor);
+
+  // test heterogeneous material
+  neighbor = model::MaterialT(SolutionData<model::MaterialT>::MaterialVal2);
+
+  model::getTransposedGodunovState(local, neighbor, FaceType::Regular, qgodLocal, qgodNeighbor);
+  test_matrix(qgodLocal, SolutionData<model::MaterialT>::SolutionHeterogeneousLocal, Epsilon);
+  test_matrix(qgodNeighbor, SolutionData<model::MaterialT>::SolutionHeterogeneousNeighbor, Epsilon);
+}
+} // namespace seissol::unit_test
---
+++ tests/Numerical/GaussianNucleation.t.h
@@ -0,0 +1,47 @@
+// SPDX-FileCopyrightText: 2022 SeisSol Group
+//
+// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-LicenseComments: Full text under /LICENSE and /LICENSES/
+//
+// SPDX-FileContributor: Author lists in /AUTHORS and /CITATION.cff
+
+#include "doctest.h"
+
+#include "Kernels/Precision.h"
+#include "Numerical/GaussianNucleationFunction.h"
+#include "TestHelper.h"
+
+#include <cassert>
+
+namespace seissol::unit_test {
+/**
+ * Reference implementation for the Gaussian Nucleation function
+ */
+template <typename T>
+inline T gaussianNucleation(T time, T dt, T tau) {
+  const auto step = [&tau](T t) {
+    if (t < 0) {
+      return static_cast<T>(0.0);
+    } else if (t > tau) {
+      return static_cast<T>(1.0);
+    } else {
+      return std::exp((t - tau) * (t - tau) / (t * (t - 2 * tau)));
+    }
+  };
+  return step(time) - step(time - dt);
+}
+
+TEST_CASE_TEMPLATE("Gaussian Nucleation Function", RealT, float, double) {
+  constexpr RealT Dt = 0.01;
+  constexpr RealT Epsilon = 1e-4;
+  for (const RealT effectiveRiseTime : {0.8, 1.0}) {
+    for (int i = -10; i < 111; i++) {
+      const auto stfEvaluated = seissol::gaussianNucleationFunction::smoothStepIncrement<RealT>(
+          i * Dt, Dt, effectiveRiseTime);
+      const auto referenceEvaluated = gaussianNucleation<RealT>(i * Dt, Dt, effectiveRiseTime);
+      REQUIRE(stfEvaluated == AbsApprox(referenceEvaluated).epsilon(Epsilon));
+    }
+  }
+}
+
+} // namespace seissol::unit_test
