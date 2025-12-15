cef9fd80dab69ae9f01b2fb71736cee013ceca92
David Schneller <david.schneller@tum.de>
2025-12-15 09:28:35 +0100

    Explicitly port the FL args

--- src/DynamicRupture/FrictionLaws/GpuImpl/BaseFrictionSolverSycl.cpp
+++ src/DynamicRupture/FrictionLaws/GpuImpl/BaseFrictionSolverSycl.cpp
@@ -49,11 +49,12 @@ void BaseFrictionSolver<T>::evaluateKernel(seissol::parallel::runtime::StreamRun
     sycl::local_accessor<real> sharedMemory(misc::NumPaddedPoints, cgh);
 
     cgh.parallel_for(rng, [=](sycl::nd_item<1> item) {
+      FrictionLawArgs argsLocal = args;
       FrictionLawContext ctx{};
       ctx.sharedMemory = &sharedMemory[0];
       ctx.item = reinterpret_cast<void*>(&item);
-      ctx.data = args.data;
-      ctx.args = &args;
+      ctx.data = argsLocal.data;
+      ctx.args = &argsLocal;
 
       const auto ltsFace = item.get_group().get_group_id(0);
       const auto pointIndex = item.get_local_id(0);
