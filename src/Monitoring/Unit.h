0bdc7ca54ea439a2a855a9c1b985f34d82145cf4
David Schneller <david.schneller@tum.de>
2026-01-03 23:34:38 +0100

    Add infoprinting the cell count

--- src/Monitoring/Unit.h
+++ src/Monitoring/Unit.h
@@ -9,12 +9,16 @@
 #define SEISSOL_SRC_MONITORING_UNIT_H_
 
 #include <cmath>
+#include <cstdint>
 #include <optional>
 #include <sstream>
 #include <string>
 #include <vector>
 
 namespace seissol {
+
+std::string formatInteger(uint64_t value);
+
 struct SIUnit {
   public:
   SIUnit(const std::string& unit, bool binary);
