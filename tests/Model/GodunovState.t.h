a416fc1b02fabdd9c3385b73590943f9639de656
David Schneller <david.schneller@tum.de>
2026-03-18 00:56:48 +0100

    refactor: Riemann problem matrix tests

--- tests/Model/GodunovState.t.h
+++ tests/Model/GodunovState.t.h
@@ -28,13 +28,31 @@ void test_matrix(init::QgodLocal::view::type& qgodLocal, const T& solution, doub
     }
   }
   // Check whether ||QgodLocal - solution||^2 < epsilon^2 * ||solution||^2
-  REQUIRE(std::abs(frobDiffSquared) < epsilon * frobASquared * epsilon * frobASquared);
+  CHECK(std::abs(frobDiffSquared) < epsilon * frobASquared * epsilon * frobASquared);
+}
+
+void test_consistency(init::QgodLocal::view::type& qgodLocal,
+                      init::QgodLocal::view::type& qgodNeighbor,
+                      double epsilon) {
+  // compute the Frobenius norms squared: ||QgodLocal - solution||^2 and ||solution||^2
+  double diffSquared = 0.0;
+  double frobASquared = 0.0;
+  for (unsigned int i = 0; i < qgodNeighbor.shape(0); i++) {
+    for (unsigned int j = 0; j < qgodNeighbor.shape(1); j++) {
+      const auto sol = i == j ? 1.0 : 0.0;
+      const auto diff = (qgodLocal(i, j) + qgodNeighbor(i, j)) - sol;
+      diffSquared += diff * diff;
+      frobASquared += sol;
+    }
+  }
+  // Check whether ||QgodLocal - solution||^2 < epsilon^2 * ||solution||^2
+  CHECK(std::abs(diffSquared) < epsilon * frobASquared * epsilon * frobASquared);
 }
 
 inline void test_nan(init::QgodLocal::view::type& qgodNeighbor) {
   for (unsigned int i = 0; i < qgodNeighbor.shape(0); i++) {
     for (unsigned int j = 0; j < qgodNeighbor.shape(1); j++) {
-      REQUIRE(std::isnan(qgodNeighbor(i, j)));
+      CHECK(std::isnan(qgodNeighbor(i, j)));
     }
   }
 }
@@ -42,31 +60,45 @@ inline void test_nan(init::QgodLocal::view::type& qgodNeighbor) {
 TEST_CASE("Godunov state is correct" * doctest::test_suite("model")) {
   constexpr real Epsilon = 1e2 * std::numeric_limits<real>::epsilon();
 
-  real localData[tensor::QgodLocal::size()];
-  real neighborData[tensor::QgodLocal::size()];
+  real localData[tensor::QgodLocal::size()]{};
+  real neighborData[tensor::QgodNeighbor::size()]{};
   init::QgodLocal::view::type qgodLocal = init::QgodLocal::view::create(localData);
   init::QgodNeighbor::view::type qgodNeighbor = init::QgodNeighbor::view::create(neighborData);
   qgodLocal.setZero();
   qgodNeighbor.setZero();
 
-  // test homogeneous material
-  const model::MaterialT local(SolutionData<model::MaterialT>::MaterialVal1);
-  model::MaterialT neighbor(SolutionData<model::MaterialT>::MaterialVal1);
+  SUBCASE("Homogenous material") {
+    // material 1 vs 1
+    const model::MaterialT local(SolutionData<model::MaterialT>::MaterialVal1);
+    const model::MaterialT neighbor(SolutionData<model::MaterialT>::MaterialVal1);
 
-  model::getTransposedGodunovState(local, neighbor, FaceType::Regular, qgodLocal, qgodNeighbor);
-  test_matrix(qgodLocal, SolutionData<model::MaterialT>::SolutionHomogeneousLocal, Epsilon);
-  test_matrix(qgodNeighbor, SolutionData<model::MaterialT>::SolutionHomogeneousNeighbor, Epsilon);
+    model::getTransposedGodunovState(local, neighbor, FaceType::Regular, qgodLocal, qgodNeighbor);
+    test_matrix(qgodLocal, SolutionData<model::MaterialT>::SolutionHomogeneousLocal, Epsilon);
+    test_matrix(qgodNeighbor, SolutionData<model::MaterialT>::SolutionHomogeneousNeighbor, Epsilon);
+    test_consistency(qgodLocal, qgodNeighbor, Epsilon);
+  }
 
-  // test free surface
-  model::getTransposedGodunovState(local, neighbor, FaceType::FreeSurface, qgodLocal, qgodNeighbor);
-  test_matrix(qgodLocal, SolutionData<model::MaterialT>::SolutionBoundary, Epsilon);
-  test_nan(qgodNeighbor);
+  SUBCASE("Free surface material") {
+    // material 1 vs 1
+    const model::MaterialT local(SolutionData<model::MaterialT>::MaterialVal1);
+    const model::MaterialT neighbor(SolutionData<model::MaterialT>::MaterialVal1);
 
-  // test heterogeneous material
-  neighbor = model::MaterialT(SolutionData<model::MaterialT>::MaterialVal2);
+    model::getTransposedGodunovState(
+        local, neighbor, FaceType::FreeSurface, qgodLocal, qgodNeighbor);
+    test_matrix(qgodLocal, SolutionData<model::MaterialT>::SolutionBoundary, Epsilon);
+    test_nan(qgodNeighbor);
+  }
 
-  model::getTransposedGodunovState(local, neighbor, FaceType::Regular, qgodLocal, qgodNeighbor);
-  test_matrix(qgodLocal, SolutionData<model::MaterialT>::SolutionHeterogeneousLocal, Epsilon);
-  test_matrix(qgodNeighbor, SolutionData<model::MaterialT>::SolutionHeterogeneousNeighbor, Epsilon);
+  SUBCASE("Heterogenous material") {
+    // material 1 vs 2
+    const model::MaterialT local(SolutionData<model::MaterialT>::MaterialVal1);
+    const model::MaterialT neighbor(SolutionData<model::MaterialT>::MaterialVal2);
+
+    model::getTransposedGodunovState(local, neighbor, FaceType::Regular, qgodLocal, qgodNeighbor);
+    test_matrix(qgodLocal, SolutionData<model::MaterialT>::SolutionHeterogeneousLocal, Epsilon);
+    test_matrix(
+        qgodNeighbor, SolutionData<model::MaterialT>::SolutionHeterogeneousNeighbor, Epsilon);
+    test_consistency(qgodLocal, qgodNeighbor, Epsilon);
+  }
 }
 } // namespace seissol::unit_test
