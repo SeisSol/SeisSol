09a223562ccb6b21360b850d3eb12c834fda6b5a
David Schneller <david.schneller@tum.de>
2026-07-04 20:10:22 +0200

    fix: coverage CI test

--- src/Common/ConfigHelper.cpp
+++ src/Common/ConfigHelper.cpp
@@ -37,18 +37,22 @@ std::string configToDescriptor() {
   str << "Convergence order: " << Cfg::ConvergenceOrder << "\n";
   str << "Precision: " << stringRealType(Cfg::Precision) << "\n";
   str << "Simulation count: " << Cfg::NumSimulations << "\n";
-  str << "Dynamic rupture quadrature rule: \"" << []() -> std::string {
-    switch (Cfg::DRQuadRule) {
-    case DRQuadRuleType::Dunavant:
-      return "Dunavant";
-    case DRQuadRuleType::Stroud:
-      return "Stroud";
-    case DRQuadRuleType::WitherdenVincent:
-      return "Witherden-Vincent";
-    default:
-      return "Unknown (consult the developers for more info)";
-    }
-  }() << "\"\n";
+  str << "Dynamic rupture quadrature rule: \"";
+  switch (Cfg::DRQuadRule) {
+  case DRQuadRuleType::Dunavant:
+    str << "Dunavant";
+    break;
+  case DRQuadRuleType::Stroud:
+    str << "Stroud";
+    break;
+  case DRQuadRuleType::WitherdenVincent:
+    str << "Witherden-Vincent";
+    break;
+  default:
+    str << "Unknown (consult the developers for more info)";
+    break;
+  }
+  str << "\"\n";
 
   return str.str();
 }
