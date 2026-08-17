8ce6a6ed3c383f5575d564d9751ccd6248ca4d17
David Schneller <david.schneller@tum.de>
2026-08-18 01:26:23 +0200

    test: always compare in double for Godunov states

--- tests/Model/GodunovState.t.h
+++ tests/Model/GodunovState.t.h
@@ -15,14 +15,24 @@
 #include <cmath>
 
 namespace seissol::unit_test {
+
+// silence double == real cast
+inline double castReal(real value) {
+  if constexpr (std::is_same_v<double, real>) {
+    return value;
+  } else {
+    return static_cast<double>(value);
+  }
+}
+
 template <typename T>
-void test_matrix(init::QgodLocal::view::type& qgodLocal, const T& solution, double epsilon) {
+void testMatrix(init::QgodLocal::view::type& qgodLocal, const T& solution, double epsilon) {
   // compute the Frobenius norms squared: ||QgodLocal - solution||^2 and ||solution||^2
   double frobDiffSquared = 0.0;
   double frobASquared = 0.0;
   for (unsigned int i = 0; i < solution[0].size(); i++) {
     for (unsigned int j = 0; j < solution.size(); j++) {
-      const auto diff = (qgodLocal(i, j) - solution[j][i]);
+      const auto diff = (castReal(qgodLocal(i, j)) - solution[j][i]);
       frobDiffSquared += diff * diff;
       frobASquared += solution[j][i] * solution[j][i];
     }
@@ -31,16 +41,16 @@ void test_matrix(init::QgodLocal::view::type& qgodLocal, const T& solution, doub
   CHECK(std::abs(frobDiffSquared) < epsilon * frobASquared * epsilon * frobASquared);
 }
 
-inline void test_consistency(init::QgodLocal::view::type& qgodLocal,
-                             init::QgodLocal::view::type& qgodNeighbor,
-                             double epsilon) {
+inline void testConsistency(init::QgodLocal::view::type& qgodLocal,
+                            init::QgodLocal::view::type& qgodNeighbor,
+                            double epsilon) {
   // compute the Frobenius norms squared: ||QgodLocal - solution||^2 and ||solution||^2
   double diffSquared = 0.0;
   double frobASquared = 0.0;
   for (unsigned int i = 0; i < qgodNeighbor.shape(0); i++) {
     for (unsigned int j = 0; j < qgodNeighbor.shape(1); j++) {
       const auto sol = i == j ? 1.0 : 0.0;
-      const auto diff = (qgodLocal(i, j) + qgodNeighbor(i, j)) - sol;
+      const auto diff = (castReal(qgodLocal(i, j)) + castReal(qgodNeighbor(i, j))) - sol;
       diffSquared += diff * diff;
       frobASquared += sol;
     }
@@ -49,7 +59,7 @@ inline void test_consistency(init::QgodLocal::view::type& qgodLocal,
   CHECK(std::abs(diffSquared) < epsilon * frobASquared * epsilon * frobASquared);
 }
 
-inline void test_nan(init::QgodLocal::view::type& qgodNeighbor) {
+inline void testNAN(init::QgodLocal::view::type& qgodNeighbor) {
   for (unsigned int i = 0; i < qgodNeighbor.shape(0); i++) {
     for (unsigned int j = 0; j < qgodNeighbor.shape(1); j++) {
       CHECK(std::isnan(qgodNeighbor(i, j)));
@@ -73,9 +83,9 @@ TEST_CASE("Godunov state is correct" * doctest::test_suite("model")) {
     const model::MaterialT neighbor(SolutionData<model::MaterialT>::MaterialVal1);
 
     model::getTransposedGodunovState(local, neighbor, FaceType::Regular, qgodLocal, qgodNeighbor);
-    test_matrix(qgodLocal, SolutionData<model::MaterialT>::SolutionHomogeneousLocal, Epsilon);
-    test_matrix(qgodNeighbor, SolutionData<model::MaterialT>::SolutionHomogeneousNeighbor, Epsilon);
-    test_consistency(qgodLocal, qgodNeighbor, Epsilon);
+    testMatrix(qgodLocal, SolutionData<model::MaterialT>::SolutionHomogeneousLocal, Epsilon);
+    testMatrix(qgodNeighbor, SolutionData<model::MaterialT>::SolutionHomogeneousNeighbor, Epsilon);
+    testConsistency(qgodLocal, qgodNeighbor, Epsilon);
   }
 
   SUBCASE("Free surface material") {
@@ -85,8 +95,8 @@ TEST_CASE("Godunov state is correct" * doctest::test_suite("model")) {
 
     model::getTransposedGodunovState(
         local, neighbor, FaceType::FreeSurface, qgodLocal, qgodNeighbor);
-    test_matrix(qgodLocal, SolutionData<model::MaterialT>::SolutionBoundary, Epsilon);
-    test_nan(qgodNeighbor);
+    testMatrix(qgodLocal, SolutionData<model::MaterialT>::SolutionBoundary, Epsilon);
+    testNAN(qgodNeighbor);
   }
 
   SUBCASE("Heterogenous material") {
@@ -95,10 +105,10 @@ TEST_CASE("Godunov state is correct" * doctest::test_suite("model")) {
     const model::MaterialT neighbor(SolutionData<model::MaterialT>::MaterialVal2);
 
     model::getTransposedGodunovState(local, neighbor, FaceType::Regular, qgodLocal, qgodNeighbor);
-    test_matrix(qgodLocal, SolutionData<model::MaterialT>::SolutionHeterogeneousLocal, Epsilon);
-    test_matrix(
+    testMatrix(qgodLocal, SolutionData<model::MaterialT>::SolutionHeterogeneousLocal, Epsilon);
+    testMatrix(
         qgodNeighbor, SolutionData<model::MaterialT>::SolutionHeterogeneousNeighbor, Epsilon);
-    test_consistency(qgodLocal, qgodNeighbor, Epsilon);
+    testConsistency(qgodLocal, qgodNeighbor, Epsilon);
   }
 }
 } // namespace seissol::unit_test
